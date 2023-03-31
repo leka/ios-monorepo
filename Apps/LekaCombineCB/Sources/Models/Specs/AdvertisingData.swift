4747a3e24864496cf2841707dcac8f3043d2e980
Hugo Pezziardi <hugo@pezziardi.net>
2023-03-31 16:40:36 +0200

    :sparkles: (specs): Add Specs Directory

---
+++ Apps/LekaCombineCB/Sources/Models/Specs/AdvertisingData.swift
@@ -0,0 +1,49 @@
+//
+//  AdvertisingData.swift
+//  LekaBLE
+//
+//  Created by Yann LOCATELLI on 08/12/2022.
+//
+import CombineCoreBluetooth
+import Foundation
+
+struct AdvertisingData {
+	private struct Index {
+		static let battery = 0
+		static let isCharging = 1
+		static let osVersionMajor = 2
+		static let osVersionMinor = 3
+		static let osVersionRevisionHighByte = 4
+		static let osVersionRevisionLowByte = 5
+	}
+
+	var name: String
+	var battery: Int
+	var isCharging: Bool
+	var osVersion: String
+
+	init?(_ advertisingData: AdvertisementData) {
+		guard let name = advertisingData.localName else { return nil }
+
+		self.name = name
+
+		guard let serviceData = advertisingData.serviceData else {
+			return nil
+		}
+		guard let lekaServiceData = serviceData[BLESpecs.AdvertisingData.service] else { return nil }
+
+		battery = Int(lekaServiceData[Index.battery])
+		isCharging = lekaServiceData[Index.isCharging] == 0x01
+
+		osVersion = ""
+		if lekaServiceData.count == 6 {
+			let osVersionMajor = lekaServiceData[Index.osVersionMajor]
+			let osVersionMinor = lekaServiceData[Index.osVersionMinor]
+			let osVersionRevision =
+				lekaServiceData[Index.osVersionRevisionHighByte] << 8
+				+ lekaServiceData[Index.osVersionRevisionLowByte]
+
+			osVersion = "\(osVersionMajor).\(osVersionMinor).\(osVersionRevision)"
+		}
+	}
+}
