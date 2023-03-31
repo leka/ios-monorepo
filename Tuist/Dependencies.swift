2ec2de830401b8d46347f2885ed1c8c0a0a348ec
Hugo Pezziardi <hugo@pezziardi.net>
2023-03-31 16:34:39 +0200

    :heavy_plus_sign: (CombineCB): Add dependency CombineCB

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -5,9 +5,11 @@
 import ProjectDescription
 
 let dependencies = Dependencies(
-    swiftPackageManager: [
-        .remote(url: "https://github.com/jpsim/Yams", requirement: .upToNextMajor(from: "5.0.5")),
-        .remote(url: "https://github.com/airbnb/lottie-ios", requirement: .upToNextMajor(from: "4.1.3")),
-    ],
-    platforms: [.iOS]
+	swiftPackageManager: [
+		.remote(url: "https://github.com/jpsim/Yams", requirement: .upToNextMajor(from: "5.0.5")),
+		.remote(url: "https://github.com/airbnb/lottie-ios", requirement: .upToNextMajor(from: "4.1.3")),
+		.remote(
+			url: "https://github.com/StarryInternet/CombineCoreBluetooth", requirement: .upToNextMajor(from: "0.3.1")),
+	],
+	platforms: [.iOS]
 )
