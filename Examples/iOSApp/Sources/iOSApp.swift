fbb85647323b2080982b7f16803d001ec22eecd4
Ladislas de Toldi <ladislas@detoldi.me>
2023-04-18 08:53:39 +0200

    :technologist: (tuist): Add examples for iOS app, macOS app, macOS cli, module

---
+++ Examples/iOSApp/Sources/iOSApp.swift
@@ -0,0 +1,14 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import SwiftUI
+
+@main
+struct iOSApp: App {  // swiftlint:disable:this type_name
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Examples/macOSApp/Sources/ContentView.swift
@@ -0,0 +1,26 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.teal
+                .ignoresSafeArea()
+
+            Text("Hello, macOS App Example!")
+                .foregroundColor(.white)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
