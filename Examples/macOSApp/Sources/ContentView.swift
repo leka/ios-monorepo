3cec31763dbbd83cc773b6bfda108c99ffdef1b2
Ladislas de Toldi <ladislas@detoldi.me>
2023-04-19 15:47:48 +0200

    :alembic: (Examples): Add Module dep to macOSApp + use HelloView

--- Examples/macOSApp/Sources/ContentView.swift
+++ Examples/macOSApp/Sources/ContentView.swift
@@ -2,20 +2,12 @@
 // Copyright 2023 APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
+import Module
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.teal
-                .ignoresSafeArea()
-
-            Text("Hello, macOS App Example!")
-                .foregroundColor(.white)
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
-        }
+        HelloView(color: .teal, name: "macOS App Example")
     }
 }
 
