76246b59f36932328ff7213656847737eb243019
mathieu J <macteuts@gmail.com>
2023-04-21 15:30:53 +0300

    :fire: Removed commented code

--- Apps/LekaActivityUIExplorer/Sources/GameLayoutTemplatesSpecs/GameLayoutTemplatesConfigurations.swift
+++ Apps/LekaActivityUIExplorer/Sources/GameLayoutTemplatesSpecs/GameLayoutTemplatesConfigurations.swift
@@ -19,20 +19,5 @@ class GameLayoutTemplatesConfigurations: ObservableObject {
         "LayoutTemplate_4", "LayoutTemplate_4_spaced", "LayoutTemplate_4_inline", "LayoutTemplate_6",
         "xylophoneTemplate",
     ]
-    //    @Published var templatesPreviews = [
-    //        "LayoutTemplate_1", "LayoutTemplate_2", "LayoutTemplate_3", "LayoutTemplate_4_spaced", "LayoutTemplate_6",
-    //    ]
-    //    @Published var templatesPreviewsAlternatives3 = ["LayoutTemplate_3", "LayoutTemplate_3_inline"]
-    //    @Published var templatesPreviewsAlternatives4 = [
-    //        "LayoutTemplate_4_spaced", "LayoutTemplate_4", "LayoutTemplate_4_inline",
-    //    ]
     @Published var typesOfActivity: [String] = ["touch_to_select", "listen_then_touch_to_select"]
-
-    // MARK: - Setup Editor when being fed a new activity (from yaml or models)
-    func setupEditor(with: Activity) {
-        preferred3AnswersLayout = .basic
-        preferred4AnswersLayout = .spaced
-        //        templatesPreviews[2] = templatesPreviewsAlternatives3[0]
-        //        templatesPreviews[3] = templatesPreviewsAlternatives4[0]
-    }
 }
