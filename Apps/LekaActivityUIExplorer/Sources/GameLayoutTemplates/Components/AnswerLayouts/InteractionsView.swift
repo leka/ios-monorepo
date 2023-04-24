cd9fb7895a5d03996f0178ecfd1cb9513c871bff
mathieu J <macteuts@gmail.com>
2023-04-24 16:37:57 +0300

    :lipstick: Move Play Button to answers Hstack + Add separator

--- Apps/LekaActivityUIExplorer/Sources/GameLayoutTemplates/Components/AnswerLayouts/InteractionsView.swift
+++ Apps/LekaActivityUIExplorer/Sources/GameLayoutTemplates/Components/AnswerLayouts/InteractionsView.swift
@@ -15,41 +15,44 @@ struct InteractionsView: View {
             if gameEngine.currentActivity.activityType == "xylophone" {
                 XylophoneLayout()
             } else {
-                if gameEngine.allAnswers.count == 1 {
-                    OneAnswerLayout()
-                } else if gameEngine.allAnswers.count == 2 {
-                    TwoAnswersLayout()
-                } else if gameEngine.allAnswers.count == 3 {
-                    switch configuration.preferred3AnswersLayout {
-                        case .inline: ThreeAnswersLayoutInline()
-                        default: ThreeAnswersLayout()
+                HStack(spacing: 0) {
+                    if gameEngine.currentActivity.activityType == "listen_then_touch_to_select" {
+                        PlaySoundButton()
+                            .padding(20)
+                        Divider()
+                            .opacity(0.4)
+                            .frame(maxHeight: 500)
+                            .padding(.vertical, 20)
                     }
-                } else if gameEngine.allAnswers.count == 4 {
-                    switch configuration.preferred4AnswersLayout {
-                        case .inline: FourAnswersLayoutInline()
-                        case .spaced: FourAnswersLayoutSpaced()
-                        default: FourAnswersLayout()
+                    Spacer()
+                    Group {
+                        if gameEngine.allAnswers.count == 1 {
+                            OneAnswerLayout()
+                        } else if gameEngine.allAnswers.count == 2 {
+                            TwoAnswersLayout()
+                        } else if gameEngine.allAnswers.count == 3 {
+                            switch configuration.preferred3AnswersLayout {
+                                case .inline: ThreeAnswersLayoutInline()
+                                default: ThreeAnswersLayout()
+                            }
+                        } else if gameEngine.allAnswers.count == 4 {
+                            switch configuration.preferred4AnswersLayout {
+                                case .inline: FourAnswersLayoutInline()
+                                case .spaced: FourAnswersLayoutSpaced()
+                                default: FourAnswersLayout()
+                            }
+                        } else if gameEngine.allAnswers.count == 6 {
+                            SixAnswersLayout()
+                        } else {
+                            Text("Sélectionner un modèle")
+                        }
                     }
-                } else if gameEngine.allAnswers.count == 6 {
-                    SixAnswersLayout()
-                } else {
-                    Text("Sélectionner un modèle")
+                    // .offset(x: gameEngine.currentActivity.activityType == "listen_then_touch_to_select" ? -120 : 0)
+                    Spacer()
                 }
             }
             Spacer()
         }
         .frame(maxWidth: .infinity)
-        .background(
-            VStack(alignment: .leading) {
-                HStack {
-                    if gameEngine.currentActivity.activityType == "listen_then_touch_to_select" {
-                        PlaySoundButton()
-                            .padding(40)
-                    }
-                    Spacer()
-                }
-            }
-            .frame(maxWidth: .infinity)
-        )
     }
 }
