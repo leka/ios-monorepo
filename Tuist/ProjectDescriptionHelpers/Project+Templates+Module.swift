9021b3d6202795c0b7c57bcbe0d267b41df285bc
Ladislas de Toldi <ladislas@detoldi.me>
2023-04-25 14:27:56 +0200

    :sparkles: (tuist): Module - add ability to define example apps
    When working on a module, it's often useful to have an example app to be
    able to test, visualize and tinker with the code
    
    `Project.Module` now lets you define example apps to do just that :)

--- Tuist/ProjectDescriptionHelpers/Project+Templates+Module.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates+Module.swift
@@ -4,21 +4,54 @@
 
 import ProjectDescription
 
+public struct ModuleExample {
+    public let name: String
+    public let infoPlist: [String: InfoPlist.Value]
+
+    public init(name: String, infoPlist: [String: InfoPlist.Value] = [:]) {
+        self.name = name
+        self.infoPlist = infoPlist
+    }
+}
+
 extension Project {
 
     public static func module(
-        name: String, platform: Platform, product: Product = .staticLibrary, dependencies: [TargetDependency]
+        name: String,
+        platform: Platform,
+        product: Product = .staticLibrary,
+        dependencies: [TargetDependency],
+        examples: [ModuleExample] = []
     ) -> Project {
-        let targets = makeFrameworkTargets(
+        let frameworkTargets = makeFrameworkTargets(
             name: name,
             platform: platform,
             product: product,
             dependencies: dependencies)
 
+        let exampleTargets = examples.compactMap { example in
+            let appInfoPlist = InfoPlist.base(version: "1.0.0").merging(example.infoPlist) { (_, new) in new }
+
+            let target = Target(
+                name: example.name,
+                platform: .iOS,
+                product: .app,
+                bundleId: "io.leka.apf.app.example.\(example.name)",
+                deploymentTarget: .iOS(targetVersion: "16.0", devices: .ipad),
+                infoPlist: .extendingDefault(with: appInfoPlist),
+                sources: ["Examples/\(example.name)/Sources/**"],
+                resources: ["Examples/\(example.name)/Resources/**"],
+                scripts: TargetScript.linters(),
+                dependencies: [.target(name: name)]
+            )
+
+            return target
+        }
+
         return Project(
             name: name,
             organizationName: "leka.io",
-            targets: targets)
+            targets: frameworkTargets + exampleTargets)
     }
 
 }
