8141e5a4afcfc4957fd50ee85ad63376b259f3a5
Ladislas de Toldi <ladislas@detoldi.me>
2023-04-28 11:10:55 +0200

    :technologist: (tuist): Run swift-format, swiftlint only on changed files
    Previously, linters were run on all the files which could take several
    seconds
    
    This refactor only runs the linters on git changed files, making it
    faster and improving the experience

--- Scripts/SwiftFormatRunScript.sh
+++ Scripts/SwiftFormatRunScript.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Leka - iOS Monorepo
 # Copyright 2023 APF France handicap
 # SPDX-License-Identifier: Apache-2.0
@@ -10,14 +12,30 @@ fi
 
 export PATH
 
+if ! command -v swift-format &> /dev/null; then
+    echo "error: swift-format not installed, download from https://github.com/apple/swift-format"
+    echo "error: or install with brew install swift-format"
+    exit 1
+fi
+
+if ! command -v gxargs &> /dev/null; then
+    echo "error: gxargs not installed, download from https://www.gnu.org/software/findutils/"
+    echo "error: or install with brew install findutils"
+    exit 1
+fi
+
 SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 ROOT_DIR=$(realpath "$SCRIPT_DIR/..")
 
-if which swift-format > /dev/null; then
-	echo "ROOT_DIR: $ROOT_DIR"
-	echo "SCRIPT_DIR: $SCRIPT_DIR"
-	swift-format format --configuration $ROOT_DIR/.swift-format --in-place --parallel  --color-diagnostics --recursive $ROOT_DIR
-else
-    echo "warning: swift-format not installed, download from https://github.com/apple/swift-format"
-fi
+echo "SCRIPT_PATH: $SCRIPT_PATH"
+echo "ROOT_DIR: $ROOT_DIR"
+echo "SCRIPT_DIR: $SCRIPT_DIR"
+
+which swift-format
+which gxargs
+
+git diff --name-status origin/main        \
+| grep -E "^A|^M" | sed "s/^[AM]\t//g"    \
+| grep -E "\.swift\$$"                    \
+| gxargs -I '{}' -d '\n' --no-run-if-empty swift-format format --configuration $ROOT_DIR/.swift-format --in-place --parallel --color-diagnostics "$ROOT_DIR/{}"
