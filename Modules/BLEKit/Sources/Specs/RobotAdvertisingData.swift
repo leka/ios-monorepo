acbeb216b2cc05bfe6d9be2a0752061351efc281
Ladislas de Toldi <ladislas@detoldi.me>
2023-05-02 14:04:37 +0200

    :recycle: (BLEKit): Refactor RobotAdvertisingData

--- Modules/BLEKit/Sources/Specs/RobotAdvertisingData.swift
+++ Modules/BLEKit/Sources/Specs/RobotAdvertisingData.swift
@@ -6,46 +6,62 @@ import CombineCoreBluetooth
 
 public struct RobotAdvertisingData {
 
-    private struct Index {
+    // MARK: - Private variables
+
+    private let advertisementData: AdvertisementData
+    private let serviceData: Data
+
+    // MARK: - Public functions
+
+    public init?(_ advertisementData: AdvertisementData) {
+        guard
+            let rawServiceData = advertisementData.serviceData,
+            let robotServiceData = rawServiceData[BLESpecs.AdvertisingData.service]
+        else {
+            return nil
+        }
+
+        self.advertisementData = advertisementData
+        self.serviceData = robotServiceData
+    }
+
+    // MARK: - Public variables
+
+    public var name: String {
+        return self.advertisementData.localName ?? "⚠️ NO NAME"
+    }
+
+    public var isCharging: Bool {
+        return serviceData[Index.isCharging] == 0x01
+    }
+
+    public var battery: Int {
+        return Int(serviceData[Index.battery])
+    }
+
+    public var osVersion: String {
+        guard self.serviceData.count == 6 else {
+            return "⚠️ NO OS VERSION"
+        }
+
+        let major = self.serviceData[Index.osVersionMajor]
+        let minor = self.serviceData[Index.osVersionMinor]
+        let revision =
+            self.serviceData[Index.osVersionRevisionHighByte] << 8
+            + self.serviceData[Index.osVersionRevisionLowByte]
+
+        return "\(major).\(minor).\(revision)"
+    }
+
+    // MARK: - Misc.
+
+    private enum Index {
         static let battery = 0
         static let isCharging = 1
         static let osVersionMajor = 2
         static let osVersionMinor = 3
         static let osVersionRevisionHighByte = 4
         static let osVersionRevisionLowByte = 5
-    }
-
-    public var name: String
-    public var battery: Int
-    public var isCharging: Bool
-    public var osVersion: String
-
-    public init?(_ advertisementData: AdvertisementData) {
-        guard let name = advertisementData.localName else { return nil }
-
-        self.name = name
-
-        guard let serviceData = advertisementData.serviceData else {
-            return nil
-        }
-
-        guard let lekaServiceData = serviceData[BLESpecs.AdvertisingData.service] else {
-            return nil
-        }
-
-        battery = Int(lekaServiceData[Index.battery])
-        isCharging = lekaServiceData[Index.isCharging] == 0x01
-
-        osVersion = ""
-        if lekaServiceData.count == 6 {
-            let osVersionMajor = lekaServiceData[Index.osVersionMajor]
-            let osVersionMinor = lekaServiceData[Index.osVersionMinor]
-            let osVersionRevision =
-                lekaServiceData[Index.osVersionRevisionHighByte] << 8
-                + lekaServiceData[Index.osVersionRevisionLowByte]
-
-            osVersion = "\(osVersionMajor).\(osVersionMinor).\(osVersionRevision)"
-        }
     }
 
 }
