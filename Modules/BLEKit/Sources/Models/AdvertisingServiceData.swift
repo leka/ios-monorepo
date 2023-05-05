ff57e352c1410111106aa4ca7866475d3352fef2
Ladislas de Toldi <ladislas@detoldi.me>
2023-05-05 14:47:26 +0200

    :recycle: (BLEKit): Refactor RobotAdvertisingData
    - create struct AdvertisingServiceData
    - add unit tests for AdvertisingServiceData
    - fix bug in osVersion computation (bit shift was done on UInt8 not on Int)
    - refactor RobotAdvertisingData to use AdvertisingServiceData
    - add unit tests for RobotAdvertisingData
    - add String extension

---
+++ Modules/BLEKit/Sources/Models/AdvertisingServiceData.swift
@@ -0,0 +1,45 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Foundation
+
+internal struct AdvertisingServiceData {
+    enum Index {
+        static let battery = 0
+        static let isCharging = 1
+        static let osVersionMajor = 2
+        static let osVersionMinor = 3
+        static let osVersionRevisionHighByte = 4
+        static let osVersionRevisionLowByte = 5
+    }
+
+    private let data: Data
+
+    init(data: Data) {
+        self.data = data
+    }
+
+    public var battery: Int {
+        return Int(data[Index.battery])
+    }
+
+    public var isCharging: Bool {
+        return data[Index.isCharging] == 0x01
+    }
+
+    public var osVersion: String {
+        guard data.count == 6 else {
+            return "⚠️ NO OS VERSION"
+        }
+
+        let major = data[Index.osVersionMajor]
+        let minor = data[Index.osVersionMinor]
+        let revision =
+            Int(data[Index.osVersionRevisionHighByte]) << 8
+            + Int(data[Index.osVersionRevisionLowByte])
+
+        return "\(major).\(minor).\(revision)"
+    }
+
+}
