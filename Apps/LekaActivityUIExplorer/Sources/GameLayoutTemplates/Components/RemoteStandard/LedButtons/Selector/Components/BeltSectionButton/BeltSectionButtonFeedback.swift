1d86b7f9810481dc48294207705ce2aef7bb9df2
Hugo Pezziardi <hugo@pezziardi.net>
2023-05-12 13:05:35 +0200

    :sparkles: (BeltSectionButton): Add BeltSectionButton & its feedback

---
+++ Apps/LekaActivityUIExplorer/Sources/GameLayoutTemplates/Components/RemoteStandard/LedButtons/Selector/Components/BeltSectionButton/BeltSectionButtonFeedback.swift
@@ -0,0 +1,31 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import SwiftUI
+
+struct BeltSectionButtonFeedback: View {
+    var startAngle: Angle
+    var endAngle: Angle
+    var color: Color
+    var lineWidth: Int
+
+    var body: some View {
+        Arc(startAngle: startAngle, endAngle: endAngle, clockwise: true)
+            .stroke(
+                color.opacity(0.5),
+                style: StrokeStyle(
+                    lineWidth: CGFloat(lineWidth),
+                    lineCap: .round,
+                    lineJoin: .round,
+                    miterLimit: 10)
+            )
+            .frame(width: 300, height: 300)
+    }
+}
+
+struct BeltSectionButtonFeedback_Previews: PreviewProvider {
+    static var previews: some View {
+        BeltSectionButtonFeedback(startAngle: .degrees(0), endAngle: .degrees(180), color: .red, lineWidth: 20)
+    }
+}
