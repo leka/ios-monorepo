3f685b50f618a0225e3dd5b1c9f1844902136585
Yann Locatelli <yann.locatelli2@gmail.com>
2023-05-12 12:02:44 +0200

    :art: (LekaUpdater): Move in new file RequirementsView

--- Apps/LekaUpdater/Sources/View/RobotUpdateAvailableView.swift
+++ Apps/LekaUpdater/Sources/View/RobotUpdateAvailableView.swift
@@ -4,41 +4,6 @@
 
 import SwiftUI
 
-struct RequirementsView: View {
-    @Binding var battery: Int
-    @Binding var isCharging: Bool
-
-    private var batteryImage: String {
-        if battery >= 100 {
-            return "battery.100"
-        } else if battery >= 75 {
-            return "battery.75"
-        } else if battery >= 50 {
-            return "battery.50"
-        } else if battery >= 25 {
-            return "battery.25"
-        } else {
-            return "battery.0"
-        }
-    }
-
-    var body: some View {
-        Image(systemName: batteryImage)
-            .resizable()
-            .scaledToFit()
-            .frame(height: 50)
-            .padding()
-            .foregroundColor(battery >= 30 ? .green : .red)
-
-        Image(systemName: "powerplug.fill")
-            .resizable()
-            .scaledToFit()
-            .frame(height: 50)
-            .padding()
-            .foregroundColor(isCharging ? .green : .red)
-    }
-}
-
 struct RobotUpdateAvailableView: View {
     @State private var isCharging = true
 
