8e65899cdd304d9de2aa8582445ca9761244176f
Hugo Pezziardi <hugo@pezziardi.net>
2023-05-15 17:41:26 +0200

    :boom: (JoystickPWM): Add convertor to get PWM from the Joystick

--- Apps/LekaActivityUIExplorer/Sources/GameLayoutTemplates/Components/RemoteStandard/Joystick/JoystickView.swift
+++ Apps/LekaActivityUIExplorer/Sources/GameLayoutTemplates/Components/RemoteStandard/Joystick/JoystickView.swift
@@ -8,6 +8,10 @@ import SwiftUIJoystick
 struct JoystickView: View {
 
     @StateObject public var joystickMonitor = JoystickMonitor()
+    @State private var leftMotor: UInt8 = 0
+    @State private var rightMotor: UInt8 = 0
+
+    private let power = 255.0 / 300.0
 
     private let dragDiameter: CGFloat = 300
     private let shape: JoystickShape = .circle
@@ -42,7 +46,6 @@ struct JoystickView: View {
                             Image(systemName: "arrow.down")
                                 .foregroundColor(.gray.opacity(0.7))
                         }
-
                     }
                 },
                 foreground: {
@@ -50,7 +53,29 @@ struct JoystickView: View {
                         .fill(.gray)
                 },
                 locksInPlace: false)
+
+//            Text("XY Point = (x: \(joystickMonitor.xyPoint.x), y: \(joystickMonitor.xyPoint.y))")
+//                .fixedSize()
+//            Text("Motor PWM = (left: \(convertPosXToPWM()), right: \(convertPosYToPWM()))")
+//                .fixedSize()
         }
 
     }
+
+    func convertPosXToPWM() -> CGFloat {
+        let leftPWM = 255.0 / dragDiameter * (joystickMonitor.xyPoint.x - joystickMonitor.xyPoint.y)
+
+        return clamp(leftPWM, lower: -255, upper: 255)
+    }
+
+    func convertPosYToPWM() -> CGFloat {
+        let rightPWM = -255 / dragDiameter * (joystickMonitor.xyPoint.x + joystickMonitor.xyPoint.y)
+
+        return clamp(rightPWM, lower: -255, upper: 255)
+    }
+
+    fileprivate func clamp<T: Comparable>(_ value: T, lower: T, upper: T) -> T {
+        return min(max(value, lower), upper)
+    }
+
 }
