2b63c2973b505380dab7bca40a054c45738a7e36
mathieu J <macteuts@gmail.com>
2023-06-04 18:42:17 +0300

    :sparkles: (UIExplorer): Add Reinforcer and EndOfActivity
    - Fix Body Length Violation in ReplayScreen's func
    - Fix Lottie assets' path + execute replay in main thread
    - Fix Replay Screen animations
    - Fix ReplayScreen animations
    - Make Reinforcer usage optional w/ topbar toggle
    - Moved reinforcer up to ContentView as well to avoid ScenePhase/HoverPhase interferences
    - Use stored values instead of GeometryReader computed values

---
+++ Apps/LekaActivityUIExplorer/Sources/GameLayoutTemplates/Components/EndOfStepFeedback/FeedbackViews/EndOfRoundView.swift
@@ -0,0 +1,141 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import SwiftUI
+
+struct EndOfRoundView: View {
+
+    @EnvironmentObject var gameEngine: GameEngine
+    @EnvironmentObject var defaults: GameLayoutTemplatesDefaults
+
+    // animation-related values
+    @State private var textOffset: CGFloat = -100
+    @State private var textOpacity: Double = 0
+    @State private var offsetGameOverBtn: CGFloat = -700
+    @State private var offsetReplayBtn: CGFloat = 700
+
+    var body: some View {
+        ZStack {
+            LottieView(
+                name: gameEngine.percentOfSuccess >= 80 ? "bravo" : "tryAgain", play: $gameEngine.showEndAnimation
+            )
+            .onAppear {
+                // Delayed to avoid artifacts on animation... SwiftUI bug
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    textOffset = 0
+                    textOpacity = 1
+                    offsetGameOverBtn = 0
+                    offsetReplayBtn = 0
+                }
+            }
+
+            VStack(spacing: 0) {
+                resultMessage(result: gameEngine.result)
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                            gameEngine.resetActivity()
+                        }
+                    } label: {
+                        Text("gameOver_button")
+                    }
+                    .buttonStyle(BorderedCapsule_ButtonStyle(isFilled: false))
+                    .offset(x: offsetGameOverBtn)
+                    .animation(
+                        .easeOut(duration: defaults.endAnimBtnDuration).delay(defaults.endAnimGameOverBtnDelay),
+                        value: offsetGameOverBtn)
+                    Spacer()
+                    Button {
+                        gameEngine.replayCurrentActivity()
+                        textOffset = -100
+                        textOpacity = 0
+                    } label: {
+                        Text("replay_button")
+                    }
+                    .buttonStyle(BorderedCapsule_ButtonStyle())
+                    .offset(x: offsetReplayBtn)
+                    .animation(
+                        .easeOut(duration: defaults.endAnimBtnDuration).delay(defaults.endAnimReplayBtnDelay),
+                        value: offsetReplayBtn)
+                    Spacer()
+                }
+            }
+            .padding(.vertical, defaults.endAnimBtnPadding)
+        }
+    }
+
+    // Result Texts
+    private func makeTopMessage(result: ResultType) -> Text {
+        switch result {
+            case .fail:
+                return Text("fail_top_message")
+            case .medium:
+                return Text("medium_top_message")
+            case .success:
+                return Text("success_top_message")
+            default:
+                return Text("")
+        }
+    }
+
+    private func makeBottomMessage(result: ResultType) -> Text {
+        switch result {
+            case .fail:
+                return Text("fail_bottom_message")
+                    + Text("(0%)")
+                    .foregroundColor(Color("bravoHighlights"))
+                    + Text(".")
+            case .medium, .success:
+                return Text("success_bottom_message")
+                    + Text(
+                        "success_bottom_result \(gameEngine.goodAnswers) \(gameEngine.currentActivity.stepsAmount) \(gameEngine.percentOfSuccess)"
+                    )
+                    .foregroundColor(Color("bravoHighlights"))
+                    + Text("!")
+            default:
+                return Text("")
+        }
+    }
+
+    @ViewBuilder
+    func resultMessage(result: ResultType) -> some View {
+        VStack(spacing: defaults.endAnimTextsSpacing) {
+            Group {
+                makeTopMessage(result: result)
+                    .foregroundColor(.accentColor)
+                    .offset(y: textOffset)
+                    .opacity(textOpacity)
+                    .animation(
+                        .easeOut(duration: defaults.endAnimDuration).delay(defaults.endAnimDelayTop),
+                        value: textOffset
+                    )
+                    .animation(
+                        .easeOut(duration: defaults.endAnimDuration).delay(defaults.endAnimDelayTop),
+                        value: textOpacity
+                    )
+                makeBottomMessage(result: result)
+                    .foregroundColor(Color("lekaDarkGray"))
+                    .offset(y: textOffset)
+                    .opacity(textOpacity)
+                    .animation(
+                        .easeOut(duration: defaults.endAnimDuration).delay(defaults.endAnimDelayBottom),
+                        value: textOffset
+                    )
+                    .animation(
+                        .easeOut(duration: defaults.endAnimDuration).delay(defaults.endAnimDelayBottom),
+                        value: textOpacity
+                    )
+            }
+            .font(
+                .system(
+                    size: defaults.endAnimFontSize,
+                    weight: defaults.endAnimFontWeight,
+                    design: defaults.endAnimFontDesign
+                )
+            )
+        }
+    }
+}
---
+++ Apps/LekaActivityUIExplorer/Sources/GameLayoutTemplates/Components/EndOfStepFeedback/LottieView.swift
@@ -0,0 +1,94 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Lottie
+import SwiftUI
+
+struct LottieView: UIViewRepresentable {
+    typealias UIViewType = UIView
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    var name: String!
+    var speed: CGFloat
+    var reverse: Bool
+    var action: () -> Void
+    @Binding var play: Bool
+
+    public init(
+        name: String,
+        speed: CGFloat = 1,
+        reverse: Bool = false,
+        action: @escaping () -> Void = {
+            // empty default implementation
+        },
+        play: Binding<Bool> = .constant(true)
+    ) {
+        self.name = name
+        self.speed = speed
+        self.reverse = reverse
+        self.action = action
+        self._play = play
+    }
+
+    var animationView = LottieAnimationView()
+
+    class Coordinator: NSObject {
+        var parent: LottieView
+
+        init(_ animationView: LottieView) {
+            self.parent = animationView
+            super.init()
+        }
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView()
+
+        animationView.animation = LottieAnimation.named(name)
+        animationView.contentMode = .scaleAspectFit
+        animationView.animationSpeed = speed
+        animationView.loopMode = .playOnce
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+        ])
+
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<LottieView>) {
+        if play {
+            if reverse {
+                context.coordinator.parent.animationView.play(fromProgress: 0.0, toProgress: 1.0, loopMode: .none) {
+                    _ in
+                    animationView.pause()
+                }
+            } else {
+                context.coordinator.parent.animationView.play { finished in
+                    if finished {
+                        animationView.pause()
+                        action()
+                    }
+                }
+            }
+        } else {
+            if reverse {
+                context.coordinator.parent.animationView.animationSpeed = speed * 1.5
+                context.coordinator.parent.animationView.play(fromProgress: 1.0, toProgress: 0.0, loopMode: .none) {
+                    _ in
+                    context.coordinator.parent.animationView.stop()
+                    context.coordinator.parent.animationView.animationSpeed = speed
+                }
+            } else {
+                context.coordinator.parent.animationView.stop()
+            }
+        }
+    }
+}
