362274fb5ee03836c06851e026cf996946b2e22d
Yann Locatelli <yann.locatelli2@gmail.com>
2023-06-09 13:56:02 +0200

    :lipstick: (LekaUpdater): Add SendingFileView

---
+++ Apps/LekaUpdater/Sources/View/UpdatingViews/SendingFileView.swift
@@ -0,0 +1,45 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import SwiftUI
+
+struct SendingFileView: View {
+    @State private var progress = 0.66
+
+    var body: some View {
+        VStack {
+            Text("Envoi de la mise à jour vers le robot")
+                .font(.title2)
+                .bold()
+
+            ProgressView(value: progress)
+                .tint(Color(red: 160 / 255, green: 185 / 255, blue: 49 / 255))
+                .scaleEffect(y: 2)
+                .padding(40)
+                .background(
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(.white)
+                        .shadow(radius: 3, y: 4)
+                )
+                .frame(width: 600)
+
+            Text(
+                """
+                Ne débranchez pas votre robot.
+                Ne le sortez pas de son socle de recharge.
+                """
+            )
+            .multilineTextAlignment(.center)
+            .padding()
+        }
+    }
+}
+
+struct SendingFileView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            SendingFileView()
+        }
+    }
+}
