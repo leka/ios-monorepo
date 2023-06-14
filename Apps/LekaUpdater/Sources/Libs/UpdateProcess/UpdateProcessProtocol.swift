fbf7631b7bb91cf73b79da1795c908493693d365
Yann Locatelli <yann.locatelli2@gmail.com>
2023-06-14 11:44:55 +0200

    🏗️ (LekaUpdater): Add states, events error and protocol for StateMachineVersion

---
+++ Apps/LekaUpdater/Sources/Libs/UpdateProcess/UpdateProcessProtocol.swift
@@ -0,0 +1,34 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Combine
+import Foundation
+
+//
+// MARK: - States, events, errors
+//
+
+enum UpdateProcessState {
+    case initial
+}
+
+enum UpdateProcessEvent {
+    case startUpdateRequested
+}
+
+enum UpdateProcessError: Error {
+    case unknown
+    case notAvailable
+}
+
+//
+// MARK: - UpdateProcess
+//
+
+protocol UpdateProcessProtocol {
+    var currentState: CurrentValueSubject<UpdateProcessState, UpdateProcessError> { get set }
+    var userState: CurrentValueSubject<UpdateStatusState, UpdateStatusError> { get set }
+
+    func startUpdate()
+}
