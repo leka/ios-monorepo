54811aeb86ee4c6b39f3ba2c200d08cf5fc55ece
Yann Locatelli <yann.locatelli2@gmail.com>
2023-06-14 16:24:51 +0200

    🏗️ (LekaUpdater): Add UpdateProcessController

---
+++ Apps/LekaUpdater/Sources/Libs/UpdateProcess/UpdateProcessController.swift
@@ -0,0 +1,41 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Combine
+import Foundation
+
+// MARK: - States, errors
+
+enum UpdateStatusState {
+    case initial
+}
+
+enum UpdateStatusError: Error {
+    case unknown
+    case updateProcessNotAvailable
+}
+
+//
+// MARK: - StateMachine
+//
+
+class UpdateProcessController {
+    private var currentStateMachine: any UpdateProcessProtocol
+
+    public var currentState = CurrentValueSubject<UpdateStatusState, UpdateStatusError>(.initial)
+
+    init(robot: DummyRobotModel) {
+        let currentRobotVersion = robot.osVersion
+
+        switch currentRobotVersion {
+            default:
+                self.currentStateMachine = UpdateProcessUnavailable()
+        }
+        self.currentState = self.currentStateMachine.userState
+    }
+
+    func startUpdate() {
+        currentStateMachine.startUpdate()
+    }
+}
