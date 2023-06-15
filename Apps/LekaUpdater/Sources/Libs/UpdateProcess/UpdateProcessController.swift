f3a7c7d0d3d98c8bef2f3c68b57019c670591b1a
Yann Locatelli <yann.locatelli2@gmail.com>
2023-06-15 11:16:11 +0200

    :truck: (LekaUpdater): Move UpdateProcessUnavailable in new file

--- Apps/LekaUpdater/Sources/Libs/UpdateProcess/UpdateProcessController.swift
+++ Apps/LekaUpdater/Sources/Libs/UpdateProcess/UpdateProcessController.swift
@@ -49,38 +49,3 @@ class UpdateProcessController {
         currentStateMachine.startUpdate()
     }
 }
-
-private class UpdateProcessUnavailable: UpdateProcessProtocol {
-    private var cancellables: Set<AnyCancellable> = []
-
-    var currentState = CurrentValueSubject<UpdateProcessState, UpdateProcessError>(.initial)
-    var userState = CurrentValueSubject<UpdateStatusState, UpdateStatusError>(.initial)
-
-    init() {
-        subscribeToStateUpdate()
-    }
-
-    private func subscribeToStateUpdate() {
-        self.currentState
-            .receive(on: DispatchQueue.main)
-            .sink(receiveCompletion: self.convertCompletion, receiveValue: self.convertReceivedValue)
-            .store(in: &cancellables)
-    }
-
-    private func convertCompletion(completion: Subscribers.Completion<UpdateProcessError>) {
-        switch completion {
-            case .finished:
-                self.userState.send(completion: .finished)
-            case .failure(let error):
-                self.userState.send(completion: .failure(.updateProcessNotAvailable))  // only available error
-        }
-    }
-
-    private func convertReceivedValue(state: UpdateProcessState) {
-        self.userState.send(.initial)  // only available state
-    }
-
-    func startUpdate() {
-        currentState.send(completion: .failure(.notAvailable))
-    }
-}
