fdbb4777a7f8cabf6842792fbd7b7b420b431497
Hugo Pezziardi <hugo@pezziardi.net>
2023-06-29 17:34:13 +0200

    :recycle: (GameEngineKitExample): Update Components & ContentView

--- Modules/GameEngineKit/Examples/GameEngineKitExample/Sources/ContentView.swift
+++ Modules/GameEngineKit/Examples/GameEngineKitExample/Sources/ContentView.swift
@@ -6,10 +6,72 @@ import GameEngineKit
 import SwiftUI
 
 struct ContentView: View {
+    let threeInlineGameplay = [
+        oneAnswerThreeInlineGameplay, allAnswersThreeInlineGameplay, someAnswersThreeInlineGameplay,
+    ]
+    let sixGridGameplay = [oneAnswerSixGridGameplay, allAnswersSixGridGameplay, someAnswersSixGridGameplay]
 
     var body: some View {
-        //        ThreeChoicesInlineView(viewModel: gameEngineThree.viewModel)
+        Grid(
+            horizontalSpacing: 30,
+            verticalSpacing: 30
+        ) {
+            GridRow {
+                ForEach(threeInlineGameplay, id: \.name) { gameplay in
+                    NavigationLink {
+                        Text(gameplay.name)
+                            .font(.title)
 
-        SixChoicesGridView(viewModel: gameEngineSix.viewModel)
+                        ThreeChoicesInlineView(gameplay: gameplay)
+                    } label: {
+                        VStack {
+                            Text("Type d'interfaces : \n Three Choices Inline")
+                                .font(.title2)
+                                .foregroundColor(.black)
+                                .padding(5)
+                            Text("Gameplay : \n \(gameplay.name)")
+                                .font(.title2)
+                                .foregroundColor(.black)
+                                .padding(5)
+                        }
+                        .background(.blue.opacity(0.5))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.primary)
+                        )
+                    }
+                }
+            }
+
+            GridRow {
+                ForEach(sixGridGameplay, id: \.name) { gameplay in
+                    NavigationLink {
+                        Text(gameplay.name)
+                            .font(.title)
+
+                        SixChoicesGridView(gameplay: gameplay)
+                    } label: {
+                        VStack {
+                            Text("Type d'interfaces : \n Six Choices Grid")
+                                .font(.title2)
+                                .foregroundColor(.black)
+                                .padding(5)
+                            Text("Gameplay : \n \(gameplay.name)")
+                                .font(.title2)
+                                .foregroundColor(.black)
+                                .padding(5)
+                        }
+                        .background(.blue.opacity(0.5))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.primary)
+                        )
+                    }
+                }
+            }
+        }
     }
 }
