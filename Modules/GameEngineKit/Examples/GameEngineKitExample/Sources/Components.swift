a5a4d9804e71a51b49a39204edda786fd82069b8
Hugo Pezziardi <hugo@pezziardi.net>
2023-06-29 17:34:13 +0200

    :recycle: (GameEngineKitExample): Update Components & ContentView

--- Modules/GameEngineKit/Examples/GameEngineKitExample/Sources/Components.swift
+++ Modules/GameEngineKit/Examples/GameEngineKitExample/Sources/Components.swift
@@ -5,31 +5,53 @@
 import GameEngineKit
 import SwiftUI
 
-let threeRawItems = ["red", "blue", "yellow"]
-let threeTypes = [DataType.color, DataType.text, DataType.color]
+let listThreeChoices = [
+    ChoiceViewModel(item: "red", type: .color),
+    ChoiceViewModel(item: "blue", type: .text),
+    ChoiceViewModel(item: "yellow", type: .color),
+]
 
-let sixRawItems = ["red", "blue", "yellow", "dummy_1", "yellow", "pink"]
-let sixTypes = [DataType.color, DataType.text, DataType.color, DataType.image, DataType.color, DataType.text]
+let listSixChoices = [
+    ChoiceViewModel(item: "red", type: .color),
+    ChoiceViewModel(item: "blue", type: .text),
+    ChoiceViewModel(item: "yellow", type: .color),
+    ChoiceViewModel(item: "purple", type: .color),
+    ChoiceViewModel(item: "blue", type: .color),
+    ChoiceViewModel(item: "pink", type: .text),
+]
 
-let listThreeChoices = (0..<3)
-    .map {
-        ChoiceViewModel(item: threeRawItems[$0], type: threeTypes[$0])
-    }
-
-let listSixChoices = (0..<6)
-    .map {
-        ChoiceViewModel(item: sixRawItems[$0], type: sixTypes[$0])
-    }
-
-public let dataThree = Data(
+public let dataThree1 = StandardGameplayData(
     choices: listThreeChoices,
     rightAnswers: [listThreeChoices[1]])
 
-public let dataSix = Data(
+public let dataThree2 = StandardGameplayData(
+    choices: listThreeChoices,
+    rightAnswers: [listThreeChoices[1], listThreeChoices[2]])
+
+public let dataThree3 = StandardGameplayData(
+    choices: listThreeChoices,
+    rightAnswers: [listThreeChoices[1], listThreeChoices[2]],
+    answersNumber: 2
+)
+
+public let dataSix1 = StandardGameplayData(
     choices: listSixChoices,
-    rightAnswers: [listSixChoices[2], listSixChoices[3], listSixChoices[4]])
+    rightAnswers: [listSixChoices[2]])
 
-public var gameEngineThree = GameEngine(
-    data: dataThree, gameplay: .selectTheRightAnswer)
+public let dataSix2 = StandardGameplayData(
+    choices: listSixChoices,
+    rightAnswers: [listSixChoices[1], listSixChoices[3], listSixChoices[4]])
 
-public var gameEngineSix = GameEngine(data: dataSix, gameplay: .selectAllRightAnswers)
+public let dataSix3 = StandardGameplayData(
+    choices: listSixChoices,
+    rightAnswers: [listSixChoices[1], listSixChoices[3], listSixChoices[4]],
+    answersNumber: 2
+)
+
+public var oneAnswerThreeInlineGameplay = gameplaySelector(type: .selectTheRightAnswer, data: dataThree1)
+public var allAnswersThreeInlineGameplay = gameplaySelector(type: .selectAllRightAnswers, data: dataThree2)
+public var someAnswersThreeInlineGameplay = gameplaySelector(type: .selectSomeRightAnswers, data: dataThree3)
+
+public var oneAnswerSixGridGameplay = gameplaySelector(type: .selectTheRightAnswer, data: dataSix1)
+public var allAnswersSixGridGameplay = gameplaySelector(type: .selectAllRightAnswers, data: dataSix2)
+public var someAnswersSixGridGameplay = gameplaySelector(type: .selectSomeRightAnswers, data: dataSix3)
