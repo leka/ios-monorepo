bc3899ab901e2a6252dfe7357a511cbb8c07f70e
Yann Locatelli <yann.locatelli2@gmail.com>
2023-06-30 11:05:28 +0200

    :sparkles: (LekaUpdater): Add a template of update process

---
+++ Apps/LekaUpdater/Sources/Libs/UpdateProcess/Template/UpdateProcessTemplate.swift
@@ -0,0 +1,63 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Combine
+import Foundation
+
+class UpdateProcessTemplate: UpdateProcessProtocol {
+
+    // MARK: - Internal states, events, errors
+
+    enum UpdateState {
+        case initial
+        case inProgress
+        case success
+    }
+
+    enum UpdateEvent {
+        case startUpdateRequested
+    }
+
+    enum UpdateError: Error {
+        case unknown
+        case notAvailable
+    }
+
+    // MARK: - Private variables
+
+    private var cancellables: Set<AnyCancellable> = []
+    private var currentInternalState = CurrentValueSubject<UpdateState, UpdateError>(.initial)
+
+    // MARK: - Public variables
+
+    public var currentStage = CurrentValueSubject<UpdateProcessStage, UpdateProcessError>(.initial)
+
+    init() {
+        subscribeToStateUpdate()
+    }
+
+    private func subscribeToStateUpdate() {
+        self.currentInternalState
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: self.convertCompletion, receiveValue: self.convertReceivedValue)
+            .store(in: &cancellables)
+    }
+
+    private func convertCompletion(completion: Subscribers.Completion<UpdateError>) {
+        switch completion {
+            case .finished:
+                self.currentStage.send(completion: .finished)
+            case .failure(let error):
+                self.currentStage.send(completion: .failure(.updateProcessNotAvailable))  // only available error
+        }
+    }
+
+    private func convertReceivedValue(state: UpdateState) {
+        self.currentStage.send(.initial)  // only available state
+    }
+
+    func startProcess() {
+        currentInternalState.send(completion: .failure(.notAvailable))
+    }
+}
