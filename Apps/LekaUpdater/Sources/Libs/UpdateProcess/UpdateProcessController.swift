0c4227bcc8b1eb37da18cf7e7544d5bb99080278
Yann Locatelli <yann.locatelli2@gmail.com>
2023-06-30 11:06:03 +0200

    :building_construction: (LekaUpdater): Add update process controller
    It is used to select the right update process depending on robot version
    and to retrieve status and error from it

--- Apps/LekaUpdater/Sources/Libs/UpdateProcess/UpdateProcessController.swift
+++ Apps/LekaUpdater/Sources/Libs/UpdateProcess/UpdateProcessController.swift
@@ -2,6 +2,7 @@
 // Copyright 2023 APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
+import Combine
 import Foundation
 
 // MARK: - General (user facing) update process states, errors
@@ -17,3 +18,30 @@ enum UpdateProcessError: Error {
     case updateProcessNotAvailable
 }
 
+// MARK: - Controller
+
+class UpdateProcessController {
+
+    // MARK: - Private variables
+
+    private var currentUpdateProcess: any UpdateProcessProtocol
+
+    // MARK: - Public variables
+
+    public var currentStage = CurrentValueSubject<UpdateProcessStage, UpdateProcessError>(.initial)
+
+    init(robot: DummyRobotModel) {
+        let currentRobotVersion = robot.osVersion
+
+        switch currentRobotVersion {
+            default:
+                self.currentUpdateProcess = UpdateProcessTemplate()
+        }
+
+        self.currentStage = self.currentUpdateProcess.currentStage
+    }
+
+    func startUpdate() {
+        currentUpdateProcess.startProcess()
+    }
+}
