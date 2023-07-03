be115df4af7e24215f07dcd18d67279fec0331ca
Hugo Pezziardi <hugo@pezziardi.net>
2023-07-03 17:17:59 +0200

    :recycle: (UIExplorer): Use robotNeededAlert modifier on XylophoneLayout

--- Apps/LekaActivityUIExplorer/Sources/GameLayoutTemplates/Components/AnswerLayouts/Layouts/Misc/XylophoneLayout.swift
+++ Apps/LekaActivityUIExplorer/Sources/GameLayoutTemplates/Components/AnswerLayouts/Layouts/Misc/XylophoneLayout.swift
@@ -10,54 +10,12 @@ struct XylophoneLayout: View {
     @EnvironmentObject var defaults: GameLayoutTemplatesDefaults
     @StateObject var xylophoneDefaults: XylophoneDefaults = Misc.xylophone
 
-    @State private var showAlert: Bool = false
-
     var body: some View {
         HStack(spacing: xylophoneDefaults.customTilesSpacing) {
             ForEach($xylophoneDefaults.customTileColors, id: \.self) { color in
                 XylophoneTile(color: color)
             }
         }
-        .onAppear {
-            showAlert = true
-        }
-        .alert("Cette activité nécessite l'utilisation du robot !", isPresented: $showAlert) {
-            alertContent
-        } message: {
-            Text("Avant de commencer l'activité, connectez-vous en Bluetooth à votre robot.")
-        }
-    }
-
-    private var alertContent: some View {
-        Group {
-            Button(
-                role: .destructive,
-                action: {
-                    showAlert.toggle()
-                },
-                label: {
-                    Text("Annuler")
-                })
-
-            Button(
-                role: .none,
-                action: {
-                    showAlert.toggle()
-                },
-                label: {
-                    Text("Continuer sans le robot")
-                })
-
-            Button(
-                role: .cancel,
-                action: {
-                    showAlert.toggle()
-                },
-                label: {
-                    Text("Me connecter")
-                        .font(defaults.semi17)
-                        .foregroundColor(.accentColor)
-                })
-        }
+        .robotNeededAlert()
     }
 }
