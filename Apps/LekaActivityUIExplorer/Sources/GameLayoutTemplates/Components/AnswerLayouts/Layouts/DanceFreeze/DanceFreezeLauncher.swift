bb94b5d72b6a14b291bb0ab2b40f5b08191e9a2b
Hugo Pezziardi <hugo@pezziardi.net>
2023-07-03 14:32:57 +0200

    :sparkles: (UIExplorer): Add DanceFreeze Launcher

---
+++ Apps/LekaActivityUIExplorer/Sources/GameLayoutTemplates/Components/AnswerLayouts/Layouts/DanceFreeze/DanceFreezeLauncher.swift
@@ -0,0 +1,32 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import SwiftUI
+
+enum DanceFreezeStage {
+    case waitingForSelection
+    case automaticMode
+    case manualMode
+}
+
+struct DanceFreezeLauncher: View {
+    @State private var mode = DanceFreezeStage.waitingForSelection
+
+    var body: some View {
+        if mode == .waitingForSelection {
+            DanceFreezeSelector(mode: $mode)
+                .robotNeededAlert()
+        } else if mode == .automaticMode {
+            DanceFreezePlayer(isAuto: true)
+        } else {
+            DanceFreezePlayer(isAuto: false)
+        }
+    }
+}
+
+struct DanceFreezeLauncher_Previews: PreviewProvider {
+    static var previews: some View {
+        DanceFreezeLauncher()
+    }
+}
