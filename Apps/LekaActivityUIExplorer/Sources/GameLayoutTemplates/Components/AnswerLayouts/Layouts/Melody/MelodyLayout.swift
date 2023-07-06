fd5b8ac5c6c553c5188e709a72ea1e9f0c536f38
Hugo Pezziardi <hugo@pezziardi.net>
2023-07-06 15:38:42 +0200

    :sparkles: (UIExplorer): Add MelodyLayout

---
+++ Apps/LekaActivityUIExplorer/Sources/GameLayoutTemplates/Components/AnswerLayouts/Layouts/Melody/MelodyLayout.swift
@@ -0,0 +1,36 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import SwiftUI
+
+struct MelodyLayout: View {
+    @EnvironmentObject var gameEngine: GameEngine
+    @EnvironmentObject var defaults: GameLayoutTemplatesDefaults
+    @StateObject var xylophoneDefaults: XylophoneDefaults = Melody.one
+
+    @State private var progress = 0.0
+
+    var body: some View {
+        VStack {
+            ContinuousProgressBar(progress: progress)
+
+            HStack(spacing: xylophoneDefaults.customTilesSpacing) {
+                ForEach(xylophoneDefaults.customTileColors.indices, id: \.self) { index in
+                    XylophoneTile(xylophoneDefaults: xylophoneDefaults, index: index)
+                }
+            }
+            .robotNeededAlert()
+        }
+
+    }
+}
+
+struct MelodyLayout_Previews: PreviewProvider {
+    static var previews: some View {
+        MelodyLayout()
+            .environmentObject(GameEngine())
+            .environmentObject(GameLayoutTemplatesDefaults())
+            .previewInterfaceOrientation(.landscapeLeft)
+    }
+}
