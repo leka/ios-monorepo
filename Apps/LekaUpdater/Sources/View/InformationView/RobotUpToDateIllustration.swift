8cfed860bc4a79da35d1c4a97d88df3f3a3375eb
Yann Locatelli <yann.locatelli2@gmail.com>
2023-07-07 10:36:04 +0200

    :lipstick: (LekaUpdater): Add RobotNeedsUpdateIllustration and RobotUpToDateIllustration

---
+++ Apps/LekaUpdater/Sources/View/InformationView/RobotUpToDateIllustration.swift
@@ -0,0 +1,65 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import DesignKit
+import SwiftUI
+
+struct RobotUpToDateIllustration: View {
+    public var illustrationSize: CGFloat = 300
+
+    private var circleSize: CGFloat {
+        illustrationSize * 250 / 300
+    }
+    private var circleLineWidth: CGFloat {
+        illustrationSize / 60
+    }
+    private var dashSpacer: CGFloat {
+        illustrationSize / 18
+    }
+    private var imageSize: CGFloat {
+        illustrationSize * 180 / 300
+    }
+    private var checkmarkSize: CGFloat {
+        illustrationSize * 56 / 300
+    }
+
+    init(size: CGFloat = 300) {
+        self.illustrationSize = size
+    }
+
+    var body: some View {
+        ZStack {
+            Circle().fill(.white)
+
+            Circle()
+                .strokeBorder(
+                    DesignKitAsset.Colors.lekaUpdaterRobotUpToDate.swiftUIColor, lineWidth: circleLineWidth)
+
+            LekaUpdaterAsset.Assets.robotOnBase.swiftUIImage
+                .resizable()
+                .scaledToFit()
+                .frame(height: imageSize)
+
+            VStack {
+                Spacer()
+
+                ZStack {
+                    Circle().fill(.white)
+                        .frame(height: checkmarkSize)
+
+                    Image(systemName: "checkmark.circle")
+                        .font(.system(size: checkmarkSize))
+                        .foregroundColor(DesignKitAsset.Colors.lekaUpdaterRobotUpToDate.swiftUIColor)
+                }
+            }
+        }
+        .frame(width: circleSize, height: illustrationSize)
+    }
+}
+
+struct RobotUpToDateIllustration_Previews: PreviewProvider {
+    static var previews: some View {
+        RobotUpToDateIllustration(size: 600)
+    }
+}
