faf0ed3ce1563068cce62e5a4c0696de333994e2
Yann Locatelli <yann.locatelli2@gmail.com>
2023-07-18 17:30:41 +0200

    :sparkles: (LekaUpdater): UpdateProcessV100 load firmware

--- Apps/LekaUpdater/Sources/Libs/FirmwareManager.swift
+++ Apps/LekaUpdater/Sources/Libs/FirmwareManager.swift
@@ -13,10 +13,25 @@ class FirmwareManager: ObservableObject {
     // swiftlint:disable:next force_cast
     let currentVersion = Bundle.main.object(forInfoDictionaryKey: "LEKA_OS_VERSION") as! String
 
+    public var data = Data()
+
     func compareWith(version: String) -> RobotUpdateStatus {
         guard version.compare(currentVersion, options: .numeric) == .orderedAscending else {
             return .upToDate
         }
         return .needsUpdate
+    }
+
+    public func load() -> Bool {
+        guard let fileURL = Bundle.main.url(forResource: "LekaOS-\(currentVersion)", withExtension: "bin") else {
+            return false
+        }
+
+        do {
+            data = try Data(contentsOf: fileURL)
+            return true
+        } catch {
+            return false
+        }
     }
 }
