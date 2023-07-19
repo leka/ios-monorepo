59e48d54fbf1f9aa7a7ac45a3a3bf4ed1b83c8ac
Yann Locatelli <yann.locatelli2@gmail.com>
2023-07-19 11:37:29 +0200

    :sparkles: (BLEKit): Add WriteOnlyCharacteristic

---
+++ Modules/BLEKit/Sources/Models/WriteOnlyCharacteristic.swift
@@ -0,0 +1,18 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import CombineCoreBluetooth
+
+public struct WriteOnlyCharacteristic {
+
+    public let characteristicUUID: CBUUID
+    public let serviceUUID: CBUUID
+    public var onWrite: (() -> Void)?
+
+    public init(characteristicUUID: CBUUID, serviceUUID: CBUUID) {
+        self.characteristicUUID = characteristicUUID
+        self.serviceUUID = serviceUUID
+    }
+
+}
