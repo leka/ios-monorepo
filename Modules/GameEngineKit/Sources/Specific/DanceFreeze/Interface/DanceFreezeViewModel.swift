8228a85ef7bb8866a90762654c83c576aadd94a8
Hugo Pezziardi <hugo@pezziardi.net>
2023-07-20 07:00:12 +0200

    :sparkles: (GameEngineKit): Add DanceFreezeViewModel

---
+++ Modules/GameEngineKit/Sources/Specific/DanceFreeze/Interface/DanceFreezeViewModel.swift
@@ -0,0 +1,44 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Combine
+import SwiftUI
+
+public class DanceFreezeViewModel: Identifiable, ObservableObject {
+    public let name = "Dance Freeze"
+    private var gameplay: GameplayPlayMusic
+
+    @Published public var progress: CGFloat = 0.0
+    @Published public var isFinished = false
+
+    private var cancellables: Set<AnyCancellable> = []
+
+    public init(gameplay: GameplayPlayMusic) {
+        self.gameplay = gameplay
+
+        self.gameplay.progressPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] in
+                guard let self = self else { return }
+                self.progress = $0
+            }
+            .store(in: &cancellables)
+
+        self.gameplay.isFinishedPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] in
+                guard let self = self else { return }
+                self.isFinished = $0
+            }
+            .store(in: &cancellables)
+    }
+
+    func onDanceFreezeToggle() {
+        gameplay.process()
+    }
+
+    func setSong(song: SongModel) {
+        gameplay.setAudioPlayer(song: song)
+    }
+}
