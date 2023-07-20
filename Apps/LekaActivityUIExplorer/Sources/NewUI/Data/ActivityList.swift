ba3f6c1ef017a0e9c40c0578f248d2d3ec376136
Ladislas de Toldi <ladislas@detoldi.me>
2023-07-20 18:34:22 +0200

    :pencil2: (UIExplorer): Fix typos for test activities

--- Apps/LekaActivityUIExplorer/Sources/NewUI/Data/ActivityList.swift
+++ Apps/LekaActivityUIExplorer/Sources/NewUI/Data/ActivityList.swift
@@ -6,12 +6,12 @@ import SwiftUI
 
 let kListOfAvailablesActivities: [ActivityModel] = [
     ActivityModel(
-        title: "Text Activity #1",
-        instructions: "This is just another test",
+        title: "Test Activity #1",
+        instructions: "This is a test",
         view: AnyView(TestActivity())
     ),
     ActivityModel(
-        title: "Text Activity #2",
+        title: "Test Activity #2",
         instructions: "This is just another test",
         view: AnyView(TestActivity())
     ),
