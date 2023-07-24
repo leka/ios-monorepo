19746974a03aa3a85a961d1b18def2c25fc35118
Hugo Pezziardi <hugo@pezziardi.net>
2023-07-24 13:29:27 +0200

    :sparkles: (UIExplorer): Add RemoteStandard activity

---
+++ Apps/LekaActivityUIExplorer/Sources/NewUI/Activities/RemoteStandardActivity.swift
@@ -0,0 +1,18 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import GameEngineKit
+import SwiftUI
+
+struct RemoteStandardActivity: View {
+    var body: some View {
+        RemoteStandardView()
+    }
+}
+
+struct RemoteStandardActivity_Previews: PreviewProvider {
+    static var previews: some View {
+        RemoteStandardActivity()
+    }
+}
