77997dbeea3c1928f7ea6f9f1b5d79deae6eea71
Hugo Pezziardi <hugo@pezziardi.net>
2023-07-27 20:05:10 +0200

    :recycle: (GameEngineKit): Refactor GameEngine into StepManager

---
+++ Modules/GameEngineKit/Sources/StepManager.swift
@@ -0,0 +1,44 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Combine
+import Foundation
+
+public enum GameplayType {
+    case selectTheRightAnswer
+    case selectAllRightAnswers
+    case selectSomeRightAnswers
+}
+
+public class StepManager {
+    public var stepModel: StandardStepModel
+    public var gameplay: any GameplayProtocol
+    public var state = CurrentValueSubject<GameplayState, Never>(.idle)
+
+    var cancellables = Set<AnyCancellable>()
+
+    public init(stepModel: StandardStepModel, state: GameplayState = .idle) {
+        self.stepModel = stepModel
+        self.gameplay = StepManager.gameplaySelector(stepModel: stepModel)
+        self.gameplay.state
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { [weak self] in
+                guard let self = self else { return }
+                self.state.send($0)
+            })
+            .store(in: &cancellables)
+    }
+
+    public static func gameplaySelector(stepModel: StandardStepModel) -> any GameplayProtocol {
+        switch stepModel.gameplay {
+            case .selectTheRightAnswer:
+                return GameplaySelectTheRightAnswer(choices: stepModel.choices)
+            case .selectAllRightAnswers:
+                return GameplaySelectAllRightAnswers(choices: stepModel.choices)
+            case .selectSomeRightAnswers:
+                return GameplaySelectSomeRightAnswers(
+                    choices: stepModel.choices, rightAnswersToFind: stepModel.answersNumber!)
+        }
+    }
+}
