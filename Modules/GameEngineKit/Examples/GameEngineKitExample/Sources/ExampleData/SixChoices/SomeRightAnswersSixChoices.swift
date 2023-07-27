f359633b8a77a19504ef9666367d2a22df15b650
Hugo Pezziardi <hugo@pezziardi.net>
2023-07-27 20:05:51 +0200

    :sparkles: (GameEngineKitExample): Refactor Components into 6 ActivityView files

---
+++ Modules/GameEngineKit/Examples/GameEngineKitExample/Sources/ExampleData/SixChoices/SomeRightAnswersSixChoices.swift
@@ -0,0 +1,67 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import GameEngineKit
+import SwiftUI
+
+private let choicesStep1 = [
+    ChoiceViewModel(item: "cow", type: .text, rightAnswer: true),
+    ChoiceViewModel(item: "sheep", type: .text),
+    ChoiceViewModel(item: "cow", type: .text, rightAnswer: true),
+    ChoiceViewModel(item: "horse", type: .text),
+    ChoiceViewModel(item: "cow", type: .text, rightAnswer: true),
+    ChoiceViewModel(item: "kangaroo", type: .text),
+]
+
+private let choicesStep2 = [
+    ChoiceViewModel(item: "chinchilla", type: .text, rightAnswer: true),
+    ChoiceViewModel(item: "frog", type: .text),
+    ChoiceViewModel(item: "chinchilla", type: .text, rightAnswer: true),
+    ChoiceViewModel(item: "cow", type: .text),
+    ChoiceViewModel(item: "bird", type: .text),
+    ChoiceViewModel(item: "horse", type: .text),
+]
+
+private let choicesStep3 = [
+    ChoiceViewModel(item: "dog", type: .text, rightAnswer: true),
+    ChoiceViewModel(item: "dog", type: .text, rightAnswer: true),
+    ChoiceViewModel(item: "lama", type: .text),
+    ChoiceViewModel(item: "dog", type: .text, rightAnswer: true),
+    ChoiceViewModel(item: "cat", type: .text),
+    ChoiceViewModel(item: "dog", type: .text, rightAnswer: true),
+]
+
+private let choicesStep4 = [
+    ChoiceViewModel(item: "frog", type: .text),
+    ChoiceViewModel(item: "panda", type: .text, rightAnswer: true),
+    ChoiceViewModel(item: "bird", type: .text),
+    ChoiceViewModel(item: "cow", type: .text),
+    ChoiceViewModel(item: "sheep", type: .text),
+    ChoiceViewModel(item: "horse", type: .text, rightAnswer: true),
+]
+
+private let choicesStep5 = [
+    ChoiceViewModel(item: "sheep", type: .text, rightAnswer: true),
+    ChoiceViewModel(item: "cat", type: .text),
+    ChoiceViewModel(item: "horse", type: .text),
+    ChoiceViewModel(item: "cow", type: .text),
+    ChoiceViewModel(item: "sheep", type: .text, rightAnswer: true),
+    ChoiceViewModel(item: "horse", type: .text),
+]
+
+private var steps: [StandardStepModel] = [
+    StandardStepModel(choices: choicesStep1, gameplay: .selectSomeRightAnswers(2), interface: .sixChoices),
+    StandardStepModel(choices: choicesStep2, gameplay: .selectSomeRightAnswers(1), interface: .sixChoices),
+    StandardStepModel(choices: choicesStep3, gameplay: .selectSomeRightAnswers(3), interface: .sixChoices),
+    StandardStepModel(choices: choicesStep4, gameplay: .selectSomeRightAnswers(1), interface: .sixChoices),
+    StandardStepModel(choices: choicesStep5, gameplay: .selectSomeRightAnswers(2), interface: .sixChoices),
+]
+
+struct SomeAnswersSixChoicesActivity: View {
+    @ObservedObject private var stepManager = StepManager(steps: steps)
+
+    var body: some View {
+        stepManager.interface
+    }
+}
---
+++ Modules/GameEngineKit/Examples/GameEngineKitExample/Sources/ExampleData/ThreeChoices/AllRightAnswersThreeChoices.swift
@@ -0,0 +1,52 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import GameEngineKit
+import SwiftUI
+
+private let choicesStep1 = [
+    ChoiceViewModel(item: "red", type: .color, rightAnswer: true),
+    ChoiceViewModel(item: "red", type: .color, rightAnswer: true),
+    ChoiceViewModel(item: "yellow", type: .color),
+]
+
+private let choicesStep2 = [
+    ChoiceViewModel(item: "blue", type: .color, rightAnswer: true),
+    ChoiceViewModel(item: "green", type: .color),
+    ChoiceViewModel(item: "blue", type: .color, rightAnswer: true),
+]
+
+private let choicesStep3 = [
+    ChoiceViewModel(item: "yellow", type: .color, rightAnswer: true),
+    ChoiceViewModel(item: "yellow", type: .color, rightAnswer: true),
+    ChoiceViewModel(item: "red", type: .color),
+]
+
+private let choicesStep4 = [
+    ChoiceViewModel(item: "green", type: .color),
+    ChoiceViewModel(item: "pink", type: .color, rightAnswer: true),
+    ChoiceViewModel(item: "pink", type: .color, rightAnswer: true),
+]
+
+private let choicesStep5 = [
+    ChoiceViewModel(item: "red", type: .color, rightAnswer: true),
+    ChoiceViewModel(item: "blue", type: .color),
+    ChoiceViewModel(item: "red", type: .color, rightAnswer: true),
+]
+
+private var steps: [StandardStepModel] = [
+    StandardStepModel(choices: choicesStep1, gameplay: .selectAllRightAnswers, interface: .threeChoices),
+    StandardStepModel(choices: choicesStep2, gameplay: .selectAllRightAnswers, interface: .threeChoices),
+    StandardStepModel(choices: choicesStep3, gameplay: .selectAllRightAnswers, interface: .threeChoices),
+    StandardStepModel(choices: choicesStep4, gameplay: .selectAllRightAnswers, interface: .threeChoices),
+    StandardStepModel(choices: choicesStep5, gameplay: .selectAllRightAnswers, interface: .threeChoices),
+]
+
+struct AllAnswersThreeChoicesActivity: View {
+    @ObservedObject private var stepManager = StepManager(steps: steps)
+
+    var body: some View {
+        stepManager.interface
+    }
+}
