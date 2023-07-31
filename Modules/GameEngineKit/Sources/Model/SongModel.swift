160cc3951f8d68fa63f2ea1b2da917fb37630ca0
Hugo Pezziardi <hugo@pezziardi.net>
2023-07-31 19:30:02 +0200

    :truck: (GameEngineKit): Move SongModel struct in its own file

---
+++ Modules/GameEngineKit/Sources/Model/SongModel.swift
@@ -0,0 +1,11 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Foundation
+
+public struct SongModel: Hashable, Equatable {
+    let id: UUID = UUID()
+    let name: String
+    let file: String
+}
