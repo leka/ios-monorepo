d7a46ebb91f79cfa6139ccccb8427866a659aa08
Hugo Pezziardi <hugo@pezziardi.net>
2023-08-02 17:37:10 +0200

    :sparkles: (GameEngineKit): Add ThreeChoices view & vm

---
+++ Modules/GameEngineKit/Sources/Interface/Layouts/TouchToSelect/ThreeChoices/ThreeChoicesView.swift
@@ -0,0 +1,43 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import SwiftUI
+
+public struct ThreeChoicesView: View {
+    @ObservedObject private var viewModel: ThreeChoicesViewModel
+    let horizontalSpacing: CGFloat = 32
+    let verticalSpacing: CGFloat = 32
+    let answerSize: CGFloat = 260
+
+    public init(gameplay: any GameplayProtocol) {
+        self.viewModel = ThreeChoicesViewModel(gameplay: gameplay)
+    }
+
+    public var body: some View {
+        Grid(
+            horizontalSpacing: horizontalSpacing,
+            verticalSpacing: verticalSpacing
+        ) {
+            GridRow {
+                ChoiceView(choice: viewModel.choices[0], size: answerSize)
+                    .onTapGesture {
+                        viewModel.onChoiceTapped(choice: viewModel.choices[0])
+                    }
+                Color.clear.gridCellUnsizedAxes([.horizontal, .vertical])
+                ChoiceView(choice: viewModel.choices[1], size: answerSize)
+                    .onTapGesture {
+                        viewModel.onChoiceTapped(choice: viewModel.choices[1])
+                    }
+            }
+            GridRow {
+                Color.clear.gridCellUnsizedAxes([.horizontal, .vertical])
+                ChoiceView(choice: viewModel.choices[2], size: answerSize)
+                    .onTapGesture {
+                        viewModel.onChoiceTapped(choice: viewModel.choices[2])
+                    }
+                Color.clear.gridCellUnsizedAxes([.horizontal, .vertical])
+            }
+        }
+    }
+}
