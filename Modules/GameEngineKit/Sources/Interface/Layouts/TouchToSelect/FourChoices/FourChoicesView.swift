186d76f9fae698c2a9fd521ff754e14f02282a2c
Hugo Pezziardi <hugo@pezziardi.net>
2023-08-02 17:37:35 +0200

    :sparkles: (GameEngineKit): Add FourChoices & Inline view & vm

---
+++ Modules/GameEngineKit/Sources/Interface/Layouts/TouchToSelect/FourChoices/FourChoicesView.swift
@@ -0,0 +1,44 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import SwiftUI
+
+public struct FourChoicesView: View {
+    @ObservedObject private var viewModel: FourChoicesViewModel
+	let horizontalSpacing: CGFloat = 200
+	let verticalSpacing: CGFloat = 40
+	let answerSize: CGFloat = 240
+
+    public init(gameplay: any GameplayProtocol) {
+        self.viewModel = FourChoicesViewModel(gameplay: gameplay)
+    }
+
+    public var body: some View {
+        Grid(
+            horizontalSpacing: horizontalSpacing,
+            verticalSpacing: verticalSpacing
+        ) {
+            GridRow {
+                ForEach(0..<2) { index in
+                    let choice = viewModel.choices[index]
+
+                    ChoiceView(choice: choice, size: answerSize)
+                        .onTapGesture {
+                            viewModel.onChoiceTapped(choice: choice)
+                        }
+                }
+            }
+            GridRow {
+                ForEach(2..<4) { index in
+                    let choice = viewModel.choices[index]
+
+                    ChoiceView(choice: choice, size: answerSize)
+                        .onTapGesture {
+                            viewModel.onChoiceTapped(choice: choice)
+                        }
+                }
+            }
+        }
+    }
+}
