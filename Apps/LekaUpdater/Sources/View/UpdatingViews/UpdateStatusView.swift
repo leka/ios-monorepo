93a28813375b89d8d451ccd5934c6efe0304efc5
Yann Locatelli <yann.locatelli2@gmail.com>
2023-08-04 17:30:20 +0200

    :lipstick: (LekaUpdater): Move UpdateStatusView in NavigationStack to get title back

--- Apps/LekaUpdater/Sources/View/UpdatingViews/UpdateStatusView.swift
+++ Apps/LekaUpdater/Sources/View/UpdatingViews/UpdateStatusView.swift
@@ -9,69 +9,70 @@ struct UpdateStatusView: View {
     @StateObject private var viewModel = UpdateStatusViewModel()
 
     var body: some View {
-        VStack {
-            Spacer()
-            Spacer()
-
+        NavigationStack {
             VStack {
-                switch viewModel.updatingStatus {
-                    case .sendingFile:
-                        SendingFileIllustration()
-                    case .rebootingRobot:
-                        RebootingIllustration()
-                    case .updateFinished:
-                        UpdateFinishedIllustration()
-                }
-            }
-            .frame(height: 250)
-            .padding(.bottom)
-            .padding(.bottom)
-
-            Text("Étape \(viewModel.stepNumber)/3")
-                .font(.title)
-                .bold()
-                .monospacedDigit()
-                .padding()
-
-            VStack {
-                switch viewModel.updatingStatus {
-                    case .sendingFile:
-                        SendingFileContentView()
-                    case .rebootingRobot:
-                        RebootingContentView()
-                    case .updateFinished:
-                        UpdateFinishedContentView()
-                }
                 Spacer()
-            }
-            .frame(maxWidth: .infinity, maxHeight: 250)
+                Spacer()
 
-            Spacer()
-
-            LekaUpdaterAsset.Assets.lekaUpdaterIcon.swiftUIImage
-                .resizable()
-                .scaledToFit()
-                .frame(height: 70)
-                .padding(35)
-
-        }
-        .foregroundColor(DesignKitAsset.Colors.darkGray.swiftUIColor)
-        .onAppear(perform: viewModel.startUpdate)
-        .navigationBarBackButtonHidden()
-        .toolbar {
-            ToolbarItem(placement: .principal) {
                 VStack {
-                    Text("Leka Updater")
-                        .font(.title2)
-                        .bold()
-                    Text("L'application pour mettre à jour vos robots Leka !")
+                    switch viewModel.updatingStatus {
+                        case .sendingFile:
+                            SendingFileIllustration()
+                        case .rebootingRobot:
+                            RebootingIllustration()
+                        case .updateFinished:
+                            UpdateFinishedIllustration()
+                    }
                 }
-                .foregroundColor(.accentColor)
-            }
+                .frame(height: 250)
+                .padding(.bottom)
+                .padding(.bottom)
 
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button("[DEBUG] Switch views", action: switchView)
-            }  // TODO(@yann): remove when debug is over
+                Text("Étape \(viewModel.stepNumber)/3")
+                    .font(.title)
+                    .bold()
+                    .monospacedDigit()
+                    .padding()
+
+                VStack {
+                    switch viewModel.updatingStatus {
+                        case .sendingFile:
+                            SendingFileContentView()
+                        case .rebootingRobot:
+                            RebootingContentView()
+                        case .updateFinished:
+                            UpdateFinishedContentView()
+                    }
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity, maxHeight: 250)
+
+                Spacer()
+
+                LekaUpdaterAsset.Assets.lekaUpdaterIcon.swiftUIImage
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 70)
+                    .padding(35)
+
+            }
+            .foregroundColor(DesignKitAsset.Colors.darkGray.swiftUIColor)
+            .onAppear(perform: viewModel.startUpdate)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    VStack {
+                        Text("Leka Updater")
+                            .font(.title2)
+                            .bold()
+                        Text("L'application pour mettre à jour vos robots Leka !")
+                    }
+                    .foregroundColor(.accentColor)
+                }
+
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("[DEBUG] Switch views", action: switchView)
+                }  // TODO(@yann): remove when debug is over
+            }
         }
     }
 
