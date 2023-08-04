dbc6b600a5551c8e1742ddcd2a4e5e79cf59c87d
Yann Locatelli <yann.locatelli2@gmail.com>
2023-08-04 16:56:35 +0200

    :lipstick: (LekaUpdater): Move InformationView in NavigationStack to get title back

--- Apps/LekaUpdater/Sources/View/InformationView/InformationView.swift
+++ Apps/LekaUpdater/Sources/View/InformationView/InformationView.swift
@@ -9,92 +9,94 @@ struct InformationView: View {
     @StateObject var viewModel = InformationViewModel()
 
     var body: some View {
-        VStack {
-            Form {
-                Section {
-                    Group {
-                        if viewModel.showRobotNeedsUpdate {
-                            RobotNeedsUpdateIllustration(size: 200)
-
-                            Text(viewModel.robotName)
-                                .font(.title3)
-
-                            Text("⬆️ Une mise à jour est disponible 📦")
-                                .font(.title2)
-                        } else {
-                            RobotUpToDateIllustration(size: 200)
-
-                            Text(viewModel.robotName)
-                                .font(.title3)
-
-                            Text("🤖 Votre robot est à jour ! 🎉 Vous n'avez rien à faire 👌")
-                                .font(.title2)
-                        }
-                    }
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .listRowSeparator(.hidden)
-                }
-                .listRowBackground(Color.clear)
-
-                Section {
-                    RobotInformationView()
-                } header: {
-                    Text("Informations du robot")
-                        .textCase(nil)
-                        .font(.title)
-                }
-
-                Section {
-                    DisclosureGroup {
-                        ChangelogView()
-                            .padding()
-                    } label: {
-                        Text("Liste des changements apportés")
-                            .foregroundStyle(DesignKitAsset.Colors.lekaSkyBlue.swiftUIColor)
-                    }
-                    .accentColor(DesignKitAsset.Colors.lekaSkyBlue.swiftUIColor)
-                } header: {
-                    Text("Informations sur LekaOS v\(viewModel.firmwareVersion)")
-                        .textCase(nil)
-                        .font(.title)
-                }
-
-                if viewModel.showRobotNeedsUpdate {
+        NavigationStack {
+            VStack {
+                Form {
                     Section {
-                        RobotUpdateAvailableView()
+                        Group {
+                            if viewModel.showRobotNeedsUpdate {
+                                RobotNeedsUpdateIllustration(size: 200)
+
+                                Text(viewModel.robotName)
+                                    .font(.title3)
+
+                                Text("⬆️ Une mise à jour est disponible 📦")
+                                    .font(.title2)
+                            } else {
+                                RobotUpToDateIllustration(size: 200)
+
+                                Text(viewModel.robotName)
+                                    .font(.title3)
+
+                                Text("🤖 Votre robot est à jour ! 🎉 Vous n'avez rien à faire 👌")
+                                    .font(.title2)
+                            }
+                        }
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .listRowSeparator(.hidden)
+                    }
+                    .listRowBackground(Color.clear)
+
+                    Section {
+                        RobotInformationView()
                     } header: {
-                        Text("État de mise à jour du robot")
+                        Text("Informations du robot")
                             .textCase(nil)
                             .font(.title)
                     }
-                }
 
-                Section {
-                    VStack {
-                        LekaUpdaterAsset.Assets.lekaUpdaterIcon.swiftUIImage
-                            .resizable()
-                            .scaledToFit()
-                            .frame(height: 70)
-                            .padding(35)
-
-                        Button("Switch (debug)", action: viewModel.switchRobotVersionForDebug)  // TODO: Remove DEBUG
+                    Section {
+                        DisclosureGroup {
+                            ChangelogView()
+                                .padding()
+                        } label: {
+                            Text("Liste des changements apportés")
+                                .foregroundStyle(DesignKitAsset.Colors.lekaSkyBlue.swiftUIColor)
+                        }
+                        .accentColor(DesignKitAsset.Colors.lekaSkyBlue.swiftUIColor)
+                    } header: {
+                        Text("Informations sur LekaOS v\(viewModel.firmwareVersion)")
+                            .textCase(nil)
+                            .font(.title)
                     }
-                    .frame(maxWidth: .infinity, alignment: .center)
+
+                    if viewModel.showRobotNeedsUpdate {
+                        Section {
+                            RobotUpdateAvailableView()
+                        } header: {
+                            Text("État de mise à jour du robot")
+                                .textCase(nil)
+                                .font(.title)
+                        }
+                    }
+
+                    Section {
+                        VStack {
+                            LekaUpdaterAsset.Assets.lekaUpdaterIcon.swiftUIImage
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 70)
+                                .padding(35)
+
+                            Button("Switch (debug)", action: viewModel.switchRobotVersionForDebug)  // TODO: Remove DEBUG
+                        }
+                        .frame(maxWidth: .infinity, alignment: .center)
+                    }
+                    .listRowBackground(Color.clear)
                 }
-                .listRowBackground(Color.clear)
             }
-        }
-        .foregroundColor(DesignKitAsset.Colors.darkGray.swiftUIColor)
-        .onAppear(perform: viewModel.onAppear)
-        .toolbar {
-            ToolbarItem(placement: .principal) {
-                VStack {
-                    Text("Leka Updater")
-                        .font(.title2)
-                        .bold()
-                    Text("L'application pour mettre à jour vos robots Leka !")
+            .foregroundColor(DesignKitAsset.Colors.darkGray.swiftUIColor)
+            .onAppear(perform: viewModel.onAppear)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    VStack {
+                        Text("Leka Updater")
+                            .font(.title2)
+                            .bold()
+                        Text("L'application pour mettre à jour vos robots Leka !")
+                    }
+                    .foregroundColor(.accentColor)
                 }
-                .foregroundColor(.accentColor)
             }
         }
     }
@@ -102,14 +104,12 @@ struct InformationView: View {
 
 struct InformationView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationStack {
-            InformationView()
-                .onAppear {
-                    globalRobotManager.name = "Leka"
-                    globalRobotManager.battery = 75
-                    globalRobotManager.isCharging = true
-                    globalRobotManager.osVersion = "1.3.0"
-                }
-        }
+        InformationView()
+            .onAppear {
+                globalRobotManager.name = "Leka"
+                globalRobotManager.battery = 75
+                globalRobotManager.isCharging = true
+                globalRobotManager.osVersion = "1.3.0"
+            }
     }
 }
