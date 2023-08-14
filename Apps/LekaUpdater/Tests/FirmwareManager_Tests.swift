b3a546b763152ef914998ebda8b1b51df4d17305
Yann Locatelli <yann.locatelli2@gmail.com>
2023-08-14 11:49:20 +0200

    :sparkles: (LekaUpdater): Add sha256 in FirmwareManager

--- Apps/LekaUpdater/Tests/FirmwareManager_Tests.swift
+++ Apps/LekaUpdater/Tests/FirmwareManager_Tests.swift
@@ -34,7 +34,7 @@ final class FirmwareManager_Tests_compareVersion: XCTestCase {
         let actual = firmwareManager.compareWith(version: robotVersion)
         XCTAssertEqual(expected, actual)
     }
-    
+
     func test_shouldReturnRobotNeedUpdate_invalidVersion() {
         let firmwareManager = FirmwareManager()
         let robotVersion = "⚠️ NO OS VERSION"
@@ -42,6 +42,20 @@ final class FirmwareManager_Tests_compareVersion: XCTestCase {
         let expected = RobotUpdateStatus.needsUpdate
         let actual = firmwareManager.compareWith(version: robotVersion)
         XCTAssertEqual(expected, actual)
+    }
+
+}
+
+final class FirmwareManager_Tests_sha256: XCTestCase {
+
+    func test_shouldReturnRobotNeedUpdate_invalidVersion() {
+        let firmwareManager = FirmwareManager()
+        _ = firmwareManager.load()
+
+        let expectedSHA256 = "e061ee13041fe73667e4e9ca8f84189fb4cbc8f3d8710f8841fb41cf0df9e1e1"  // shasum -a 256 LekaOS-1.4.0.bin
+        let actualSHA256 = firmwareManager.sha256
+
+        XCTAssertEqual(expectedSHA256, actualSHA256)
     }
 
 }
