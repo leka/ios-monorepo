43225983313f6012c00738769fafa03c21621ba5
Hugo Pezziardi <hugo@pezziardi.net>
2023-08-14 15:42:37 +0200

    :sparkles: (UIExplorer): Add Xylophone activity

---
+++ Apps/LekaActivityUIExplorer/Sources/NewUI/Activities/XylophoneActivity.swift
@@ -0,0 +1,19 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import GameEngineKit
+import SwiftUI
+
+struct XylophoneActivity: View {
+    var body: some View {
+        XylophoneView()
+    }
+}
+
+struct XylophoneActivity_Previews: PreviewProvider {
+    static var previews: some View {
+        XylophoneActivity()
+            .previewInterfaceOrientation(.landscapeLeft)
+    }
+}
