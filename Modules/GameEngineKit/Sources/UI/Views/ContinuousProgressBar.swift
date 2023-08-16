845ddd5440709f09d2ba97a9fb250ab0b26484e0
Hugo Pezziardi <hugo@pezziardi.net>
2023-08-16 12:45:20 +0200

    :lipstick: (GameEngineKit): Add padding to ContinuousProgressBar

--- Modules/GameEngineKit/Sources/UI/Views/ContinuousProgressBar.swift
+++ Modules/GameEngineKit/Sources/UI/Views/ContinuousProgressBar.swift
@@ -2,8 +2,8 @@
 // Copyright 2023 APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
-import SwiftUI
 import DesignKit
+import SwiftUI
 
 struct ContinuousProgressBar: View {
     var progress: CGFloat
@@ -11,7 +11,7 @@ struct ContinuousProgressBar: View {
     var body: some View {
         GeometryReader { geometry in
             Capsule()
-				.fill(DesignKitAsset.Colors.progressBar.swiftUIColor)
+                .fill(DesignKitAsset.Colors.progressBar.swiftUIColor)
                 .frame(height: 30)
                 .frame(width: geometry.size.width)
                 .overlay(alignment: .leading) {
@@ -23,5 +23,6 @@ struct ContinuousProgressBar: View {
                 .position(x: geometry.frame(in: .local).midX, y: geometry.frame(in: .local).midY)
         }
         .frame(maxWidth: 1100, maxHeight: 100)
+        .padding(20)
     }
 }
