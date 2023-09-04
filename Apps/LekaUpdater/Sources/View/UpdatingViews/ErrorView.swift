5bf294d0c68818bb52ec56ae09fdf07dd1843acf
Yann Locatelli <yann.locatelli2@gmail.com>
2023-09-04 14:40:40 +0200

    :children_crossing: (LekaUpdater): Go back to connectionView from errorView

--- Apps/LekaUpdater/Sources/View/UpdatingViews/ErrorView.swift
+++ Apps/LekaUpdater/Sources/View/UpdatingViews/ErrorView.swift
@@ -15,9 +15,12 @@ struct ErrorIllustration: View {
 }
 
 struct ErrorContentView: View {
+    @Environment(\.dismiss) var dismiss
 
     public let errorDescription: String
     public let errorInstruction: String
+
+    @Binding var isConnectionViewPresented: Bool
 
     var body: some View {
         VStack {
@@ -28,11 +31,31 @@ struct ErrorContentView: View {
             Text(errorInstruction)
                 .multilineTextAlignment(.center)
                 .padding()
+
+            HStack {
+                Button {
+                    dismiss()
+                    isConnectionViewPresented = true
+                } label: {
+                    Text("Revenir à la page de connexion")
+                        .padding(.horizontal)
+                        .foregroundColor(.white)
+                        .frame(height: 50)
+                        .background(DesignKitAsset.Colors.lekaSkyBlue.swiftUIColor)
+                        .cornerRadius(10)
+                }
+                .buttonStyle(.plain)
+                .padding(.leading)
+            }
+            .shadow(radius: 3, y: 4)
+            .padding()
         }
     }
 }
 
 struct ErrorView_Previews: PreviewProvider {
+
+    @State static var isConnectionViewPresented = false
 
     static let errorDescription = "Erreur inconnue"
     static let errorActionRequired = "Contacter le support technique"
@@ -51,7 +74,9 @@ struct ErrorView_Previews: PreviewProvider {
                 .padding()
 
             VStack {
-                ErrorContentView(errorDescription: errorDescription, errorInstruction: errorActionRequired)
+                ErrorContentView(
+                    errorDescription: errorDescription, errorInstruction: errorActionRequired,
+                    isConnectionViewPresented: $isConnectionViewPresented)
                 Spacer()
             }
             .foregroundColor(DesignKitAsset.Colors.darkGray.swiftUIColor)
