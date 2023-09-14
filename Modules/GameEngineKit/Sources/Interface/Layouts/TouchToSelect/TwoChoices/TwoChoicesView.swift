f80bc1909863067ec42f08ffc0e6069a292d2c40
Hugo Pezziardi <hugo@pezziardi.net>
2023-09-14 14:45:10 +0200

    :lipstick: (GameEngineKit): Adapt spacing or size of interfaces to fit

--- Modules/GameEngineKit/Sources/Interface/Layouts/TouchToSelect/TwoChoices/TwoChoicesView.swift
+++ Modules/GameEngineKit/Sources/Interface/Layouts/TouchToSelect/TwoChoices/TwoChoicesView.swift
@@ -6,7 +6,7 @@ import SwiftUI
 
 public struct TwoChoicesView: View {
     @ObservedObject private var viewModel: GenericViewModel
-    let horizontalSpacing: CGFloat = 60
+    let horizontalSpacing: CGFloat = 150
     let answerSize: CGFloat = 300
 
     public init(gameplay: any GameplayProtocol) {
