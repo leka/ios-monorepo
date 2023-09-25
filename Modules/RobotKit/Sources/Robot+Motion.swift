5c6f93891ab6de9b0b74c977ef0fe63e95ed9769
Ladislas de Toldi <ladislas@detoldi.me>
2023-09-25 14:29:30 +0200

    :sparkles: (RobotKit): Add first (empty) implementation of Robot class
    This commit defines the public interface of the Robot class
    
    Methods don't do anything except from printing to the console. Detailed
    implementation will follow

---
+++ Modules/RobotKit/Sources/Robot+Motion.swift
@@ -0,0 +1,21 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+extension Robot {
+
+    public enum Direction {
+        case forward
+        case forwardLeft
+        case forwardRight
+        case backward
+        case backwardLeft
+        case backwardRight
+    }
+
+    public enum Rotation {
+        case clockwise
+        case counterclockwise
+    }
+
+}
--- Modules/RobotKit/Sources/Robot.swift
+++ Modules/RobotKit/Sources/Robot.swift
@@ -2,14 +2,63 @@
 // Copyright 2023 APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
-import CombineCoreBluetooth
+import Combine
 
 public class Robot {
 
-    // MARK: - Public variables
+    // MARK: - General
 
-    // MARK: - Private variables
+    public func stop() {
+        print("🤖 STOP 🛑 - Everything")
+    }
 
-    // MARK: - Public functions
+    public func reboot() {
+        print("🤖 REBOOT 💫")
+    }
+
+    // MARK: - Motion
+
+    public func move(_ direction: Direction, speed: Float) {
+        print("🤖 MOVE \(direction) at \(speed)")
+    }
+
+    public func move(left speedLeft: Float, right speedRight: Float) {
+        print("🤖 MOVE left motor at \(speedLeft) and right motor at \(speedRight)")
+    }
+
+    public func spin(_ rotation: Rotation, speed: Float) {
+        print("🤖 SPIN \(rotation) at \(speed)")
+    }
+
+    public func stopMotion() {
+        print("🤖 STOP 🛑 - Motion")
+    }
+
+    // MARK: - Lights
+
+    public func shine(_ lights: Lights, color: Color) {
+        print("🤖 SHINE \(lights) in \(color)")
+    }
+
+    public func blacken(_ lights: Lights) {
+        print("🤖 BLACKEN \(lights)")
+    }
+
+    public func stopLights() {
+        print("🤖 STOP 🛑 - Lights")
+    }
+
+    // MARK: - Reinforcers
+
+    public func run(_ reinforcer: Reinforcer) {
+        print("🤖 RUN reinforcer \(reinforcer)")
+    }
+
+    // MARK: - Magic Cards
+
+    public func onMagicCard() -> AnyPublisher<MagicCard, Never> {
+        Just(MagicCard.dice_roll)
+            .eraseToAnyPublisher()
+    }
 
 }
