db7cf9737526968ee9ed94607829572155258e9d
Ladislas de Toldi <ladislas@detoldi.me>
2023-10-12 00:25:00 +0200

    :sparkles: (RobotKit): Lights - implement all lights use cases

--- Modules/RobotKit/Sources/Robot+Lights.swift
+++ Modules/RobotKit/Sources/Robot+Lights.swift
@@ -2,21 +2,39 @@
 // Copyright 2023 APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
-// swiftlint:disable identifier_name
+// swiftlint:disable identifier_name nesting cyclomatic_complexity
 
 extension Robot {
 
     public enum Lights {
 
-        public enum Position: UInt8 {
-            case ears = 0x11
-            case belt = 0x12
+        public enum Spot {
+            static let id: UInt8 = 0x10
+            public enum Position: UInt8 {
+                case ears = 0x11
+                case belt = 0x12
+            }
         }
 
-        static let id: UInt8 = 0x10
+        public enum Full {
+            static let id: UInt8 = 0x13
+            public enum Position: UInt8 {
+                case ears = 0x14
+                case belt = 0x15
+            }
+        }
 
-        case belt
-        case ears
+        public enum Range {
+            static let id: UInt8 = 0x16
+            public enum Position: UInt8 {
+                case ears = 0x17
+                case belt = 0x18
+            }
+        }
+
+        case all
+
+        case full(_ position: Full.Position)
 
         case halfLeft
         case halfRight
@@ -28,14 +46,14 @@ extension Robot {
         case earLeft
         case earRight
 
-        case spot(Position, ids: [UInt8])
-        case range(start: Int, end: Int)
+        case spot(Spot.Position, ids: [UInt8])
+        case range(start: UInt8, end: UInt8)
 
-        static func spot(on position: Position, ids: UInt8...) -> Self {
+        static func spot(on position: Spot.Position, ids: UInt8...) -> Self {
             .spot(position, ids: ids)
         }
 
-        static func range(startId: Int, endId: Int) -> Self {
+        static func range(startId: UInt8, endId: UInt8) -> Self {
             .range(start: startId, end: endId)
         }
 
@@ -43,20 +61,75 @@ extension Robot {
             var output: [[UInt8]] = [[]]
 
             switch self {
+                case .all:
+                    let ears = shineFull(.ears, in: color)
+                    let belt = shineFull(.belt, in: color)
+                    output.append(contentsOf: [ears, belt])
+
                 case .spot(_: let position, let ids):
                     for id in ids {
-                        let payload = shineSpot(id, in: color, on: position)
+                        let payload = shineSpot(id, on: position, in: color)
                         output.append(payload)
                     }
 
-                default:
-                    return [[]]
+                case .full(let position):
+                    let payload = shineFull(position, in: color)
+                    output.append(payload)
+
+                case .range(let start, let end):
+                    let payload = shineRange(from: start, to: end, in: color)
+                    output.append(payload)
+
+                case .halfLeft:
+                    let start: UInt8 = 0
+                    let end: UInt8 = 9
+                    let payload = shineRange(from: start, to: end, in: color)
+                    output.append(payload)
+
+                case .halfRight:
+                    let start: UInt8 = 10
+                    let end: UInt8 = 19
+                    let payload = shineRange(from: start, to: end, in: color)
+                    output.append(payload)
+
+                case .quarterFrontLeft:
+                    let start: UInt8 = 0
+                    let end: UInt8 = 4
+                    let payload = shineRange(from: start, to: end, in: color)
+                    output.append(payload)
+
+                case .quarterFrontRight:
+                    let start: UInt8 = 15
+                    let end: UInt8 = 19
+                    let payload = shineRange(from: start, to: end, in: color)
+                    output.append(payload)
+
+                case .quarterBackLeft:
+                    let start: UInt8 = 5
+                    let end: UInt8 = 9
+                    let payload = shineRange(from: start, to: end, in: color)
+                    output.append(payload)
+
+                case .quarterBackRight:
+                    let start: UInt8 = 10
+                    let end: UInt8 = 14
+                    let payload = shineRange(from: start, to: end, in: color)
+                    output.append(payload)
+
+                case .earLeft:
+                    let payload = shineSpot(0, on: .ears, in: color)
+                    output.append(payload)
+
+                case .earRight:
+                    let payload = shineSpot(1, on: .ears, in: color)
+                    output.append(payload)
+
             }
 
             return output
         }
 
-        private func shineSpot(_ id: UInt8, in color: Color, on position: Position) -> [UInt8] {
+        private func shineSpot(_ id: UInt8, on position: Spot.Position, in color: Color) -> [UInt8] {
             var payload: [UInt8] = []
 
             payload.append(contentsOf: [
@@ -67,10 +140,43 @@ extension Robot {
             payload.append(contentsOf: color.data)
             payload.append(payload.checksum8)
 
-            payload.insert(Self.id, at: 0)
+            payload.insert(Spot.id, at: 0)
 
             return payload
         }
+
+        private func shineFull(_ position: Full.Position, in color: Color) -> [UInt8] {
+            var payload: [UInt8] = []
+
+            payload.append(contentsOf: [
+                position.rawValue
+            ])
+
+            payload.append(contentsOf: color.data)
+            payload.append(payload.checksum8)
+
+            payload.insert(Full.id, at: 0)
+
+            return payload
+        }
+
+        private func shineRange(from start: UInt8, to end: UInt8, in color: Color) -> [UInt8] {
+            var payload: [UInt8] = []
+
+            payload.append(contentsOf: [
+                Range.Position.belt.rawValue,
+                start,
+                end,
+            ])
+
+            payload.append(contentsOf: color.data)
+            payload.append(payload.checksum8)
+
+            payload.insert(Range.id, at: 0)
+
+            return payload
+        }
+
     }
 
     public func shine(_ lights: Lights, color: Color) {
@@ -81,17 +187,20 @@ extension Robot {
         connectedPeripheral?
             .sendCommand(output)
 
+        // TODO(@ladislas): remove when done
         dump(output)
     }
 
     public func blacken(_ lights: Lights) {
         print("🤖 BLACKEN \(lights)")
+        shine(lights, color: .black)
     }
 
     public func stopLights() {
         print("🤖 STOP 🛑 - Lights")
+        shine(.all, color: .black)
     }
 
 }
 
-// swiftlint:enable identifier_name
+// swiftlint:enable identifier_name nesting cyclomatic_complexity
