45e3bba00620d2a3bbff0486e3ad6b5897530dc6
Ladislas de Toldi <ladislas@detoldi.me>
2023-10-11 18:26:41 +0200

    :sparkles: (RobotKit): Add command generator + tests

---
+++ Modules/RobotKit/Sources/Robot+Commands.swift
@@ -0,0 +1,24 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Foundation
+
+extension Robot {
+
+    static let kHeaderPattern: [UInt8] = [0x2A, 0x2A, 0x2A, 0x2A]
+
+    func commandGenerator(commands: [UInt8]...) -> Data {
+        var output: [UInt8] = []
+
+        output.append(contentsOf: Robot.kHeaderPattern)
+        output.append(UInt8(commands.count))
+
+        for command in commands {
+            output.append(contentsOf: command)
+        }
+
+        return Data(output)
+    }
+
+}
---
+++ Modules/RobotKit/Tests/Robot_Commands_Tests.swift
@@ -0,0 +1,44 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import XCTest
+
+@testable import RobotKit
+
+final class Robot_Commands_Tests: XCTestCase {
+
+    func test_generateFrameForOneCommand() {
+        // Given
+        let cmd: [UInt8] = [0x50, 0x51, 0x51]
+        let robot = Robot.shared
+
+        // When
+        let output = robot.commandGenerator(commands: cmd)
+        let expected = Data([0x2A, 0x2A, 0x2A, 0x2A, 0x01, 0x50, 0x51, 0x51])
+
+        // Then
+        XCTAssertEqual(output, expected)
+    }
+
+    func test_generateFrameForMultipleCommands() {
+        // Given
+        let cmd1: [UInt8] = [0x50, 0x51, 0x51]
+        let cmd2: [UInt8] = [0x60, 0x61, 0x61]
+        let cmd3: [UInt8] = [0x70, 0x71, 0x71]
+        let robot = Robot.shared
+
+        // When
+        let output = robot.commandGenerator(commands: cmd1, cmd2, cmd3)
+        let expected = Data([
+            0x2A, 0x2A, 0x2A, 0x2A, 0x03,
+            0x50, 0x51, 0x51,
+            0x60, 0x61, 0x61,
+            0x70, 0x71, 0x71,
+        ])
+
+        // Then
+        XCTAssertEqual(output, expected)
+    }
+
+}
