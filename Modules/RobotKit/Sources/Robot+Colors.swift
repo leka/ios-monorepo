599527ff159373a64297c57048202c591b68a1db
Ladislas de Toldi <ladislas@detoldi.me>
2023-10-12 00:24:14 +0200

    :technologist: (RobotKit): Add colors black and white

--- Modules/RobotKit/Sources/Robot+Colors.swift
+++ Modules/RobotKit/Sources/Robot+Colors.swift
@@ -23,6 +23,9 @@ extension Robot {
         }
 
         // TODO(@hugo): Add all colors decided w/ Lucie, Hortense
+        public static let black: Color = Color(data: [0, 0, 0])
+        public static let white: Color = Color(data: [255, 255, 255])
+
         public static let red: Color = Color(data: [255, 0, 0])
         public static let green: Color = Color(data: [0, 255, 0])
         public static let blue: Color = Color(data: [0, 0, 255])
