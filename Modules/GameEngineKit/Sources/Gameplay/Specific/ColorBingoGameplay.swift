3b72fede8db341a31dbf124859244a131cb3b1ec
Ladislas de Toldi <ladislas@detoldi.me>
2023-10-13 11:32:27 +0200

    :bulb: (TODO): Fix comments formatting

--- Modules/GameEngineKit/Sources/Gameplay/Specific/ColorBingoGameplay.swift
+++ Modules/GameEngineKit/Sources/Gameplay/Specific/ColorBingoGameplay.swift
@@ -31,7 +31,7 @@ public class ColorBingoGameplay: SelectionGameplayProtocol {
                 // TODO(@ladislas): Run reinforcers and lottie animation
                 robot.run(.rainbow)
 
-                // TO DO (@hugo) asyncAwait
+                // TODO(@HPezz) asyncAwait
                 DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
                     self.choices.value[index].status = .notSelected
                     self.state.send(.finished)
@@ -41,7 +41,7 @@ public class ColorBingoGameplay: SelectionGameplayProtocol {
             if let index = choices.value.firstIndex(where: { $0.id == choice.id }) {
                 self.choices.value[index].status = .wrongAnswer
 
-                // TO DO (@hugo) asyncAwait
+                // TODO(@HPezz) asyncAwait
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
                     self.choices.value[index].status = .notSelected
                 }
