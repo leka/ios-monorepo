81abf8df4c78217e8991e56083b8933b4d95ddd5
Ladislas de Toldi <ladislas@detoldi.me>
2023-10-21 23:08:07 +0200

    :sparkles: (Modules): Add ContentKit (initial scaffolding)

---
+++ Modules/ContentKit/Examples/ContentKitExample/Sources/MainApp.swift
@@ -0,0 +1,17 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import ContentKit
+import SwiftUI
+
+@main
+struct ContentKitExample: App {
+
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+
+}
---
+++ Modules/ContentKit/Sources/ContentKit.swift
@@ -0,0 +1,17 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Logging
+
+public class ContentKit {
+
+    public var shared: ContentKit {
+        ContentKit()
+    }
+
+    private init() {
+        // nothing to do
+    }
+
+}
