33b44065471e41f0f29f384da96aca3965f033e1
Ladislas de Toldi <ladislas@detoldi.me>
2023-10-26 00:38:29 +0200

    :technologist: (GEK): Example - allow dev to choose the activity under test

--- Modules/GameEngineKit/Examples/GameEngineKitExample/Sources/ContentView.swift
+++ Modules/GameEngineKit/Examples/GameEngineKitExample/Sources/ContentView.swift
@@ -6,14 +6,43 @@ import ContentKit
 import GameEngineKit
 import SwiftUI
 
+public enum ActivityList {
+    public static let mixed: Activity = ContentKit.decodeActivity("activity-mixed")
+    public static let touchToSelectOneAnswer: Activity = ContentKit.decodeActivity("activity-seq1-selection")
+}
+
 struct ContentView: View {
 
-    let activity = ContentKit.ActivityList.seq1Selection
+    @State var currentActivity: Activity?
 
     var body: some View {
-        VStack {
-            ActivityView(viewModel: ActivityViewViewModel(activity: activity))
+        VStack(spacing: 30) {
+            Button("Touch to select one answer") {
+                currentActivity = ActivityList.touchToSelectOneAnswer
+            }
+
+            Button("Touch to select multple answers") {
+                // TODO(@ladislas): change when implemented
+                currentActivity = ActivityList.touchToSelectOneAnswer
+            }
+
+            Button("Mix of interfaces") {
+                currentActivity = ActivityList.mixed
+            }
         }
+        .fullScreenCover(item: $currentActivity) {
+            currentActivity = nil
+        } content: { activity in
+            ActivityView(viewModel: ActivityViewViewModel(activity: activity))
+                .onLongPressGesture {
+                    currentActivity = nil
+                }
+        }
+        .buttonStyle(.borderedProminent)
     }
 
+}
+
+#Preview {
+    ContentView()
 }
