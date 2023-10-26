8a3dd4fd8abe57b4b04173752b2fd13fc82f2a5b
Hugo Pezziardi <hugo@pezziardi.net>
2023-10-26 17:21:33 +0200

    :sparkles: (GEK): Add ChoiceImageView

---
+++ Modules/GameEngineKit/Sources/_NewSystem/Views/ChoiceImageView.swift
@@ -0,0 +1,117 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+//import Combine
+import ContentKit
+import RobotKit
+import SwiftUI
+
+struct ChoiceImageView: View {
+
+    private let image: String
+    private let size: CGFloat
+    private let state: GameplayChoiceState
+    private let kOverLayScaleFactor: CGFloat = 1.08
+
+    @State private var animationPercent: CGFloat = .zero
+    @State private var overlayOpacity: CGFloat = .zero
+
+    init(image: String, size: CGFloat, state: GameplayChoiceState = .idle) {
+        self.image = image
+        self.size = size
+        self.state = state
+    }
+
+    @ViewBuilder
+    var circle: some View {
+        if let uiImage = UIImage(named: image) {
+            Image(uiImage: uiImage)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(
+                    width: size,
+                    height: size
+                )
+                .clipShape(Circle())
+        } else {
+            Text("❌\nImage not found:\n\(image)")
+                .multilineTextAlignment(.center)
+                .frame(
+                    width: size,
+                    height: size
+                )
+                .overlay {
+                    Circle()
+                        .stroke(Color.red, lineWidth: 5)
+                }
+        }
+    }
+
+    var body: some View {
+        switch state {
+            case .idle:
+                circle
+                    .onAppear {
+                        withAnimation {
+                            animationPercent = 0.0
+                            overlayOpacity = 0.0
+                        }
+                    }
+
+            case .rightAnswer:
+                circle
+                    .overlay {
+                        RightAnswerFeedback(animationPercent: animationPercent)
+                            .frame(
+                                width: size * kOverLayScaleFactor,
+                                height: size * kOverLayScaleFactor
+                            )
+                    }
+                    .onAppear {
+                        withAnimation {
+                            animationPercent = 1.0
+                        }
+                    }
+
+            case .wrongAnswer:
+                circle
+                    .overlay {
+                        WrongAnswerFeedback(overlayOpacity: overlayOpacity)
+                            .frame(
+                                width: size * kOverLayScaleFactor,
+                                height: size * kOverLayScaleFactor
+                            )
+                    }
+                    .onAppear {
+                        withAnimation {
+                            overlayOpacity = 0.8
+                        }
+                    }
+        }
+    }
+
+}
+
+#Preview {
+    VStack(spacing: 30) {
+        HStack(spacing: 50) {
+            ChoiceImageView(image: "image-placeholder-animals", size: 200)
+            ChoiceImageView(image: "image-placeholder-missing", size: 200)
+        }
+
+        HStack(spacing: 50) {
+            ChoiceImageView(image: "image-placeholder-animals", size: 200)
+            ChoiceImageView(image: "image-placeholder-animals", size: 200, state: .rightAnswer)
+            ChoiceImageView(image: "image-placeholder-animals", size: 200, state: .wrongAnswer)
+        }
+
+        HStack(spacing: 0) {
+            ChoiceImageView(image: "image-placeholder-animals", size: 200)
+            ChoiceColorView(color: "blue", size: 200)
+
+            ChoiceImageView(image: "image-placeholder-animals", size: 200, state: .rightAnswer)
+            ChoiceColorView(color: "blue", size: 200, state: .rightAnswer)
+        }
+    }
+}
