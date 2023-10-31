5bef986a774fb67c2832fa90a0713082a2857a0a
mathieu J <macteuts@gmail.com>
2023-10-31 18:12:39 +0200

    :sparkles: (GEK): Add updated ActivityProgressBar 2 colors only

---
+++ Modules/GameEngineKit/Sources/_NewSystem/Views/ActivityProgressBar.swift
@@ -0,0 +1,50 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import DesignKit
+import SwiftUI
+
+struct ActivityProgressBar: View {
+
+    @ObservedObject var viewModel: ActivityViewViewModel
+
+    var body: some View {
+        HStack(spacing: 0) {
+            Spacer()
+            ForEach(0..<viewModel.totalSequences, id: \.self) { sequenceIndex in
+                HStack(spacing: 0) {
+                    ForEach(0..<viewModel.totalExercisesInCurrentSequence, id: \.self) { exerciseIndex in
+                        let dotColor: Color = {
+                            guard
+                                sequenceIndex < viewModel.currentSequenceIndex
+                                    || (sequenceIndex == viewModel.currentSequenceIndex
+                                        && exerciseIndex < viewModel.currentExerciseIndexInSequence)
+                            else {
+                                return .white
+                            }
+                            return .green
+                        }()
+                        ActivityProgressBarMarker(
+                            color: dotColor,
+                            isCurrent: exerciseIndex == viewModel.currentExerciseIndexInSequence
+                        )
+                        .padding(6)
+
+                        if exerciseIndex < viewModel.totalExercisesInCurrentSequence - 1 {
+                            Spacer().frame(maxWidth: 100)
+                        }
+                    }
+                }
+                .frame(height: 30)
+                .background(DesignKitAsset.Colors.progressBar.swiftUIColor, in: Capsule())
+
+                if sequenceIndex < viewModel.totalSequences - 1 {
+                    Spacer().frame(minWidth: 20, maxWidth: 60)
+                }
+            }
+            Spacer()
+        }
+        .id(viewModel.currentExerciseIndexInSequence)
+    }
+}
