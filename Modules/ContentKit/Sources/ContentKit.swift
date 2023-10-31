26229b93f93be7aa48366fd5a8bb33975ce817c5
Ladislas de Toldi <ladislas@detoldi.me>
2023-10-31 09:18:33 +0100

    :recycle: (ContentKit): Add do try catch to yaml decoding, log errors

--- Modules/ContentKit/Sources/ContentKit.swift
+++ Modules/ContentKit/Sources/ContentKit.swift
@@ -3,7 +3,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import Foundation
+import LogKit
 import Yams
+
+let log = LogKit.createLoggerFor(module: "ContentKit")
 
 public class ContentKit {
 
@@ -15,24 +18,40 @@ public class ContentKit {
         // nothing to do
     }
 
+    // TODO(@ladislas): maybe return optional activity instead of fatalError
     public static func decodeActivity(_ filename: String) -> Activity {
-        // swiftlint:disable force_try
-        let data = try! String(
-            contentsOfFile: Bundle.main.path(forResource: filename, ofType: "yml")!, encoding: .utf8)
-        let activity = try! YAMLDecoder().decode(Activity.self, from: data)
-        // swiftlint:enable force_try
+        do {
+            guard let file = Bundle.main.path(forResource: filename, ofType: "yml") else {
+                log.error("File not found: \(filename)")
+                fatalError("💥 File not found: \(filename)")
+            }
 
-        return activity
+            let data = try String(contentsOfFile: file, encoding: .utf8)
+            let activity = try YAMLDecoder().decode(Activity.self, from: data)
+
+            return activity
+        } catch {
+            log.error("Error decoding \(filename): \(error)")
+            fatalError("💥 Error decoding \(filename): \(error)")
+        }
     }
 
+    // TODO(@ladislas): maybe return optional activity instead of fatalError
     public static func decodeActivityFromModule(_ filename: String) -> Activity {
-        // swiftlint:disable force_try
-        let data = try! String(
-            contentsOfFile: Bundle.module.path(forResource: filename, ofType: "yml")!, encoding: .utf8)
-        let activity = try! YAMLDecoder().decode(Activity.self, from: data)
-        // swiftlint:enable force_try
+        do {
+            guard let file = Bundle.module.path(forResource: filename, ofType: "yml") else {
+                log.error("File not found: \(filename)")
+                fatalError("💥 File not found: \(filename)")
+            }
 
-        return activity
+            let data = try String(contentsOfFile: file, encoding: .utf8)
+            let activity = try YAMLDecoder().decode(Activity.self, from: data)
+
+            return activity
+        } catch {
+            log.error("Error decoding \(filename): \(error)")
+            fatalError("💥 Error decoding \(filename): \(error)")
+        }
     }
 
 }
