af87594ce193243eb3594e1e6c542b9a92666e2e
mathieu J <macteuts@gmail.com>
2023-11-03 01:09:40 +0200

    :dizzy: (GEK): Animate Current exercice indicator on progressBar

--- Modules/GameEngineKit/Sources/_NewSystem/Views/ActivityProgressBarMarker.swift
+++ Modules/GameEngineKit/Sources/_NewSystem/Views/ActivityProgressBarMarker.swift
@@ -10,20 +10,30 @@ struct ActivityProgressBarMarker: View {
     @State var color: Color = .white
     @State var isCurrent: Bool
 
+    @State private var scale: CGFloat = 0
+
     var body: some View {
         Circle()
             .stroke(Color.white, lineWidth: 3)
             .background(color, in: Circle())
             .overlay {
                 currentExerciseIndicator
+                    .scaleEffect(scale)
+                    .onAppear {
+                        withAnimation(Animation.easeIn(duration: 0.5).delay(0.5)) {
+                            scale = 1
+                        }
+                    }
             }
     }
 
     @ViewBuilder private var currentExerciseIndicator: some View {
         if isCurrent {
             Circle()
-                .fill(DesignKitAsset.Colors.darkGray.swiftUIColor)
-                .padding(6)
+                .fill(DesignKitAsset.Colors.chevron.swiftUIColor)
+                .padding(4)
+        } else {
+            EmptyView()
         }
     }
 }
