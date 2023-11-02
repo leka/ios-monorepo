561c9c1587010be4c12b2f0b5bdc719befbaea8c
Hugo Pezziardi <hugo@pezziardi.net>
2023-11-02 12:38:55 +0100

    :recycle: (GemEngineKit): Refactor old base DnD code to fit new architecture

---
+++ Modules/GameEngineKit/Sources/_NewSystem/Views/DragAndDrop/DraggableImageAnswerNode.swift
@@ -0,0 +1,31 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import ContentKit
+import SpriteKit
+
+class DraggableImageAnswerNode: SKSpriteNode {
+
+    var isDraggable: Bool = true
+    var defaultPosition: CGPoint?
+
+    init(choice: DragAndDropChoice, scale: CGFloat = 1, position: CGPoint) {
+        super.init(texture: SKTexture(image: UIImage(named: choice.value)!), color: .clear, size: CGSize.zero)
+
+        let action = SKAction.setTexture(texture!, resize: true)
+        self.run(action)
+
+        self.name = choice.value
+        self.texture = texture
+        self.setScale(scale)
+        self.size = size
+        self.position = position
+        self.defaultPosition = position
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
---
+++ Modules/GameEngineKit/Sources/_NewSystem/Views/DragAndDrop/DraggableImageShadowNode.swift
@@ -0,0 +1,30 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import SpriteKit
+
+class DraggableImageShadowNode: SKSpriteNode {
+
+    init(draggableImageAnswerNode: DraggableImageAnswerNode) {
+        super
+            .init(
+                texture: draggableImageAnswerNode.texture, color: draggableImageAnswerNode.color,
+                size: draggableImageAnswerNode.size)
+
+        let actionShadow = SKAction.setTexture(draggableImageAnswerNode.texture!, resize: true)
+        self.run(actionShadow)
+
+        self.blendMode = SKBlendMode.alpha
+        self.colorBlendFactor = 1.0
+        self.color = .black
+        self.alpha = 0.15
+        self.setScale(draggableImageAnswerNode.xScale)
+        self.position = draggableImageAnswerNode.position
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
