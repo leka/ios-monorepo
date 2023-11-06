09abe638cb81c6629ecdd182a85b8533872e78f6
Hugo Pezziardi <hugo@pezziardi.net>
2023-11-06 18:15:43 +0100

    :sparkles: (GameEngineKit): Add RobotThenTouchToSelect views

---
+++ Modules/GameEngineKit/Sources/_NewSystem/Views/RobotThenTouchToSelect/RobotThenTouchToSelectView.swift
@@ -0,0 +1,157 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Combine
+import ContentKit
+import RobotKit
+import SwiftUI
+
+public struct RobotThenTouchToSelectView: View {
+
+    enum Interface: Int {
+        case oneChoice = 1
+        case twoChoices
+        case threeChoices
+        case fourChoices
+        case fiveChoices
+        case sixChoices
+    }
+
+    @StateObject private var viewModel: SelectionViewViewModel
+    @State private var animationProgress: CGFloat = 0.0
+    @State private var didSendCommandToRobot = false
+
+    private let color: String
+
+    let robot = Robot.shared
+
+    public init(choices: [SelectionChoice]) {
+        self._viewModel = StateObject(wrappedValue: SelectionViewViewModel(choices: choices))
+        guard let rightAnswer = choices.first(where: { $0.isRightAnswer }) else {
+            fatalError("No right answer found in choices")
+        }
+
+        guard rightAnswer.type == .color else {
+            log.error("Type \(rightAnswer.type) not implemented")
+            fatalError("💥 Type \(rightAnswer.type) not implemented")
+        }
+
+        self.color = rightAnswer.value
+    }
+
+    public init(exercise: Exercise, data: ExerciseSharedData? = nil) {
+        guard case .selection(let payload) = exercise.payload else {
+            fatalError("Exercise payload is not .selection")
+        }
+        guard case .robot(type: .color(value: let name)) = payload.action else {
+            fatalError("Exercise payload does not contain an iPad color action")
+        }
+
+        self.color = name
+
+        self._viewModel = StateObject(
+            wrappedValue: SelectionViewViewModel(choices: payload.choices, shared: data))
+    }
+
+    public var body: some View {
+        let interface = Interface(rawValue: viewModel.choices.count)
+
+        HStack(spacing: 0) {
+            Button {
+                robot.shine(.all(in: .init(from: color)))
+                withAnimation {
+                    didSendCommandToRobot = true
+                    animationProgress = 1.0
+                }
+            } label: {
+                ZStack {
+                    LinearGradient(
+                        gradient: Gradient(colors: [
+                            Color.black.opacity(0.7), Color.black.opacity(0.3), Color.black.opacity(0.7),
+                        ]), startPoint: .top, endPoint: .bottom
+                    )
+
+                    VStack {
+                        Image(systemName: "hand.tap")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(.white)
+                        Text("Tap to show")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                    }
+                }
+                .frame(width: 200)
+            }
+            .disabled(didSendCommandToRobot)
+            .buttonStyle(ActionButtonStyle(progress: animationProgress))
+            .scaleEffect(didSendCommandToRobot ? 1.0 : 0.8, anchor: .center)
+            .shadow(
+                color: .accentColor.opacity(0.2),
+                radius: didSendCommandToRobot ? 6 : 3,
+                x: 0,
+                y: 3
+            )
+            .animation(.spring(response: 1, dampingFraction: 0.45), value: didSendCommandToRobot)
+            .padding(20)
+
+            Divider()
+                .opacity(0.4)
+                .frame(maxHeight: 500)
+                .padding(.vertical, 20)
+
+            Spacer()
+
+            switch interface {
+                case .oneChoice:
+                    OneChoiceView(viewModel: viewModel, isTappable: didSendCommandToRobot)
+                        .onTapGestureIf(didSendCommandToRobot) {
+                            viewModel.onChoiceTapped(choice: viewModel.choices[0])
+                        }
+                        .animation(.easeOut(duration: 0.3), value: didSendCommandToRobot)
+
+                case .twoChoices:
+                    TwoChoicesView(viewModel: viewModel, isTappable: didSendCommandToRobot)
+                        .onTapGestureIf(didSendCommandToRobot) {
+                            viewModel.onChoiceTapped(choice: viewModel.choices[0])
+                        }
+                        .animation(.easeOut(duration: 0.3), value: didSendCommandToRobot)
+
+                case .threeChoices:
+                    ThreeChoicesView(viewModel: viewModel, isTappable: didSendCommandToRobot)
+                        .onTapGestureIf(didSendCommandToRobot) {
+                            viewModel.onChoiceTapped(choice: viewModel.choices[0])
+                        }
+                        .animation(.easeOut(duration: 0.3), value: didSendCommandToRobot)
+
+                case .fourChoices:
+                    FourChoicesView(viewModel: viewModel, isTappable: didSendCommandToRobot)
+                        .onTapGestureIf(didSendCommandToRobot) {
+                            viewModel.onChoiceTapped(choice: viewModel.choices[0])
+                        }
+                        .animation(.easeOut(duration: 0.3), value: didSendCommandToRobot)
+
+                case .fiveChoices:
+                    FiveChoicesView(viewModel: viewModel, isTappable: didSendCommandToRobot)
+                        .onTapGestureIf(didSendCommandToRobot) {
+                            viewModel.onChoiceTapped(choice: viewModel.choices[0])
+                        }
+                        .animation(.easeOut(duration: 0.3), value: didSendCommandToRobot)
+
+                case .sixChoices:
+                    SixChoicesView(viewModel: viewModel, isTappable: didSendCommandToRobot)
+                        .onTapGestureIf(didSendCommandToRobot) {
+                            viewModel.onChoiceTapped(choice: viewModel.choices[0])
+                        }
+                        .animation(.easeOut(duration: 0.3), value: didSendCommandToRobot)
+
+                default:
+                    Text("❌ Interface not available for \(viewModel.choices.count) choices")
+            }
+
+            Spacer()
+        }
+    }
+
+}
