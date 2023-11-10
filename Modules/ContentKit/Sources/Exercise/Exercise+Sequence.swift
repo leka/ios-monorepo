d81fbc5624dc2b1a07ec439c090d4d91f281468d
Ladislas de Toldi <ladislas@detoldi.me>
2023-11-10 17:47:57 +0100

    :recycle: (ContentKit): Move ExerciseSequence inside Exercise

---
+++ Modules/ContentKit/Sources/Exercise/Exercise+Sequence.swift
@@ -0,0 +1,13 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Foundation
+
+extension Exercise {
+
+    public struct Sequence: Codable {
+        public let exercises: [Exercise]
+    }
+
+}
