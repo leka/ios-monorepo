0a73ed252e3937b2ae7721eb970c81629be4eb58
Ladislas de Toldi <ladislas@detoldi.me>
2023-11-10 13:12:52 +0100

    :children_crossing: (GEK): ActionButtonObserve - Improve reveal animation

--- Modules/GameEngineKit/Sources/_NewSystem/Views/ActionButton/ActionButton+Observe.swift
+++ Modules/GameEngineKit/Sources/_NewSystem/Views/ActionButton/ActionButton+Observe.swift
@@ -5,71 +5,84 @@
 import DesignKit
 import SwiftUI
 
-struct ActionButtonObserve: View {
-    let image: String
-    @Binding var imageWasTapped: Bool
-    @State private var animationPercent: CGFloat = 0.0
+struct AnimatableBlur: AnimatableModifier {
+    var blurRadius: CGFloat
 
-    var body: some View {
-        Button {
-            withAnimation {
-                imageWasTapped = true
-                animationPercent = 1.0
-            }
-        } label: {
-            if let uiImage = UIImage(named: image) {
-                Image(uiImage: uiImage)
-                    .resizable()
-                    .overlay {
-                        RadialGradient(
-                            gradient: Gradient(colors: [
-                                Color.black.opacity(0.6), Color.black.opacity(0.9), Color.black.opacity(1),
-                            ]),
-                            center: .center,
-                            startRadius: 50,
-                            endRadius: 300
-                        )
-                        .opacity(1 - animationPercent)
+    var animatableData: CGFloat {
+        get { blurRadius }
+        set { blurRadius = newValue }
+    }
 
-                        VStack {
-                            Image(systemName: "hand.tap")
-                                .resizable()
-                                .frame(width: 50, height: 50)
-                                .foregroundColor(.white)
-                            Text("Tap to reveal")
-                                .font(.headline)
-                                .foregroundColor(.white)
-                        }
-                        .opacity(1 - animationPercent)
-                    }
-                    .clipShape(RoundedRectangle(cornerRadius: 8))
-                    .frame(
-                        width: 460,
-                        height: 460
-                    )
-
-            } else {
-                Text("❌\nImage not found:\n\(image)")
-                    .multilineTextAlignment(.center)
-                    .overlay {
-                        Circle()
-                            .stroke(Color.red, lineWidth: 5)
-                    }
-                    .frame(
-                        width: 460,
-                        height: 460
-                    )
-            }
-        }
-        .disabled(imageWasTapped)
-        .shadow(
-            color: .accentColor.opacity(0.2),
-            radius: imageWasTapped ? 6 : 3, x: 0, y: 3
-        )
-        .animation(.bouncy, value: animationPercent)
+    func body(content: Content) -> some View {
+        content
+            .blur(radius: blurRadius)
     }
 }
 
+struct AnimatableSaturation: AnimatableModifier {
+    var saturation: Double
+
+    var animatableData: Double {
+        get { saturation }
+        set { saturation = newValue }
+    }
+
+    func body(content: Content) -> some View {
+        content
+            .saturation(saturation)
+    }
+}
+
+struct ActionButtonObserve: View {
+    let image: String
+
+    @Binding var imageWasTapped: Bool
+
+    var body: some View {
+        if let uiImage = UIImage(named: image) {
+            Button {
+                withAnimation {
+                    imageWasTapped = true
+                }
+            } label: {
+                VStack {
+                    Image(systemName: "hand.tap")
+                        .resizable()
+                        .frame(width: 70, height: 70)
+                        .foregroundColor(.white)
+                    Text("Tap to reveal")
+                        .font(.title)
+                        .foregroundColor(.white)
+                }
+                .transition(.opacity)
+                .opacity(imageWasTapped ? 0 : 1)
+                .frame(width: 460, height: 460)
+                .contentShape(RoundedRectangle(cornerRadius: 10))
+            }
+            .disabled(imageWasTapped)
+            .background {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .frame(width: 460, height: 460)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .modifier(AnimatableBlur(blurRadius: imageWasTapped ? 0 : 20))
+                    .modifier(AnimatableSaturation(saturation: imageWasTapped ? 1 : 0))
+
+            }
+        } else {
+            Text("❌\nImage not found:\n\(image)")
+                .multilineTextAlignment(.center)
+                .overlay {
+                    Circle()
+                        .stroke(Color.red, lineWidth: 5)
+                }
+                .frame(
+                    width: 460,
+                    height: 460
+                )
+        }
+    }
+}
 #Preview {
 
     struct ActionObserveButtonContainer: View {
