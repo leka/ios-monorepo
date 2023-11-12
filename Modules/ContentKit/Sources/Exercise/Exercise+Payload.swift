4b968ca37ded1f2914bffa91d92ea0a674374d0c
Ladislas de Toldi <ladislas@detoldi.me>
2023-11-12 23:11:02 +0100

    :recycle: (ContentKit): Move Payload to Exercise

--- Modules/ContentKit/Sources/Exercise/Exercise+Payload.swift
+++ Modules/ContentKit/Sources/Exercise/Exercise+Payload.swift
@@ -4,51 +4,55 @@
 
 import Foundation
 
-public enum ExercisePayload: Codable {
-    case selection(SelectionPayload)
-    case dragAndDrop(DragAndDropPayload)
+extension Exercise {
 
-    // TODO(@ladislas): see if we can decode based on interface in Exercise
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CustomKeys.self)
+    public enum Payload: Codable {
+        case selection(SelectionPayload)
+        case dragAndDrop(DragAndDropPayload)
 
-        if container.allKeys.contains(.dropZoneA) {
-            let dropZoneA = try container.decode(DropZoneDetails.self, forKey: .dropZoneA)
-            let dropZoneB = try container.decodeIfPresent(DropZoneDetails.self, forKey: .dropZoneB)
-            let choices = try container.decode([DragAndDropChoice].self, forKey: .choices)
+        // TODO(@ladislas): see if we can decode based on interface in Exercise
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CustomKeys.self)
 
-            self = .dragAndDrop(DragAndDropPayload(dropZoneA: dropZoneA, dropZoneB: dropZoneB, choices: choices))
-            return
+            if container.allKeys.contains(.dropZoneA) {
+                let dropZoneA = try container.decode(DropZoneDetails.self, forKey: .dropZoneA)
+                let dropZoneB = try container.decodeIfPresent(DropZoneDetails.self, forKey: .dropZoneB)
+                let choices = try container.decode([DragAndDropChoice].self, forKey: .choices)
+
+                self = .dragAndDrop(DragAndDropPayload(dropZoneA: dropZoneA, dropZoneB: dropZoneB, choices: choices))
+                return
+            }
+
+            // ? Selection
+            if container.allKeys.contains(.choices) {
+                let choices = try container.decode([SelectionChoice].self, forKey: .choices)
+                let action = try? container.decode(Exercise.Action.self, forKey: .action)
+                let shuffleChoices = try container.decodeIfPresent(Bool.self, forKey: .shuffleChoices) ?? false
+
+                self = .selection(SelectionPayload(choices: choices, action: action, shuffleChoices: shuffleChoices))
+                return
+            }
+
+            throw DecodingError.dataCorruptedError(
+                forKey: CustomKeys.payload, in: container,
+                debugDescription:
+                    "Cannot decode ExercisePayload. Available keys: \(container.allKeys.map { $0.stringValue })")
         }
 
-        // ? Selection
-        if container.allKeys.contains(.choices) {
-            let choices = try container.decode([SelectionChoice].self, forKey: .choices)
-            let action = try? container.decode(Exercise.Action.self, forKey: .action)
-            let shuffleChoices = try container.decodeIfPresent(Bool.self, forKey: .shuffleChoices) ?? false
-
-            self = .selection(SelectionPayload(choices: choices, action: action, shuffleChoices: shuffleChoices))
-            return
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.singleValueContainer()
+            switch self {
+                case .selection(let payload):
+                    try container.encode(payload)
+                case .dragAndDrop(let payload):
+                    try container.encode(payload)
+            }
         }
 
-        throw DecodingError.dataCorruptedError(
-            forKey: CustomKeys.payload, in: container,
-            debugDescription:
-                "Cannot decode ExercisePayload. Available keys: \(container.allKeys.map { $0.stringValue })")
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        switch self {
-            case .selection(let payload):
-                try container.encode(payload)
-            case .dragAndDrop(let payload):
-                try container.encode(payload)
+        private enum CustomKeys: String, CodingKey {
+            case choices, dropZoneA, dropZoneB, payload, action
+            case shuffleChoices = "shuffle_choices"
         }
     }
 
-    private enum CustomKeys: String, CodingKey {
-        case choices, dropZoneA, dropZoneB, payload, action
-        case shuffleChoices = "shuffle_choices"
-    }
 }
