60bcc30d074f54d1d3f10139d89d4cb3be478214
Ladislas de Toldi <ladislas@detoldi.me>
2023-11-13 12:17:30 +0100

    :alembic: (ContentKit): DragAndDropAssociation - Add basic enum to hold things

--- Modules/ContentKit/Sources/Exercise/Exercise+DragAndDropAssociation.swift
+++ Modules/ContentKit/Sources/Exercise/Exercise+DragAndDropAssociation.swift
@@ -2,4 +2,23 @@
 // Copyright 2023 APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
-// ? to be implemented
+// TODO(@macteuts): Add real implementation if needed
+public enum DragAndDropAssociation {
+
+    public enum Catergory: String, Codable {
+        case catA
+        case catB
+        case catC
+    }
+
+    public struct Choice: Codable {
+        public let value: String
+        public let type: UIElementType
+        public let catergory: Catergory?
+    }
+
+    public struct Payload: Codable {
+        public let choices: [Choice]
+    }
+
+}
