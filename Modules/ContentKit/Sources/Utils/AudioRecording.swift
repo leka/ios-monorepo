41a35608a80f347477ee34e0611c4972de44c962
Hugo Pezziardi <hugo@pezziardi.net>
2023-11-20 17:27:41 +0100

    :recycle: (ContentKit): Add Song enum in AudioRecording

--- Modules/ContentKit/Sources/Utils/AudioRecording.swift
+++ Modules/ContentKit/Sources/Utils/AudioRecording.swift
@@ -5,6 +5,36 @@
 import Foundation
 
 public struct AudioRecording: Codable, Hashable, Equatable {
+
+    public enum Song: String, Codable {
+        case earlyBird
+        case emptyPage
+        case gigglySquirrel
+        case handsOn
+        case happyDays
+        case inTheGame
+        case littleByLittle
+
+        var details: (name: String, file: String) {
+            switch self {
+                case .earlyBird:
+                    return (name: "Early Bird", file: "Early_Bird")
+                case .emptyPage:
+                    return (name: "Empty Page", file: "Empty_Page")
+                case .gigglySquirrel:
+                    return (name: "Giggly Squirrel", file: "Giggly_Squirrel")
+                case .handsOn:
+                    return (name: "Hands on", file: "Hands_On")
+                case .happyDays:
+                    return (name: "Happy Days", file: "Happy_Days")
+                case .inTheGame:
+                    return (name: "In the game", file: "In_The_Game")
+                case .littleByLittle:
+                    return (name: "Little by Little", file: "Little_by_little")
+            }
+        }
+    }
+
     public let name: String
     public let file: String
 
@@ -12,4 +42,10 @@ public struct AudioRecording: Codable, Hashable, Equatable {
         self.name = name
         self.file = file
     }
+
+    public init(_ song: Song) {
+        self.name = song.details.name
+        self.file = song.details.file
+    }
+
 }
