525e7f3701ef0e946e080508fc086d26b76d3670
Hugo Pezziardi <hugo@pezziardi.net>
2023-11-20 17:34:25 +0100

    :sparkles: (GEK): Add DanceFreeze VM & RobotManager

---
+++ Modules/GameEngineKit/Sources/_NewSystem/Views/DanceFreeze/MainViewViewModel.swift
@@ -0,0 +1,123 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Combine
+import ContentKit
+import RobotKit
+import SwiftUI
+
+extension DanceFreeze {
+
+    class MainViewViewModel: ObservableObject {
+        @ObservedObject var exercicesSharedData: ExerciseSharedData
+        @Published public var progress: CGFloat = 0.0
+        @Published public var isDancing: Bool = false
+
+        var robotManager: RobotManager
+        var audioPlayer: AudioPlayer
+        let songs: [AudioRecording]
+        var motionMode: Motion = .rotation
+        var cancellables: Set<AnyCancellable> = []
+
+        init(songs: [AudioRecording], shuffle: Bool = false, shared: ExerciseSharedData? = nil) {
+            self.songs = songs
+            self.audioPlayer = AudioPlayer(audioRecording: songs.first!)
+            self.robotManager = RobotManager()
+
+            self.exercicesSharedData = shared ?? ExerciseSharedData()
+            self.exercicesSharedData.state = .playing
+
+            subscribeToAudioPlayerProgress()
+        }
+
+        private func subscribeToAudioPlayerProgress() {
+            self.audioPlayer.$progress
+                .receive(on: DispatchQueue.main)
+                .sink { [weak self] in
+                    guard let self = self else { return }
+                    self.progress = $0
+                    if self.progress == 1 {
+                        completeDanceFreeze()
+                    }
+                }
+                .store(in: &cancellables)
+        }
+
+        public func onDanceFreezeToggle() {
+            if progress == 1.0 {
+                exercicesSharedData.state = .completed
+                return
+            }
+            if audioPlayer.isPlaying {
+                audioPlayer.pause()
+                isDancing = false
+                robotManager.freeze()
+            } else {
+                audioPlayer.play()
+                isDancing = true
+                robotDance()
+            }
+        }
+
+        public func setAudioRecording(audioRecording: AudioRecording) {
+            audioPlayer.setAudioPlayer(audioRecording: audioRecording)
+        }
+
+        public func setMotionMode(motion: Motion) {
+            motionMode = motion
+        }
+
+        func completeDanceFreeze() {
+            robotManager.stopRobot()
+            exercicesSharedData.state = .completed
+        }
+
+        private func robotDance() {
+            switch motionMode {
+                case .rotation:
+                    robotRotation()
+                case .movement:
+                    robotMovement()
+            }
+            robotLightFrenzy()
+        }
+
+        private func robotLightFrenzy() {
+            guard exercicesSharedData.state != .completed, isDancing else { return }
+
+            robotManager.shineRandomly()
+
+            DispatchQueue.main.asyncAfter(
+                deadline: .now() + 0.2,
+                execute: {
+                    self.robotLightFrenzy()
+                })
+        }
+
+        private func robotRotation() {
+            guard exercicesSharedData.state != .completed, isDancing else { return }
+
+            let duration = robotManager.rotationDance()
+
+            DispatchQueue.main.asyncAfter(
+                deadline: .now() + duration,
+                execute: {
+                    self.robotRotation()
+                })
+        }
+
+        private func robotMovement() {
+            guard exercicesSharedData.state != .completed, isDancing else { return }
+
+            let duration = robotManager.movementDance()
+
+            DispatchQueue.main.asyncAfter(
+                deadline: .now() + duration,
+                execute: {
+                    self.robotMovement()
+                })
+        }
+    }
+
+}
