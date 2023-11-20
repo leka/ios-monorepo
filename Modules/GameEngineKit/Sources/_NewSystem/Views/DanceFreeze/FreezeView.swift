25ff093eeaababc3f95bab32bb1b7dba6a9ec6f3
Hugo Pezziardi <hugo@pezziardi.net>
2023-11-20 17:33:30 +0100

    :sparkles: (GEK): Add DanceFreeze views

---
+++ Modules/GameEngineKit/Sources/_NewSystem/Views/DanceFreeze/FreezeView.swift
@@ -0,0 +1,23 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import DesignKit
+import SwiftUI
+
+extension DanceFreeze {
+
+    struct FreezeView: View {
+        var body: some View {
+            LottieView(
+                name: "freeze", speed: 0.5,
+                loopMode: .loop
+            )
+        }
+    }
+
+}
+
+#Preview {
+    DanceFreeze.FreezeView()
+}
