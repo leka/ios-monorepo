afc6e576463a651b13c8d4b73a48327734d4ae4f
Yann Locatelli <yann.locatelli2@gmail.com>
2023-11-27 13:55:57 +0100

    :recycle: (LekaUpdater): Use RobotKit RobotConnectionView, ViewModel
    - delete unused files
    - Remove DesignKit
    - Remove VStack
    - Remove viewModel init in view
    - Use #Preview

--- Apps/LekaUpdater/Sources/View/ConnectionView/ConnectionView.swift
+++ Apps/LekaUpdater/Sources/View/ConnectionView/ConnectionView.swift
@@ -2,72 +2,32 @@
 // Copyright 2023 APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
-import DesignKit
 import LocalizationKit
+import RobotKit
 import SwiftUI
 
 struct ConnectionView: View {
-    @StateObject private var viewModel = ConnectionViewModel()
-
-    private let bold15: Font = .system(size: 15, weight: .bold)
-
-    @Environment(\.dismiss) var dismiss
-
     var body: some View {
         NavigationStack {
-            VStack {
-                RobotConnectionView(robotConnectionViewModel: viewModel.robotConnectionViewModel)
-
-                continueButton
-
-                LekaUpdaterAsset.Assets.lekaUpdaterIcon.swiftUIImage
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 70)
-                    .padding(35)
-            }
-            .foregroundColor(DesignKitAsset.Colors.darkGray.swiftUIColor)
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    VStack {
-                        Text(l10n.main.appName)
-                            .font(.title2)
-                            .bold()
-                        Text(l10n.main.appDescription)
+            RobotConnectionView()
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        VStack {
+                            Text(l10n.main.appName)
+                                .font(.title2)
+                                .bold()
+                            Text(l10n.main.appDescription)
+                        }
+                        .foregroundColor(.accentColor)
                     }
-                    .foregroundColor(.accentColor)
                 }
-            }
         }
-    }
-
-    private var continueButton: some View {
-        Button(
-            action: {
-                dismiss()
-            },
-            label: {
-                HStack {
-                    Text(l10n.connection.continueButton)
-                    Image(systemName: "arrow.forward.circle")
-                }
-                .font(bold15)
-                .foregroundColor(.white)
-                .padding(6)
-                .frame(width: 210)
-            }
-        )
-        .buttonStyle(.borderedProminent)
-        .tint(DesignKitAsset.Colors.lekaGreen.swiftUIColor)
-        .disabled(viewModel.continueButtonDisabled)
     }
 }
 
-struct ConnectionView_Previews: PreviewProvider {
-    static var previews: some View {
-        ConnectionView()
-            .environment(\.locale, .init(identifier: "en"))
-        ConnectionView()
-            .environment(\.locale, .init(identifier: "fr"))
-    }
+#Preview {
+    return ConnectionView()
+        .environment(\.locale, .init(identifier: "en"))
+    return ConnectionView()
+        .environment(\.locale, .init(identifier: "fr"))
 }
