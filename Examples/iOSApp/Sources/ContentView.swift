fe56aae6a624867e63d2e0a94e29149b2f811044
Ladislas de Toldi <ladislas@detoldi.me>
2023-11-29 12:28:45 +0100

    :alembic: (ObservableObject): Experiment ObservableObject manual update management

--- Examples/iOSApp/Sources/ContentView.swift
+++ Examples/iOSApp/Sources/ContentView.swift
@@ -5,14 +5,51 @@
 import Module
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        HelloView(color: .mint, name: "iOS App Example")
+class ViewModel: ObservableObject {
+
+    var number1: Int = 42
+    var number2: Int = 1337
+
+    func randomize() {
+        number1 = Int.random(in: 0...100)
+        number2 = Int.random(in: 0...100)
+        print("randomized numbers \(number1) - \(number2)")
     }
+
+    func update() {
+        objectWillChange.send()
+        print("updated numbers \(number1) - \(number2)")
+    }
+
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+
+    @StateObject var viewModel = ViewModel()
+
+    var body: some View {
+        Text("Number 1: \(viewModel.number1) - Number 2: \(viewModel.number2)")
+            .padding()
+            .onChange(of: viewModel.number1) { [oldValue = viewModel.number1] newValue in
+                print("ON CHANGE - number 1 - \(oldValue) -> \(newValue)")
+            }
+            .onChange(of: viewModel.number2) { [oldValue = viewModel.number2] newValue in
+                print("ON CHANGE - number 2 - \(oldValue) -> \(newValue)")
+            }
+
+        Button("Randomize") {
+            viewModel.randomize()
+        }
+        .buttonStyle(.bordered)
+
+        Button("Update") {
+            viewModel.update()
+        }
+        .buttonStyle(.bordered)
     }
+
+}
+
+#Preview {
+    ContentView()
 }
