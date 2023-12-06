d9ed9d0342dd562c7541bb70268fcae0270563e7
Hugo Pezziardi <hugo@pezziardi.net>
2023-12-06 17:54:17 +0100

    :truck: (GEK): Move BinaryToggleChoiceStyle to general Views folder

--- Modules/GameEngineKit/Sources/_NewSystem/Exercises/Specialized/DanceFreeze/DanceFreeze+LauncherView+ButtonStyles.swift
+++ Modules/GameEngineKit/Sources/_NewSystem/Exercises/Specialized/DanceFreeze/DanceFreeze+LauncherView+ButtonStyles.swift
@@ -6,31 +6,6 @@ import SwiftUI
 
 extension DanceFreeze.LauncherView {
 
-    struct BinaryChoiceToggleStyle: ToggleStyle {
-        func makeBody(configuration: Configuration) -> some View {
-            HStack {
-                configuration.label
-                RoundedRectangle(cornerRadius: 20)
-                    .foregroundColor(.teal)
-
-                    .frame(width: 50, height: 30)
-                    .overlay(
-                        Circle()
-                            .foregroundColor(Color.white)
-                            .padding(3)
-                            .offset(x: configuration.isOn ? 10 : -10)
-                    )
-                    .onTapGesture {
-                        withAnimation {
-                            configuration.isOn.toggle()
-                        }
-                    }
-            }
-            .frame(alignment: .center)
-            .padding(0)
-        }
-    }
-
     struct StageModeButtonStyle: View {
         let text: String
         let color: Color
