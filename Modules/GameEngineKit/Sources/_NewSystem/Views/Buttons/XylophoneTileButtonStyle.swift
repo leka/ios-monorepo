63be11904c00ede1d4d580fbc153998e23c19062
Hugo Pezziardi <hugo@pezziardi.net>
2023-12-06 18:00:12 +0100

    :truck: (GEK): Move XyloTileStyle to genral Views folder

---
+++ Modules/GameEngineKit/Sources/_NewSystem/Views/Buttons/XylophoneTileButtonStyle.swift
@@ -0,0 +1,84 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import DesignKit
+import SwiftUI
+
+struct XylophoneTileButtonStyle: ButtonStyle {
+    let xyloAttachColor = Color(red: 0.87, green: 0.65, blue: 0.54)
+    let defaultMaxTileHeight: Int = 500
+    let defaultTileHeightGap: Int = 250
+    let defaultTilesScaleFeedback: CGFloat = 0.98
+    let defaultTilesRotationFeedback: CGFloat = -1
+
+    let index: Int
+    let tileNumber: Int
+    let tileWidth: CGFloat
+    let isTappable: Bool
+
+    init(index: Int, tileNumber: Int, tileWidth: CGFloat = 100, isTappable: Bool = true) {
+        self.index = index
+        self.tileNumber = tileNumber
+        self.tileWidth = tileWidth
+        self.isTappable = isTappable
+    }
+
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .overlay {
+                VStack {
+                    Spacer()
+                    Circle()
+                        .fill(xyloAttachColor)
+                        .shadow(
+                            color: .black.opacity(0.4),
+                            radius: 3, x: 0, y: 3
+                        )
+                    Spacer()
+                    Circle()
+                        .fill(xyloAttachColor)
+                        .shadow(
+                            color: isTappable ? .black.opacity(0.4) : .clear,
+                            radius: 3, x: 0, y: 3
+                        )
+                    Spacer()
+                }
+                .frame(width: 44)
+            }
+            .overlay {
+                RoundedRectangle(cornerRadius: 7, style: .circular)
+                    .stroke(.black.opacity(configuration.isPressed ? 0.3 : 0), lineWidth: 20)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 7, style: .circular))
+            .frame(width: tileWidth, height: setSizeFromIndex())
+            .scaleEffect(
+                configuration.isPressed ? defaultTilesScaleFeedback : 1,
+                anchor: .center
+            )
+            .rotationEffect(
+                Angle(degrees: configuration.isPressed ? defaultTilesRotationFeedback : 0),
+                anchor: .center
+            )
+            .shadow(
+                color: isTappable ? .black.opacity(0.4) : .clear,
+                radius: 3, x: 0, y: 3
+            )
+    }
+
+    private func setSizeFromIndex() -> CGFloat {
+        let sizeDiff = defaultTileHeightGap / tileNumber
+        let tileHeight = defaultMaxTileHeight - index * sizeDiff
+
+        return CGFloat(tileHeight)
+    }
+}
+
+#Preview {
+    Button {
+        // Nothing to do
+    } label: {
+        Color(.red)
+    }
+    .buttonStyle(XylophoneTileButtonStyle(index: 0, tileNumber: 1, tileWidth: 130))
+}
