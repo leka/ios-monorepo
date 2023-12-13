08d74674cc0509a07a63458e3594e9e733858e28
Hugo Pezziardi <hugo@pezziardi.net>
2023-12-13 16:50:45 +0100

    :sparkles: (GEK): Add Pairing views

---
+++ Modules/GameEngineKit/Sources/_NewSystem/Exercises/Specialized/Pairing/PairingView+ActionButton.swift
@@ -0,0 +1,55 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import DesignKit
+import SwiftUI
+
+// MARK: - Action
+
+extension PairingView {
+    struct ActionButton: View {
+        // MARK: Lifecycle
+
+        init(_ actionType: ActionType, text: String, hasStarted: Bool = false, action: @escaping () -> Void) {
+            self.actionType = actionType
+            self.text = text
+            self.hasStarted = hasStarted
+            self.action = action
+        }
+
+        // MARK: Internal
+
+        let actionType: ActionType
+        let text: String
+        let hasStarted: Bool
+        let action: () -> Void
+
+        var body: some View {
+            VStack {
+                Button {
+                    self.action()
+                } label: {
+                    self.actionType.icon(self.hasStarted)
+                }
+
+                .background(
+                    Circle()
+                        .fill(.white)
+                        .shadow(color: .black.opacity(0.5), radius: 7, x: 0, y: 4)
+                )
+
+                Text(self.text)
+                    .font(.body)
+                    .foregroundColor(DesignKitAsset.Colors.lekaDarkGray.swiftUIColor)
+                    .padding(.vertical, 10)
+            }
+        }
+    }
+}
+
+#Preview {
+    PairingView.ActionButton(.stop, text: "Stop", hasStarted: true) {
+        print("Button tapped !")
+    }
+}
---
+++ Modules/GameEngineKit/Sources/_NewSystem/Exercises/Specialized/Pairing/PairingView.swift
@@ -0,0 +1,87 @@
+// Leka - iOS Monorepo
+// Copyright 2023 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import ContentKit
+import SwiftUI
+
+// MARK: - PairingView
+
+struct PairingView: View {
+    // MARK: Lifecycle
+
+    init(instructions: Pairing.Payload.Instructions) {
+        self.instructions = instructions
+        self.shared = ExerciseSharedData()
+    }
+
+    init(exercise: Exercise, data: ExerciseSharedData? = nil) {
+        guard let payload = exercise.payload as? Pairing.Payload else {
+            fatalError("Exercise payload is not .selection")
+        }
+
+        self.instructions = payload.instructions
+        self.shared = data
+    }
+
+    // MARK: Internal
+
+    let instructions: Pairing.Payload.Instructions
+    let robotManager = RobotManager()
+    let shared: ExerciseSharedData?
+
+    var body: some View {
+        VStack {
+            Text(self.instructions.textMainInstructions)
+                .font(.body)
+                .foregroundColor(.primary)
+                .multilineTextAlignment(.center)
+
+            Spacer()
+
+            HStack(spacing: 180) {
+                if self.isPlaying {
+                    ActionButton(.pause, text: self.instructions.textButtonPause) {
+                        print("Pairing behavior is pausing")
+                        self.robotManager.pausePairing()
+                        self.isPlaying = false
+                    }
+                } else {
+                    ActionButton(.start, text: self.instructions.textButtonPlay) {
+                        print("Pairing behavior is running")
+                        self.robotManager.startPairing()
+                        self.isPlaying = true
+                        self.hasStarted = true
+                    }
+                }
+
+                ActionButton(.stop, text: self.instructions.textButtonStop, hasStarted: self.hasStarted) {
+                    print("Pairing behavior stopped")
+                    self.robotManager.stopPairing()
+
+                    self.isPlaying = false
+                    self.hasStarted = false
+                }
+                .disabled(!self.hasStarted)
+            }
+
+            Spacer()
+        }
+    }
+
+    // MARK: Private
+
+    @State private var isPlaying: Bool = false
+    @State private var hasStarted: Bool = false
+}
+
+#Preview {
+    let instructions = Pairing.Payload.Instructions(
+        textMainInstructions: "Instructions principales",
+        textButtonPlay: "Jouer",
+        textButtonPause: "Pause",
+        textButtonStop: "Stop"
+    )
+
+    return PairingView(instructions: instructions)
+}
