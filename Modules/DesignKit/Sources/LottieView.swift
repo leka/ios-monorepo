ba8e27ae37adf32d74cc5ffa64fa6442b6144035
Ladislas de Toldi <ladislas@detoldi.me>
2024-01-03 14:39:17 +0100

    :recycle: (DesignKit): Refactor LottieView to use vanilla implementation

--- Modules/DesignKit/Sources/LottieView.swift
+++ Modules/DesignKit/Sources/LottieView.swift
@@ -5,117 +5,37 @@
 import Lottie
 import SwiftUI
 
-public struct LottieView: UIViewRepresentable {
-    // MARK: Lifecycle
-
-    public init(
+public extension LottieView {
+    init(
         name: String,
-        speed: CGFloat = 1,
-        reverse: Bool = false,
+        speed: CGFloat,
         loopMode: LottieLoopMode = .playOnce,
-        action: @escaping () -> Void = {
-            // empty default implementation
-        },
-        play _: Binding<Bool> = .constant(true)
-    ) {
+        _ completion: LottieCompletionBlock? = nil
+    ) where Placeholder == EmptyView {
         let animation = LottieAnimation.named(name)!
-        self.init(animation: animation, speed: speed, reverse: reverse, loopMode: loopMode, action: action)
+        let view = LottieView(animation: animation)
+            .configure {
+                $0.loopMode = loopMode
+                $0.animationSpeed = speed
+                $0.play(completion: completion)
+            }
+
+        self = view
     }
 
-    public init(
+    init(
         animation: LottieAnimation,
-        speed: CGFloat = 1,
-        reverse: Bool = false,
+        speed: CGFloat,
         loopMode: LottieLoopMode = .playOnce,
-        action: @escaping () -> Void = {
-            // empty default implementation
-        },
-        play: Binding<Bool> = .constant(true)
-    ) {
-        self.animation = animation
-        self.speed = speed
-        self.reverse = reverse
-        self.loopMode = loopMode
-        self.action = action
-        _play = play
-    }
-
-    // MARK: Public
-
-    public typealias UIViewType = UIView
-
-    public class Coordinator: NSObject {
-        // MARK: Lifecycle
-
-        init(_ animationView: LottieView) {
-            self.parent = animationView
-            super.init()
-        }
-
-        // MARK: Internal
-
-        var parent: LottieView
-    }
-
-    public func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-
-    public func makeUIView(context _: UIViewRepresentableContext<LottieView>) -> UIView {
-        let view = UIView()
-
-        self.animationView.animation = self.animation
-        self.animationView.contentMode = .scaleAspectFit
-        self.animationView.animationSpeed = self.speed
-        self.animationView.loopMode = self.loopMode
-        self.animationView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(self.animationView)
-
-        NSLayoutConstraint.activate([
-            self.animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
-            self.animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
-        ])
-
-        return view
-    }
-
-    public func updateUIView(_: UIView, context: UIViewRepresentableContext<LottieView>) {
-        if self.play {
-            if self.reverse {
-                context.coordinator.parent.animationView.play(fromProgress: 0.0, toProgress: 1.0, loopMode: .none) {
-                    _ in
-                    self.animationView.pause()
-                }
-            } else {
-                context.coordinator.parent.animationView.play { finished in
-                    if finished {
-                        self.animationView.pause()
-                        self.action()
-                    }
-                }
+        _ completion: LottieCompletionBlock? = nil
+    ) where Placeholder == EmptyView {
+        let view = Lottie.LottieView(animation: animation)
+            .configure {
+                $0.loopMode = loopMode
+                $0.animationSpeed = speed
+                $0.play(completion: completion)
             }
-        } else {
-            if self.reverse {
-                context.coordinator.parent.animationView.animationSpeed = self.speed * 1.5
-                context.coordinator.parent.animationView.play(fromProgress: 1.0, toProgress: 0.0, loopMode: .none) {
-                    _ in
-                    context.coordinator.parent.animationView.stop()
-                    context.coordinator.parent.animationView.animationSpeed = self.speed
-                }
-            } else {
-                context.coordinator.parent.animationView.stop()
-            }
-        }
+
+        self = view
     }
-
-    // MARK: Internal
-
-    var animation: LottieAnimation
-    var speed: CGFloat
-    var reverse: Bool
-    var loopMode: LottieLoopMode
-    var action: () -> Void
-    @Binding var play: Bool
-
-    var animationView = LottieAnimationView()
 }
--- Modules/GameEngineKit/Sources/_NewSystem/Exercises/Specialized/HideAndSeek/HideAndSeekView+HiddenView.swift
+++ Modules/GameEngineKit/Sources/_NewSystem/Exercises/Specialized/HideAndSeek/HideAndSeekView+HiddenView.swift
@@ -1,8 +1,13 @@
 // Leka - iOS Monorepo
+// Copyright 2024 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+// Leka - iOS Monorepo
 // Copyright 2023 APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
 import DesignKit
+import Lottie
 import SwiftUI
 
 extension HideAndSeekView {
