4a238aaebda5023f1889adfa0c08f45771e82263
Ladislas de Toldi <ladislas@detoldi.me>
2024-01-03 14:45:38 +0100

    :recycle: (GEK): Success/Failure views - refactor to use as fullscreencover

--- Modules/GameEngineKit/Sources/_NewSystem/Views/Activity/Activityview+FailureView.swift
+++ Modules/GameEngineKit/Sources/_NewSystem/Views/Activity/Activityview+FailureView.swift
@@ -1,28 +1,40 @@
 // Leka - iOS Monorepo
+// Copyright 2024 APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+// Leka - iOS Monorepo
 // Copyright 2023 APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
 import DesignKit
+import Lottie
 import SwiftUI
 
 extension ActivityView {
     struct FailureView: View {
         var body: some View {
-            ZStack {
-                LottieView(
-                    animation: .tryAgain, speed: 0.6
-                )
-
+            VStack(spacing: 40) {
+                // TODO(@ladislas/@hugo): Use text variables to be localization friendly
                 VStack {
-                    // TODO(@ladislas/@hugo): Use text variables to be localization friendly
                     Text("10% de réussite !")
                         .font(.largeTitle)
                         .foregroundStyle(.teal)
                         .padding(10)
                     Text("Essayez à nouveau !")
                         .font(.largeTitle)
-                    Spacer()
                 }
+
+                LottieView(
+                    animation: .tryAgain,
+                    speed: 0.6
+                )
+                .frame(height: 500)
+                .padding(-100)
+
+                Button("Revenir au menu") {
+                    UIApplication.shared.dismissAll(animated: true)
+                }
+                .buttonStyle(.bordered)
             }
         }
     }
