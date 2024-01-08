0139f165ec0aaea753057772562c738c5c27b9ea
Ladislas de Toldi <ladislas@detoldi.me>
2024-01-08 14:31:16 +0100

    :technologist: (tuist): InfoPlist - set default app category to utilities

--- Tuist/ProjectDescriptionHelpers/InfoPlist+Base.swift
+++ Tuist/ProjectDescriptionHelpers/InfoPlist+Base.swift
@@ -33,6 +33,7 @@ public extension InfoPlist {
                 "UIInterfaceOrientationLandscapeRight",
                 "UIInterfaceOrientationLandscapeLeft",
             ],
+            "LSApplicationCategoryType": "public.app-category.utilities",
         ]
     }
 
