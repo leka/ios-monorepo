ce3c3bcadfc04c08989f294fda1d2b3f99b9dd77
Hugo Pezziardi <hugo@pezziardi.net>
2024-01-12 19:24:47 +0100

    :construction: (LekaApp): Add temporary empty CreateUserProfile view

---
+++ Apps/LekaApp/Sources/_NEWCodeBase/Views/Signup/AccountCreationView+CreateUserProfileView.swift
@@ -0,0 +1,43 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import DesignKit
+import SwiftUI
+
+// MARK: - CreateUserProfileView
+
+extension AccountCreationView {
+    struct CreateUserProfileView: View {
+        // MARK: Internal
+
+        @EnvironmentObject var viewRouter: ViewRouter
+
+        var body: some View {
+            VStack {
+                Text("To implement on Teacher profiles structure ready !")
+                    .edgesIgnoringSafeArea(.top)
+                    .navigationDestination(isPresented: self.$navigateToStepFinal) {
+                        StepFinal()
+                    }
+                    .toolbar {
+                        ToolbarItem(placement: .principal) {
+                            NavigationTitle()
+                        }
+                    }
+
+                Button("Go final step !") {
+                    self.navigateToStepFinal.toggle()
+                }
+            }
+        }
+
+        // MARK: Private
+
+        @State private var navigateToStepFinal: Bool = false
+    }
+}
+
+#Preview {
+    AccountCreationView.CreateUserProfileView()
+}
