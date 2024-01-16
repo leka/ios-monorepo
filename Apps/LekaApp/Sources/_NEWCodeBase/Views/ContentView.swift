dfdf3490519b23dae4ba0025eb3e9e645a24bbe3
Ladislas de Toldi <ladislas@detoldi.me>
2024-01-16 12:01:03 +0100

    :fire: (LekaApp): ContentView - remove fullscreen cover used for debug

--- Apps/LekaApp/Sources/_NEWCodeBase/Views/ContentView.swift
+++ Apps/LekaApp/Sources/_NEWCodeBase/Views/ContentView.swift
@@ -5,8 +5,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    // MARK: Internal
-
     @EnvironmentObject var viewRouter: ViewRouter
 
     var body: some View {
@@ -21,29 +19,5 @@ struct ContentView: View {
             }
         }
         .animation(.default, value: self.viewRouter.currentPage)
-        .fullScreenCover(isPresented: self.$isCurrentPageModalShown) {
-            VStack {
-                Button("Show WelcomeView") {
-                    self.viewRouter.currentPage = .welcome
-                    self.isCurrentPageModalShown.toggle()
-                }
-                .buttonStyle(.borderedProminent)
-
-                Button("Show MainView") {
-                    self.viewRouter.currentPage = .home
-                    self.isCurrentPageModalShown.toggle()
-                }
-                .buttonStyle(.borderedProminent)
-
-                Button("Dismiss") {
-                    self.isCurrentPageModalShown.toggle()
-                }
-                .buttonStyle(.bordered)
-            }
-        }
     }
-
-    // MARK: Private
-
-    @State private var isCurrentPageModalShown = true
 }
