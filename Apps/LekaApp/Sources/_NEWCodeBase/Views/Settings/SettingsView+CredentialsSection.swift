ba9473a06702831b64d27dfb2c5349be69aca4c7
Hugo Pezziardi <hugo@pezziardi.net>
2024-01-19 17:09:38 +0100

    :sparkles: (LekaApp): Add account related settings sections

---
+++ Apps/LekaApp/Sources/_NEWCodeBase/Views/Settings/SettingsView+CredentialsSection.swift
@@ -0,0 +1,36 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import LocalizationKit
+import SwiftUI
+
+// MARK: - SettingsView.CredentialsSection
+
+extension SettingsView {
+    struct CredentialsSection: View {
+        @ObservedObject var rootOwnerViewModel: RootOwnerViewModel = .shared
+
+        var body: some View {
+            Section(String(l10n.SettingsView.CredentialsSection.header.characters)) {
+                LabeledContent {
+                    Text(self.rootOwnerViewModel.currentCompany.email)
+                        // TODO: (@ui/ux) - Design System - replace with Leka font
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                } label: {
+                    Text(l10n.SettingsView.CredentialsSection.emailLabel)
+                }
+
+                Text(l10n.SettingsView.CredentialsSection.changeCredentialsButtonLabel)
+                    .foregroundColor(.accentColor)
+            }
+        }
+    }
+}
+
+#Preview {
+    Form {
+        SettingsView.CredentialsSection()
+    }
+}
