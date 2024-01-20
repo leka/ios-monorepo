552e58d3cf45c1ed942cc617f891f5d7be67cd3e
Ladislas de Toldi <ladislas@detoldi.me>
2024-01-20 15:01:07 +0100

    :recycle: (ContentKit): Refactor to enum instead of class

--- Modules/ContentKit/Sources/ContentKit.swift
+++ Modules/ContentKit/Sources/ContentKit.swift
@@ -8,16 +8,4 @@ let log = LogKit.createLoggerFor(module: "ContentKit")
 
 // MARK: - ContentKit
 
-public class ContentKit {
-    // MARK: Lifecycle
-
-    private init() {
-        // nothing to do
-    }
-
-    // MARK: Public
-
-    public var shared: ContentKit {
-        ContentKit()
-    }
-}
+public enum ContentKit {}
