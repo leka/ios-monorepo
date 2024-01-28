20829fa2af0f2b5d0b080d5b1a31a8a05a0d234b
mathieu J <macteuts@gmail.com>
2024-01-28 15:59:24 +0200

    :globe_with_meridians: (LekaApp): Add l10n to email and password error messages

---
+++ Apps/LekaApp/Sources/_NEWCodeBase/Views/TextFields/TextFieldEmail+l10n.swift
@@ -0,0 +1,17 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import LocalizationKit
+
+// swiftlint:disable line_length
+
+extension l10n {
+    enum TextFieldEmail {
+        static let label = LocalizedString("lekaapp.TextFieldEmail.label", value: "Email", comment: "TextFieldEmail label")
+
+        static let invalidEmailErrorLabel = LocalizedString("lekaapp.TextFieldEmail.invalidEmailErrorLabel", value: "Please enter a valid email address", comment: "TextFieldEmail invalid Email Error Label")
+    }
+}
+
+// swiftlint:enable line_length
