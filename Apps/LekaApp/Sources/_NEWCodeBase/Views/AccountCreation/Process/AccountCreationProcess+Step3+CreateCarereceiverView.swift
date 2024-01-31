1c744aaef30369bae27380dfe850033d7826d2ac
Hugo Pezziardi <hugo@pezziardi.net>
2024-01-31 17:11:08 +0100

    :sparkles: (LekaApp): Add Carereceiver creation view

--- Apps/LekaApp/Sources/_NEWCodeBase/Views/AccountCreation/Process/AccountCreationProcess+Step3+CreateCarereceiverView.swift
+++ Apps/LekaApp/Sources/_NEWCodeBase/Views/AccountCreation/Process/AccountCreationProcess+Step3+CreateCarereceiverView.swift
@@ -3,30 +3,58 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import DesignKit
+import LocalizationKit
 import SwiftUI
 
-// MARK: - CreateUserProfileView
+// MARK: - CreateCarereceiverView
 
 extension AccountCreationProcess {
     struct CreateCarereceiverView: View {
+        // MARK: Internal
+
         @Binding var selectedTab: Step
         @Binding var isPresented: Bool
 
         var body: some View {
-            VStack {
-                Text("To implement on Teacher profiles structure ready !")
-                    .edgesIgnoringSafeArea(.top)
-                    .toolbar {
-                        ToolbarItem(placement: .principal) {
-                            NavigationTitle()
-                        }
-                    }
+            NavigationStack {
+                ScrollView {
+                    VStack(spacing: 40) {
+                        self.avatarNavigationLink
 
-                Button("Go step 4 !") {
-                    withAnimation {
-                        self.isPresented.toggle()
-                        self.selectedTab = .final
+                        TextFieldDefault(label: String(l10n.AccountCreationProcess.Step3.CarereceiverCreation.carereceiverNameLabel.characters),
+                                         entry: self.$rootOwnerViewModel.bufferCarereceiver.name)
+                            .frame(width: 400)
+
+                        Button(String(l10n.AccountCreationProcess.Step3.CarereceiverCreation.registerProfilButton.characters)) {
+                            withAnimation {
+                                self.isPresented.toggle()
+                                self.selectedTab = .final
+                            }
+                            // TODO: (@team) : add the carereceiver profile to the account
+                            // TODO: (@team) : assign the carereceiver profile as the current selected one
+                        }
+                        .disabled(self.rootOwnerViewModel.bufferCarereceiver.name.isEmpty)
+                        .buttonStyle(.borderedProminent)
                     }
+                    .padding()
+                    .navigationTitle(String(l10n.AccountCreationProcess.Step3.CarereceiverCreation.title.characters))
+                }
+            }
+        }
+
+        // MARK: Private
+
+        @ObservedObject private var rootOwnerViewModel = RootOwnerViewModel.shared
+
+        private var avatarNavigationLink: some View {
+            NavigationLink {
+                AvatarPicker(avatar: self.$rootOwnerViewModel.bufferCarereceiver.avatar)
+            } label: {
+                VStack(spacing: 15) {
+                    AvatarPicker.ButtonLabel(image: self.rootOwnerViewModel.bufferCarereceiver.avatar)
+                    Text(l10n.AccountCreationProcess.Step3.CarereceiverCreation.avatarChoiceButton)
+                        // TODO: (@ui/ux) - Design System - replace with Leka font
+                        .font(.headline)
                 }
             }
         }
