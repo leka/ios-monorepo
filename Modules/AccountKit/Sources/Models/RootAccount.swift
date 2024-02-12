5e9cbbe7f209d533b753138901ee9b0a9b185e5c
mathieu J <macteuts@gmail.com>
2024-02-12 22:29:05 +0200

    :sparkles: (AccountKit): Add RootOwner CodingKeys and Firestore PropertyWrappers

--- Modules/AccountKit/Sources/Models/RootAccount.swift
+++ Modules/AccountKit/Sources/Models/RootAccount.swift
@@ -2,22 +2,30 @@
 // Copyright APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
+import FirebaseFirestore
 import SwiftUI
 
 // MARK: - AccountDocument
 
-protocol AccountDocument: Codable, Identifiable {
-    var id: String { get set }
+public protocol AccountDocument: Codable, Identifiable {
+    var id: String? { get set }
     var rootOwnerUid: String { get set }
-    var createdAt: Date { get set }
-    var lastEditedAt: Date { get set }
+    var createdAt: Date? { get set }
+    var lastEditedAt: Date? { get set }
 }
 
 // MARK: - RootAccount
 
 struct RootAccount: AccountDocument {
-    var id: String
+    enum CodingKeys: String, CodingKey {
+        case id
+        case rootOwnerUid = "root_owner_uid"
+        case createdAt = "created_at"
+        case lastEditedAt = "last_edited_at"
+    }
+
+    @DocumentID var id: String?
     var rootOwnerUid: String
-    var createdAt: Date
-    var lastEditedAt: Date
+    @ServerTimestamp var createdAt: Date?
+    @ServerTimestamp var lastEditedAt: Date?
 }
