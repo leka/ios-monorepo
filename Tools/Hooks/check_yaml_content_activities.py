fc70b7154d658772c3b91d8bbd64e2ea1e5cd62d
Ladislas de Toldi <ladislas@detoldi.me>
2024-02-15 18:34:59 +0100

    :bug: (hooks): Check activity content - fix arg parsing to handle all files

--- Tools/Hooks/check_yaml_content_activities.py
+++ Tools/Hooks/check_yaml_content_activities.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Check the content of a YAML file for an activity"""
 
 # Leka - LekaOS
 # Copyright 2020 APF France handicap
@@ -7,11 +8,68 @@
 import os
 import subprocess
 import uuid
-import ruamel.yaml
 import sys
+import ruamel.yaml
 
 
 JTD_SCHEMA = "Specs/jtd/activity.jtd.json"
+
+
+#
+# Mark: - Functions
+#
+
+
+def check_content_activity(filename):
+    """Check the content of a YAML file for an activity"""
+    # ? Create a YAML object
+    yaml = ruamel.yaml.YAML(typ="rt")
+    yaml.indent(mapping=2, sequence=4, offset=2)
+
+    file_is_valid = True
+
+    # ? Load the YAML file
+    with open(filename, "r", encoding="utf8") as file:
+        data = yaml.load(file)
+
+    # ? Check uuid is the same as the filename
+    filename_uuid = os.path.basename(filename).split("-")[0]
+    if data["uuid"] != filename_uuid:
+        print(f"\n❌ The id in {filename} is not the same as the filename")
+        print(f"uuid:     {data['uuid']}")
+        print(f"filename: {filename_uuid}")
+        file_is_valid = False
+
+    # ? Check uuid is valid
+    try:
+        uuid.UUID(data["uuid"])
+    except ValueError:
+        print(f"\n❌ The id in {filename} is not valid")
+        print(f"uuid: {data['uuid']}")
+        file_is_valid = False
+
+    # ? Check schema validation with ajv
+    os.environ["FORCE_COLOR"] = "true"
+    cmd = (
+        f"ajv validate --verbose --all-errors --spec=jtd -s {JTD_SCHEMA} -d {filename}"
+    )
+    result = subprocess.run(cmd, shell=True, capture_output=True, check=False)
+
+    if result.returncode != 0:
+        error = result.stderr.decode("utf-8")
+        print(f"\n❌ File does not match the schema {JTD_SCHEMA}")
+        print(error)
+        file_is_valid = False
+
+    # ? Check name is the same as filename
+    filename_name = os.path.basename(filename).split("-")[-1].split(".activity.yml")[0]
+    if data["name"] != filename_name:
+        print(f"\n❌ The name in {filename} is not the same as the filename")
+        print(f"name:     {data['name']}")
+        print(f"filename: {filename_name}")
+        file_is_valid = False
+
+    return file_is_valid
 
 
 #
@@ -19,55 +77,27 @@ JTD_SCHEMA = "Specs/jtd/activity.jtd.json"
 #
 
 
-# ? Check if a file was specified
-if len(sys.argv) > 1:
-    FILENAME = sys.argv[1]
-else:
-    print("❌ No file specified")
-    exit(1)
+def main():
+    """Main function to check the content of a YAML file for an activity"""
+    # ? Check if a file was specified
+    if len(sys.argv) > 1:
+        activity_files = sys.argv[1:]
+    else:
+        print("\n❌ No file specified")
+        sys.exit(1)
 
-# ? Create a YAML object
-yaml = ruamel.yaml.YAML(typ='rt')
-yaml.indent(mapping=2, sequence=4, offset=2)
+    must_fail = False
 
-# ? Load the YAML file
-with open(FILENAME, 'r') as file:
-    data = yaml.load(file)
+    for file in activity_files:
+        file_is_valid = check_content_activity(file)
+        if file_is_valid is False:
+            must_fail = True
 
-# ? Check uuid is the same as the filename
-filename_uuid = os.path.basename(FILENAME).split("-")[0]
-if data['uuid'] != filename_uuid:
-    print(f"❌ The id in {FILENAME} is not the same as the filename")
-    print(f"uuid:     {data['uuid']}")
-    print(f"filename: {filename_uuid}")
-    exit(1)
+    if must_fail:
+        return 1
 
-# ? Check uuid is valid
-try:
-    uuid.UUID(data['uuid'])
-except ValueError:
-    print(f"❌ The id in {FILENAME} is not valid")
-    print(f"uuid: {data['uuid']}")
-    exit(1)
-
-# ? Check schema validation with ajv
-os.environ['FORCE_COLOR'] = 'true'
-cmd = f"ajv validate --verbose --all-errors --spec=jtd -s {JTD_SCHEMA} -d {FILENAME}"
-result = subprocess.run(cmd, shell=True, capture_output=True)
-
-if result.returncode != 0:
-    error = result.stderr.decode('utf-8')
-    print(f"❌ File does not match the schema {JTD_SCHEMA}")
-    print(error)
-    exit(1)
-
-# ? Check name is the same as filename
-filename_name = os.path.basename(FILENAME).split('-')[-1].split('.activity.yml')[0]
-if data['name'] != filename_name:
-    print(f"❌ The name in {FILENAME} is not the same as the filename")
-    print(f"name:     {data['name']}")
-    print(f"filename: {filename_name}")
-    exit(1)
+    return 0
 
 
-exit(0)
+if __name__ == "__main__":
+    sys.exit(main())
