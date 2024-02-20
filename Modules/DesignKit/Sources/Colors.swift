b7013c86ab0ed6d1e9c4d8dad18b5280e2ceb726
Ladislas de Toldi <ladislas@detoldi.me>
2024-02-20 14:40:53 +0100

    :lipstick: (DesignKit): Add lkGreen color

--- Modules/DesignKit/Sources/Colors.swift
+++ Modules/DesignKit/Sources/Colors.swift
@@ -8,10 +8,13 @@ public extension Color {
     static let lkBackground: Color = .init(uiColor: .systemGray6)
     static let lkStroke: Color = .init(uiColor: .systemGray)
     static let lkNavigationTitle: Color = DesignKitAsset.Colors.lekaDarkBlue.swiftUIColor
+    static let lkGreen: Color = .init(light: UIColor(displayP3Red: 175 / 255, green: 206 / 255, blue: 54 / 255, alpha: 1),
+                                      dark: UIColor(displayP3Red: 175 / 255, green: 206 / 255, blue: 54 / 255, alpha: 1))
 }
 
 public extension ShapeStyle where Self == Color {
     static var lkBackground: Color { .lkBackground }
     static var lkStroke: Color { .lkStroke }
     static var lkNavigationTitle: Color { .lkNavigationTitle }
+    static var lkGreen: Color { .lkGreen }
 }
