96bf96d575a19e16b896854c1f15821ad5be2619
Ladislas de Toldi <ladislas@detoldi.me>
2024-02-22 13:00:52 +0100

    :recycle: (ContentKit): Exercise - touch to select - add isRightAnswer coding key value

--- Modules/ContentKit/Sources/Exercise/Exercise+TouchToSelect.swift
+++ Modules/ContentKit/Sources/Exercise/Exercise+TouchToSelect.swift
@@ -32,7 +32,7 @@ public enum TouchToSelect {
         private enum CodingKeys: String, CodingKey {
             case value
             case type
-            case isRightAnswer
+            case isRightAnswer = "is_right_answer"
         }
     }
 
