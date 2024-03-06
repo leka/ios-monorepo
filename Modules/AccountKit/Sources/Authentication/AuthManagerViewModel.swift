463ae3237dbe85aae7dd42c740680fc82f35fb59
Ladislas de Toldi <ladislas@detoldi.me>
2024-03-07 00:29:38 +0100

    🌐 (AccountKit): Fix l10n by specifying bundle

--- Modules/AccountKit/Sources/Authentication/AuthManagerViewModel.swift
+++ Modules/AccountKit/Sources/Authentication/AuthManagerViewModel.swift
@@ -6,19 +6,7 @@ import Combine
 import Foundation
 import LocalizationKit
 
-// MARK: - l10n.AuthManagerViewModel
-
-// swiftlint:disable line_length
-
-extension l10n {
-    enum AuthManagerViewModel {
-        static let unverifiedEmailNotification = LocalizedString("accountkit.auth_manager_view_model.unverified_email_notification", value: "Your email hasn't been verified yet. Please verify your email to avoid losing your data.", comment: "Unverified email notification message")
-    }
-}
-
 // MARK: - AuthManagerViewModel
-
-// swiftlint:enable line_length
 
 public class AuthManagerViewModel: ObservableObject {
     // MARK: Lifecycle
@@ -107,3 +95,18 @@ public class AuthManagerViewModel: ObservableObject {
         self.showErrorAlert = false
     }
 }
+
+// MARK: - l10n.AuthManagerViewModel
+
+// swiftlint:disable line_length
+
+extension l10n {
+    enum AuthManagerViewModel {
+        static let unverifiedEmailNotification = LocalizedString("accountkit.auth_manager_view_model.unverified_email_notification",
+                                                                 bundle: AccountKitResources.bundle,
+                                                                 value: "Your email hasn't been verified yet. Please verify your email to avoid losing your data.",
+                                                                 comment: "Unverified email notification message")
+    }
+}
+
+// swiftlint:enable line_length
