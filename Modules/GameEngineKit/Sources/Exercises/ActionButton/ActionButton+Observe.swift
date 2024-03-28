ad11403fe414db22bef0d4cb1990e3d49beb4ec5
Hugo Pezziardi <hugo@pezziardi.net>
2024-03-28 18:13:11 +0100

    ♻️ (GEK): Update ActionButton+Observe with Bundle changes

--- Modules/GameEngineKit/Sources/Exercises/ActionButton/ActionButton+Observe.swift
+++ Modules/GameEngineKit/Sources/Exercises/ActionButton/ActionButton+Observe.swift
@@ -4,6 +4,7 @@
 
 import DesignKit
 import LocalizationKit
+import SVGView
 import SwiftUI
 
 // MARK: - AnimatableBlur
@@ -41,52 +42,93 @@ struct AnimatableSaturation: AnimatableModifier {
 // MARK: - ActionButtonObserve
 
 struct ActionButtonObserve: View {
+    // MARK: Lifecycle
+
+    init(image: String, imageWasTapped: Binding<Bool>) {
+        if let path = Bundle.path(forImage: image) {
+            log.debug("Image found at path: \(path)")
+            self.image = path
+        } else {
+            log.error("Image not found: \(image)")
+            self.image = image
+        }
+        self._imageWasTapped = imageWasTapped
+    }
+
+    // MARK: Internal
+
     let image: String
 
     @Binding var imageWasTapped: Bool
 
     var body: some View {
-        if let uiImage = UIImage(named: image) {
-            Button {
-                withAnimation {
-                    self.imageWasTapped = true
-                }
-            } label: {
-                VStack {
-                    Image(systemName: "hand.tap")
-                        .resizable()
-                        .frame(width: 70, height: 70)
-                        .foregroundColor(.white)
-                    Text(l10n.ActionButtonObserve.buttonLabel)
-                        .font(.title)
-                        .foregroundColor(.white)
-                }
-                .transition(.opacity)
-                .opacity(self.imageWasTapped ? 0 : 1)
-                .frame(width: 460, height: 460)
-                .contentShape(RoundedRectangle(cornerRadius: 10))
+        Button {
+            withAnimation {
+                self.imageWasTapped = true
             }
-            .disabled(self.imageWasTapped)
-            .background {
-                Image(uiImage: uiImage)
+        } label: {
+            VStack {
+                Image(systemName: "hand.tap")
+                    .resizable()
+                    .frame(width: 70, height: 70)
+                    .foregroundColor(.white)
+                Text(l10n.ActionButtonObserve.buttonLabel)
+                    .font(.title)
+                    .foregroundColor(.white)
+            }
+            .transition(.opacity)
+            .opacity(self.imageWasTapped ? 0 : 1)
+            .frame(width: 460, height: 460)
+            .contentShape(RoundedRectangle(cornerRadius: 10))
+        }
+        .disabled(self.imageWasTapped)
+        .background {
+            if !FileManager.default.fileExists(atPath: self.image) {
+                self.imageNotFound()
+            }
+
+            if self.image.isRasterImageFile {
+                Image(uiImage: UIImage(named: self.image)!)
                     .resizable()
                     .frame(width: 460, height: 460)
                     .clipShape(RoundedRectangle(cornerRadius: 10))
                     .modifier(AnimatableBlur(blurRadius: self.imageWasTapped ? 0 : 20))
                     .modifier(AnimatableSaturation(saturation: self.imageWasTapped ? 1 : 0))
             }
-        } else {
-            Text(l10n.ActionButtonObserve.imageUnknownError(self.image))
-                .multilineTextAlignment(.center)
-                .overlay {
-                    Circle()
-                        .stroke(Color.red, lineWidth: 5)
-                }
-                .frame(
-                    width: 460,
-                    height: 460
-                )
+
+            if self.image.isVectorImageFile {
+                SVGView(contentsOf: URL(fileURLWithPath: self.image))
+                    .frame(width: 460, height: 460)
+                    .background(self.choiceBackgroundColor)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .modifier(AnimatableBlur(blurRadius: self.imageWasTapped ? 0 : 20))
+                    .modifier(AnimatableSaturation(saturation: self.imageWasTapped ? 1 : 0))
+            }
         }
+    }
+
+    // MARK: Private
+
+    private let choiceBackgroundColor: Color = .init(
+        light: .white,
+        dark: UIColor(displayP3Red: 242 / 255, green: 242 / 255, blue: 247 / 255, alpha: 1.0)
+    )
+
+    private func imageNotFound() -> some View {
+        Text("Image not found: \(self.image)")
+            .multilineTextAlignment(.center)
+            .frame(
+                width: 460,
+                height: 460
+            )
+            .background(self.choiceBackgroundColor)
+            .overlay {
+                Circle()
+                    .stroke(Color.red, lineWidth: 5)
+            }
+            .onAppear {
+                log.error("Image not found: \(self.image)")
+            }
     }
 }
 
