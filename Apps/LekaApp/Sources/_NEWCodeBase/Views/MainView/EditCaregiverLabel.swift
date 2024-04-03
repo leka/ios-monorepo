2c5646542bffcc3de1d962c7628cd41125caf622
mathieu J <macteuts@gmail.com>
2024-04-03 20:28:27 +0300

    🐛 (LekaApp): Move persistence logic to MainView

--- Apps/LekaApp/Sources/_NEWCodeBase/Views/MainView/EditCaregiverLabel.swift
+++ Apps/LekaApp/Sources/_NEWCodeBase/Views/MainView/EditCaregiverLabel.swift
@@ -87,51 +87,14 @@ struct EditCaregiverLabel: View {
                 }
             }
         }
-        .onAppear {
-            self.persistentDataManager.checkInactivity()
-        }
-        .onChange(of: self.scenePhase) { newPhase in
-            switch newPhase {
-                case .active:
-                    self.persistentDataManager.checkInactivity()
-                case .inactive,
-                     .background:
-                    self.persistentDataManager.updateLastActiveTimestamp()
-                    if let currentCaregiverID = self.caregiverManagerViewModel.currentCaregiver?.id {
-                        self.persistentDataManager.lastActiveCaregiverID = currentCaregiverID
-                    }
-                @unknown default:
-                    break
-            }
-        }
-        .onReceive(self.persistentDataManager.inactivityTimeoutPublisher) { isTimedOut in
-            if isTimedOut {
-                self.caregiverManager.resetCurrentCaregiver()
-                guard self.navigation.sheetContent == nil, self.navigation.fullScreenCoverContent == nil else {
-                    return
-                }
-                self.navigation.sheetContent = .caregiverPicker
-            } else {
-                guard let storedCaregiverID = self.persistentDataManager.lastActiveCaregiverID else {
-                    self.navigation.sheetContent = .caregiverPicker
-                    return
-                }
-                self.caregiverManager.setCurrentCaregiver(byID: storedCaregiverID)
-            }
-        }
     }
 
     // MARK: Private
-
-    @Environment(\.scenePhase) private var scenePhase
 
     @ObservedObject private var styleManager: StyleManager = .shared
     @ObservedObject private var navigation: Navigation = .shared
 
     @StateObject private var caregiverManagerViewModel = CaregiverManagerViewModel()
-
-    private var persistentDataManager: PersistentDataManager = .shared
-    private var caregiverManager: CaregiverManager = .shared
 }
 
 // MARK: - l10n.ChangeCaregiverProfile
