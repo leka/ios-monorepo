6ef0da04f37d7701487966c6efb93d0b83c5c1fc
Ladislas de Toldi <ladislas@detoldi.me>
2024-04-05 15:54:39 +0200

    ♻️ (content): Activity - review only l10n/details

--- Tools/Content/activity_chatgpt_review.py
+++ Tools/Content/activity_chatgpt_review.py
@@ -9,16 +9,28 @@ YAML filesin a Git repository using OpenAI's GPT-4 model.
 # Copyright APF France handicap
 # SPDX-License-Identifier: Apache-2.0
 
+import io
 import os
 import sys
 import subprocess
 import argparse
 import logging
+import ruamel.yaml
+from ruamel.yaml.error import YAMLError
 from openai import OpenAI
 
 # Define global variables
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 GITHUB_ENV_FILE = os.getenv("GITHUB_ENV")
+
+# Configure YAML parser globally if the configuration doesn't need to be dynamic per file
+yaml = ruamel.yaml.YAML(typ="rt")
+yaml.indent(mapping=2, sequence=4, offset=2)
+yaml.preserve_quotes = True
+yaml.representer.add_representer(
+    type(None),
+    lambda dumper, data: dumper.represent_scalar("tag:yaml.org,2002:null", "null"),
+)
 
 # Setup basic logging
 logging.basicConfig(
@@ -39,7 +51,15 @@ def get_modified_files(content_type):
     try:
         # Run git diff command to get list of modified files
         result = subprocess.run(
-            ["git", "diff", "--name-only", "-r", "HEAD^1", "HEAD"],
+            [
+                "git",
+                "diff",
+                "--name-only",
+                "-r",
+                "HEAD^1",
+                "HEAD",
+                "--diff-filter=AMCR",
+            ],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
@@ -64,11 +84,40 @@ def read_file_content(file_path):
     Read and return the content of a file.
     """
     try:
-        with open(file_path, "r", encoding="utf8") as file:
+        with open(file_path, "r") as file:
             return file.read()
     except Exception as e:
         logging.error(f"Error reading file {file_path}: {e}")
         return None
+
+
+def read_yaml_data(file_path):
+    """
+    Reads and returns the 'l10n' section from a YAML file.
+
+    :param file_path: Path to the YAML file to be read.
+    :return: YAML string of the 'l10n' section if present, None otherwise.
+    """
+    try:
+        with open(file_path, "r", encoding="utf8") as file:
+            content = file.read()
+    except IOError as e:
+        logging.error(f"Error opening/reading file {file_path}: {e}")
+        return None
+
+    try:
+        data = yaml.load(content)
+    except YAMLError as e:
+        logging.error(f"Error parsing YAML content from {file_path}: {e}")
+        return None
+
+    l10n_data = data.get("l10n")
+    if l10n_data:
+        stream = io.StringIO("")
+        yaml.dump(l10n_data, stream)
+        return stream.getvalue()
+
+    return None
 
 
 def review_files_with_gpt(prompt_template, files):
@@ -77,11 +126,11 @@ def review_files_with_gpt(prompt_template, files):
     """
     client = OpenAI(api_key=OPENAI_API_KEY)
     responses = []
-    for file_path in files:
-        file_content = read_file_content(file_path)
+    for index, file_path in enumerate(files):
+        file_content = read_yaml_data(file_path)
         if file_content:
             try:
-                logging.info(f"Reviewing file {file_path}")
+                logging.info(f"Reviewing file {os.path.basename(file_path)}")
                 response = client.chat.completions.create(
                     model="gpt-4-turbo-preview",
                     messages=[
@@ -95,11 +144,28 @@ def review_files_with_gpt(prompt_template, files):
                     presence_penalty=0.0,
                 )
                 response_text = response.choices[0].message.content.strip()
-                responses.append(f"## Review for `{file_path}`:\n\n{response_text}")
-                logging.info(f"Reviewed file {file_path}")
+                responses.append(
+                    (
+                        f"## Activity Review {index + 1}\n\n"
+                        "```\n"
+                        f"{os.path.basename(file_path)}\n"
+                        "```\n\n"
+                        f"{response_text}"
+                    )
+                )
+                logging.info(f"Reviewed file {os.path.basename(file_path)}")
             except Exception as e:
                 logging.error(f"Error reviewing file {file_path} with GPT: {e}")
-    return "\n\n".join(responses)
+
+    formatted_files = "\n".join(f"- {os.path.basename(file)}" for file in files)
+    review_text = (
+        "## Files reviewed\n\n"
+        "```\n"
+        f"{formatted_files}\n"
+        "```\n\n"
+        f"{"\n\n".join(responses)}"
+    )
+    return review_text
 
 
 def main():
