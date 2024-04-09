34a71e8422a447b00cce29a4f41eeab62c815e67
mathieu J <macteuts@gmail.com>
2024-04-09 10:12:41 +0300

    ✨ (AccountKit): ActivityCompletionDataManager pre-implementation

---
+++ Modules/AccountKit/Sources/Managers/ActivityCompletionDataManager.swift
@@ -0,0 +1,51 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Combine
+
+public class ActivityCompletionDataManager {
+    // MARK: Lifecycle
+
+    private init() {
+        self.initializeActivityCompletionDataListener()
+    }
+
+    // MARK: Public
+
+    public static let shared = ActivityCompletionDataManager()
+
+    public func initializeActivityCompletionDataListener() {
+        // TODO: (@macteuts) observe ActivityCompletiondata for the whole User (i.e. RootOwner, data for all owned profiles)
+    }
+
+    public func fetchActivityCompletionData(CarereceiverID _: String) {
+        // TODO: (@macteuts) fetch ActivityCompletionData for a specific profile (i.e. Carereceiver)
+    }
+
+//    public func saveActivityCompletionData(data: [ExerciseCompletionData]) -> AnyPublisher<[ExerciseCompletionData], Error> {
+//        // TODO: (@macteuts) save ActivityCompletionData when an Activity completes
+//    }
+
+    // MARK: Internal
+
+    var rootOwnerCompletionData: AnyPublisher<[ExerciseCompletionData], Never> {
+        self.allRootOwnerActivityCompletionData.eraseToAnyPublisher()
+    }
+
+    var carereceiverCompletionData: AnyPublisher<[ExerciseCompletionData], Never> {
+        self.activityCompletionDataPerCarereceiver.eraseToAnyPublisher()
+    }
+
+    var fetchErrorPublisher: AnyPublisher<Error, Never> {
+        self.fetchErrorSubject.eraseToAnyPublisher()
+    }
+
+    // MARK: Private
+
+    private var allRootOwnerActivityCompletionData = CurrentValueSubject<[ExerciseCompletionData], Never>([])
+    private var activityCompletionDataPerCarereceiver = CurrentValueSubject<[ExerciseCompletionData], Never>([])
+    private var fetchErrorSubject = PassthroughSubject<Error, Never>()
+    private let dbOps = DatabaseOperations.shared
+    private var cancellables = Set<AnyCancellable>()
+}
