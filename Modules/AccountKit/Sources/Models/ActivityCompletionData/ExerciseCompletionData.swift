8ef683b12022b38c984826eab8a3f1008ae74f81
mathieu J. <macteuts@gmail.com>
2024-04-29 14:49:32 +0300

    🏷️ (AccountKit): Add ExerciseCompletionData Type to AK Models

---
+++ Modules/AccountKit/Sources/Models/ActivityCompletionData/ExerciseCompletionData.swift
@@ -0,0 +1,22 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Foundation
+
+public struct ExerciseCompletionData: Equatable {
+    // MARK: Lifecycle
+
+    public init(
+        startTimestamp: Date? = Date(),
+        endTimestamp: Date? = nil
+    ) {
+        self.startTimestamp = startTimestamp
+        self.endTimestamp = endTimestamp
+    }
+
+    // MARK: Public
+
+    public var startTimestamp: Date?
+    public var endTimestamp: Date?
+}
