8af9f77601e8cc7831498efd6da43720b3b8f03f
Ladislas de Toldi <ladislas@detoldi.me>
2024-05-28 12:22:04 +0200

    ⬆️ (tuist): Upgrade dependencies (except Firebase)

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/StarryInternet/CombineCoreBluetooth",
-            exact: "0.7.2"
+            exact: "0.8.0"
         ),
         .package(
             url: "https://github.com/michael94ellis/SwiftUIJoystick",
@@ -52,7 +52,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/OlehKorchytskyi/Fit",
-            exact: "1.0.0"
+            exact: "1.0.1"
         ),
         .package(
             url: "https://github.com/exyte/SVGView",
@@ -60,7 +60,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/devicekit/DeviceKit",
-            exact: "5.2.3"
+            exact: "5.3.0"
         ),
     ]
 )
