e44dc396da7d0cc7f2a56137274726298385b086
Yann Locatelli <yann.locatelli2@gmail.com>
2024-06-24 16:58:36 +0200

    :sparkles: (RobotKit): Add magicCard property to Robot
    Linked to magic card characteristic

---
+++ Modules/RobotKit/Sources/Robot+MagicCards.swift
@@ -0,0 +1,48 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import BLEKit
+import Combine
+
+extension Robot {
+    func registerMagicCardsNotificationCallback() {
+        let characteristic = CharacteristicModelNotifying(
+            characteristicUUID: BLESpecs.MagicCard.Characteristics.idLanguage,
+            serviceUUID: BLESpecs.MagicCard.service,
+            onNotification: { data in
+                if let data {
+                    let id = data.dropLast().map { UInt16($0) }.reduce(0) { $0 << 8 + $1 }
+                    let language = MagicCard.Language(rawValue: data.last!)!
+
+                    let card = MagicCard(language: language, id: id)
+                    self.magicCard.send(card)
+
+                    log.trace("🤖 magicCard: language \(language.rawValue), id \(String(format: "%04X", id))")
+                }
+            }
+        )
+
+        connectedPeripheral?.notifyingCharacteristics.insert(characteristic)
+    }
+
+    func registerMagicCardsReadCallback() {
+        let characteristic = CharacteristicModelReadOnly(
+            characteristicUUID: BLESpecs.MagicCard.Characteristics.idLanguage,
+            serviceUUID: BLESpecs.MagicCard.service,
+            onRead: { data in
+                if let data {
+                    let id = data.dropLast().map { UInt16($0) }.reduce(0) { $0 << 8 + $1 }
+                    let language = MagicCard.Language(rawValue: data.last!)!
+
+                    let card = MagicCard(language: language, id: id)
+                    self.magicCard.send(card)
+
+                    log.trace("🤖 magicCard: language \(language.rawValue), id \(String(format: "%04X", id))")
+                }
+            }
+        )
+
+        connectedPeripheral?.readOnlyCharacteristics.insert(characteristic)
+    }
+}
