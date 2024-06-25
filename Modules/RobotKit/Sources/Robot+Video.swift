c7b2da0d45c72d2c1f6c8190b40fedacf19c8d73
Yann Locatelli <yann.locatelli2@gmail.com>
2024-06-25 17:25:58 +0200

    :sparkles: (RobotKit): Display image on robot

---
+++ Modules/RobotKit/Sources/Robot+Video.swift
@@ -0,0 +1,26 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import UtilsKit
+
+public extension Robot {
+    enum Video {
+        static let id: UInt8 = 0x80
+    }
+
+    func display(imageID: UInt16) {
+        log.trace("🤖 DISPLAY image id\(imageID)")
+
+        var command: [UInt8] = []
+
+        command.append(imageID.highByte)
+        command.append(imageID.lowByte)
+        command.append(command.checksum8)
+        command.insert(Video.id, at: 0)
+
+        let output = Self.commandGenerator(commands: command)
+
+        connectedPeripheral?.sendCommand(output)
+    }
+}
