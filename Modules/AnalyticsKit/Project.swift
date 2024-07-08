2107ea7ecca36d019ae67e49968c8bb9ef442f4d
Ladislas de Toldi <ladislas@detoldi.me>
2024-07-08 17:29:02 +0200

    ✨ (modules): Add AnalyticsKit module (empty)

---
+++ Modules/AnalyticsKit/Project.swift
@@ -0,0 +1,20 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+// swiftformat:disable acronyms
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.module(
+    name: "AnalyticsKit",
+    examples: [
+        ModuleExample(
+            name: "AnalyticsKitExample"
+        ),
+    ],
+    dependencies: [
+        .project(target: "LogKit", path: Path("../../Modules/LogKit")),
+    ]
+)
