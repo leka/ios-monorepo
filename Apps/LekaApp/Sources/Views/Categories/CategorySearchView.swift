ed87c50fad281b2dfc9c9000a7a4a39a2d519df1
Hugo Pezziardi <hugo@pezziardi.net>
2024-08-21 14:09:05 +0200

    🚸 (LekaApp): Use Ifrit searchSync() function

--- Apps/LekaApp/Sources/Views/Categories/CategorySearchView.swift
+++ Apps/LekaApp/Sources/Views/Categories/CategorySearchView.swift
@@ -43,31 +43,31 @@ struct CategorySearchView: View {
         if self.searchText.isEmpty {
             return self.activities
         } else {
-            let bestResultSorted = self.activities.sorted { activity1, activity2 in
+            let normalizedSearchText = self.searchText.normalized()
 
-                let score1 = self.fuse.search(self.searchText.normalized(), in: activity1.details.title.normalized())?.score ?? 1
-                let score2 = self.fuse.search(self.searchText.normalized(), in: activity2.details.title.normalized())?.score ?? 1
-
-                return score1 < score2
-            }
-
-            let bestResultFiltered = bestResultSorted.filter { activity in
-                if let score = self.fuse.search(self.searchText.normalized(), in: activity.details.title.normalized())?.score {
+            let fuzzyMatches = self.activities.filter { activity in
+                if let score = self.fuse.searchSync(normalizedSearchText, in: activity.details.title.normalized())?.score {
                     return score < self.tolerance
                 }
                 return false
             }
 
-            let bestResultByTag = self.activities.filter { activity in
+            let tagMatches = self.activities.filter { activity in
                 activity.tags.contains { tag in
-                    if let score = self.fuse.search(self.searchText.normalized(), in: tag.name.normalized())?.score {
+                    if let score = self.fuse.searchSync(self.searchText.normalized(), in: tag.name.normalized())?.score {
                         return score < self.tolerance
                     }
                     return false
                 }
             }
 
-            return Array(Set(bestResultFiltered + bestResultByTag))
+            let combinedResults = Array(Set(fuzzyMatches + tagMatches))
+
+            return combinedResults.sorted { activity1, activity2 in
+                let score1 = self.fuse.searchSync(normalizedSearchText, in: activity1.details.title.normalized())?.score ?? 1
+                let score2 = self.fuse.searchSync(normalizedSearchText, in: activity2.details.title.normalized())?.score ?? 1
+                return score1 < score2
+            }
         }
     }
 
@@ -75,15 +75,21 @@ struct CategorySearchView: View {
         if self.searchText.isEmpty {
             return self.skills
         } else {
-            let bestResultSorted = self.skills.sorted { skill1, skill2 in
-                self.fuse.search(self.searchText.normalized(), in: skill1.name.normalized())?.score ?? 1 <
-                    self.fuse.search(self.searchText.normalized(), in: skill2.name.normalized())?.score ?? 1
-            }
-            return bestResultSorted.filter { skill in
-                if let score = self.fuse.search(self.searchText.normalized(), in: skill.name.normalized())?.score {
+            let normalizedSearchText = self.searchText.normalized()
+
+            let fuzzyMatches = self.skills.filter { skill in
+                if let score = self.fuse.searchSync(normalizedSearchText, in: skill.name.normalized())?.score {
                     return score < self.tolerance
                 }
                 return false
+            }
+
+            return fuzzyMatches.sorted { skill1, skill2 in
+
+                let score1 = self.fuse.searchSync(normalizedSearchText, in: skill1.name.normalized())?.score ?? 1
+                let score2 = self.fuse.searchSync(normalizedSearchText, in: skill2.name.normalized())?.score ?? 1
+
+                return score1 < score2
             }
         }
     }
@@ -92,25 +98,31 @@ struct CategorySearchView: View {
         if self.searchText.isEmpty {
             return self.curriculums
         } else {
-            let bestResultSorted = self.curriculums.sorted { curriculum1, curriculum2 in
-                self.fuse.search(self.searchText.normalized(), in: curriculum1.name.normalized())?.score ?? 1 <
-                    self.fuse.search(self.searchText.normalized(), in: curriculum2.name.normalized())?.score ?? 1
-            }
-            let bestResultFiltered = bestResultSorted.filter { curriculum in
-                if let score = self.fuse.search(self.searchText.normalized(), in: curriculum.name.normalized())?.score {
+            let normalizedSearchText = self.searchText.normalized()
+
+            let fuzzyMatches = self.curriculums.filter { curriculum in
+                if let score = self.fuse.searchSync(normalizedSearchText, in: curriculum.name.normalized())?.score {
                     return score < self.tolerance
                 }
                 return false
             }
-            let bestResultByTag = self.curriculums.filter { curriculum in
+
+            let tagMatches = self.curriculums.filter { curriculum in
                 curriculum.tags.contains { tag in
-                    if let score = self.fuse.search(self.searchText.normalized(), in: tag.name.normalized())?.score {
+                    if let score = self.fuse.searchSync(normalizedSearchText, in: tag.name.normalized())?.score {
                         return score < self.tolerance
                     }
                     return false
                 }
             }
-            return Array(Set(bestResultFiltered + bestResultByTag))
+
+            let combinedResults = Array(Set(fuzzyMatches + tagMatches))
+
+            return combinedResults.sorted { curriculum1, curriculum2 in
+                let score1 = self.fuse.searchSync(normalizedSearchText, in: curriculum1.name.normalized())?.score ?? 1
+                let score2 = self.fuse.searchSync(normalizedSearchText, in: curriculum2.name.normalized())?.score ?? 1
+                return score1 < score2
+            }
         }
     }
 
@@ -120,7 +132,7 @@ struct CategorySearchView: View {
     private let curriculums: [Curriculum] = ContentKit.allPublishedCurriculums
     private let skills: [Skill] = Skills.primarySkillsList
     private let tags: [Tag] = Tags.primaryTagsList
-    private let tolerance: Double = 0.5
+    private let tolerance: Double = 0.6
     private let fuse = Fuse()
 
     @State private var searchText = ""
