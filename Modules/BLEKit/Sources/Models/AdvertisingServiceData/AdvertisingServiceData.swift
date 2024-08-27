badc25cc9f3702b426eb405a533afd8a6b3463ba
Yann Locatelli <yann.locatelli2@gmail.com>
2024-08-27 15:27:54 +0200

    ♻️ (BLEKit): Modular AdvertisingServiceData depending on version

--- Modules/BLEKit/Sources/Models/AdvertisingServiceData/AdvertisingServiceData.swift
+++ Modules/BLEKit/Sources/Models/AdvertisingServiceData/AdvertisingServiceData.swift
@@ -10,47 +10,29 @@ struct AdvertisingServiceData {
     // MARK: Lifecycle
 
     init(data: Data) {
-        self.battery = getBattery(data: data)
-        self.isCharging = getChargingState(data: data)
-        self.osVersion = getOsVersion(data: data)
+        let dataCount = data.count
+
+        let currentAdvertisingServiceData: any AdvertisingServiceDataProtocol = switch dataCount {
+            case 2:
+                AdvertisingServiceDataV100(data: data)
+            case 6:
+                AdvertisingServiceDataV130(data: data)
+            case 5:
+                AdvertisingServiceDataV200(data: data)
+            default:
+                AdvertisingServiceDataV100(data: data)
+        }
+
+        self.osVersion = currentAdvertisingServiceData.osVersion
+        self.battery = currentAdvertisingServiceData.battery
+        self.isCharging = currentAdvertisingServiceData.isCharging
+        self.isDeepSleeping = currentAdvertisingServiceData.isDeepSleeping
     }
 
     // MARK: Internal
 
+    let osVersion: String?
     let battery: Int
     let isCharging: Bool
-    let osVersion: String?
-}
-
-// MARK: - AdvertisingServiceDataIndex
-
-private enum AdvertisingServiceDataIndex {
-    static let battery = 0
-    static let isCharging = 1
-    static let osVersionMajor = 2
-    static let osVersionMinor = 3
-    static let osVersionRevisionHighByte = 4
-    static let osVersionRevisionLowByte = 5
-}
-
-private func getBattery(data: Data) -> Int {
-    Int(data[AdvertisingServiceDataIndex.battery])
-}
-
-private func getChargingState(data: Data) -> Bool {
-    data[AdvertisingServiceDataIndex.isCharging] == 0x01
-}
-
-private func getOsVersion(data: Data) -> String? {
-    guard data.count == 6 else {
-        return nil
-    }
-
-    let major = data[AdvertisingServiceDataIndex.osVersionMajor]
-    let minor = data[AdvertisingServiceDataIndex.osVersionMinor]
-    let revision =
-        Int(data[AdvertisingServiceDataIndex.osVersionRevisionHighByte]) << 8
-            + Int(data[AdvertisingServiceDataIndex.osVersionRevisionLowByte])
-
-    return "\(major).\(minor).\(revision)"
+    let isDeepSleeping: Bool?
 }
