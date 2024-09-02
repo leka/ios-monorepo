8dcceb91d59976a889760bfe4be16356784b1785
Ladislas de Toldi <ladislas@detoldi.me>
2024-09-02 12:15:58 +0200

    ⚗️ (GEKExample): Set up project, navigation

--- Modules/GameEngineKit/Examples/GameEngineKitExample/Sources/ContentView.swift
+++ Modules/GameEngineKit/Examples/GameEngineKitExample/Sources/ContentView.swift
@@ -7,7 +7,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World")
+        NavigationStack {
+            Text("Choose your gameplay!")
+                .font(.largeTitle)
+                .padding()
+
+            VStack(spacing: 20) {
+                NavigationLink("Find The Right Answers", destination: {
+                    Text("Find The Right Answers")
+                        .font(.largeTitle)
+                })
+                .tint(.orange)
+                .buttonStyle(.borderedProminent)
+
+                NavigationLink("Find The Right Order", destination: {
+                    Text("Find The Right Order")
+                        .font(.largeTitle)
+                })
+                .tint(.green)
+                .buttonStyle(.borderedProminent)
+
+                NavigationLink("Associate Categories", destination: {
+                    Text("Associate Categories")
+                        .font(.largeTitle)
+                })
+                .tint(.cyan)
+                .buttonStyle(.borderedProminent)
+            }
+        }
     }
 }
 
