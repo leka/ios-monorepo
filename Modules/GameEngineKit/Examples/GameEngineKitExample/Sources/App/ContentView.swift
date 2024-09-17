54cf3fcf77a15c511271aa6accbe99ecf9316048
mathieu J. <macteuts@gmail.com>
2024-09-17 13:16:44 +0300

    ✨ (GEK): Update ContentView with new ex AssociateCategory

--- Modules/GameEngineKit/Examples/GameEngineKitExample/Sources/App/ContentView.swift
+++ Modules/GameEngineKit/Examples/GameEngineKitExample/Sources/App/ContentView.swift
@@ -39,30 +39,33 @@ struct ContentView: View {
                     .tint(.pink)
                     .buttonStyle(.borderedProminent)
                     .frame(maxWidth: .infinity)
+
+                    NavigationLink("Find The Right Order", destination: {
+                        let gameplay = GameplayFindTheRightOrder(choices: TTSCoordinatorFindTheRightOrder.kDefaultChoices)
+                        let coordinator = TTSCoordinatorFindTheRightOrder(gameplay: gameplay)
+                        let viewModel = TTSViewViewModel(coordinator: coordinator)
+
+                        return TTSView(viewModel: viewModel)
+                            .navigationTitle("Find The Right Order")
+                            .navigationBarTitleDisplayMode(.large)
+                    })
+                    .tint(.green)
+                    .buttonStyle(.borderedProminent)
+                    .frame(maxWidth: .infinity)
+
+                    NavigationLink("Associate Categories", destination: {
+                        let gameplay = GameplayAssociateCategories(choices: TTSCoordinatorAssociateCategories.kDefaultChoices)
+                        let coordinator = TTSCoordinatorAssociateCategories(gameplay: gameplay)
+                        let viewModel = TTSViewViewModel(coordinator: coordinator)
+
+                        return TTSView(viewModel: viewModel)
+                            .navigationTitle("Associate Categories")
+                            .navigationBarTitleDisplayMode(.large)
+                    })
+                    .tint(.cyan)
+                    .buttonStyle(.borderedProminent)
+                    .frame(maxWidth: .infinity)
                 }
-
-                NavigationLink("Find The Right Order", destination: {
-                    let gameplay = GameplayFindTheRightOrder(choices: TTSCoordinatorFindTheRightOrder.kDefaultChoices)
-                    let coordinator = TTSCoordinatorFindTheRightOrder(gameplay: gameplay)
-                    let viewModel = TTSViewViewModel(coordinator: coordinator)
-
-                    return TTSView(viewModel: viewModel)
-                        .navigationTitle("Find The Right Order")
-                        .navigationBarTitleDisplayMode(.large)
-                })
-                .tint(.green)
-                .buttonStyle(.borderedProminent)
-                .frame(maxWidth: .infinity)
-
-                NavigationLink("Associate Categories", destination: {
-                    Text("Find the right categories: (1/3), (2/5), (4/6)")
-                        .font(.title)
-                        .navigationTitle("Associate Categories")
-                        .navigationBarTitleDisplayMode(.large)
-                })
-                .tint(.cyan)
-                .buttonStyle(.borderedProminent)
-                .frame(maxWidth: .infinity)
             }
 
             Text("Or choose a template")
