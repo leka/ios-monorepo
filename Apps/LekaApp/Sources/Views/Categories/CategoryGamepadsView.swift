b32de0166b4ba50ca847389da6f1543004190fa2
Hugo Pezziardi <hugo@pezziardi.net>
2024-09-17 18:47:45 +0200

    💄 (LekaApp x ContentKit): Upgrade Gamepad category section

--- Apps/LekaApp/Sources/Views/Categories/CategoryGamepadsView.swift
+++ Apps/LekaApp/Sources/Views/Categories/CategoryGamepadsView.swift
@@ -15,33 +15,36 @@ struct CategoryGamepadsView: View {
 
     var body: some View {
         VStack {
-            ScrollView(showsIndicators: true) {
-                HStack(alignment: .center, spacing: 30) {
-                    Image(systemName: "gamecontroller")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 80, height: 80)
-                        .foregroundStyle(self.styleManager.accentColor!)
+            HStack(alignment: .center, spacing: 30) {
+                Image(systemName: "gamecontroller")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 80, height: 80)
+                    .foregroundStyle(self.styleManager.accentColor!)
 
-                    VStack(alignment: .leading) {
-                        Text(self.category.details.subtitle)
-                            .font(.title2)
+                VStack(alignment: .leading) {
+                    Text(self.category.details.subtitle)
+                        .font(.title2)
 
-                        Text(self.category.details.description)
-                            .foregroundStyle(.secondary)
-                    }
+                    Text(self.category.details.description)
+                        .foregroundStyle(.secondary)
                 }
-                .padding(.horizontal)
-
-                ActivityGridView(activities: self.category.activities, onStartActivity: { activity in
-                    if self.authManagerViewModel.userAuthenticationState == .loggedIn, !self.navigation.demoMode {
-                        self.navigation.sheetContent = .carereceiverPicker(activity: activity, story: nil)
-                    } else {
-                        self.navigation.currentActivity = activity
-                        self.navigation.fullScreenCoverContent = .activityView(carereceivers: [])
-                    }
-                })
             }
+            .padding(.horizontal)
+            .padding(.bottom)
+
+            Spacer()
+
+            GamepadGridView(gamepads: self.category.activities, onStartGamepad: { activity in
+                if self.authManagerViewModel.userAuthenticationState == .loggedIn, !self.navigation.demoMode {
+                    self.navigation.sheetContent = .carereceiverPicker(activity: activity, story: nil)
+                } else {
+                    self.navigation.currentActivity = activity
+                    self.navigation.fullScreenCoverContent = .activityView(carereceivers: [])
+                }
+            })
+
+            Spacer()
         }
         .navigationTitle(self.category.details.title)
     }
