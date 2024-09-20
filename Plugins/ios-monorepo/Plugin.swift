2c1248d6f34821b2e83ff580bf39e2ed48afa4c4
Ladislas de Toldi <ladislas@detoldi.me>
2024-09-20 08:29:33 +0200

    🔥 (tuist): Remove Plugins directory

--- Plugins/ios-monorepo/Plugin.swift
+++
@@ -1,7 +0,0 @@
-// Leka - iOS Monorepo
-// Copyright APF France handicap
-// SPDX-License-Identifier: Apache-2.0
-
-import ProjectDescription
-
-let plugin = Plugin(name: "MyPlugin")
--- Plugins/ios-monorepo/Sources/tuist-my-cli/main.swift
+++
@@ -1,5 +0,0 @@
-// Leka - iOS Monorepo
-// Copyright APF France handicap
-// SPDX-License-Identifier: Apache-2.0
-
-print("Hello, from your Tuist Task")
