8cec9be8020bca10df7afb3d974684753951bcea
mathieu J. <macteuts@gmail.com>
2024-10-01 10:14:15 +0300

    🏷️ (AccountKit): Add Library & SavedItems Models and inits

---
+++ Modules/AccountKit/Sources/Models/RootAccount/Library/SavedCurriculum/SavedCurriculum+Init.swift
@@ -0,0 +1,19 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import SwiftUI
+
+public extension SavedCurriculum {
+    init(
+        id: String? = nil,
+        rootOwnerUid: String = "",
+        caregiverID: String
+    ) {
+        self.id = id
+        self.rootOwnerUid = rootOwnerUid
+        self.caregiverID = caregiverID
+        self.createdAt = nil
+        self.lastEditedAt = nil
+    }
+}
