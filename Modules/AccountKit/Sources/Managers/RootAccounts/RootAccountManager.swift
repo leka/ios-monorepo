6dd5b2ee7ffb28d391cf26ab1ba7007ad6fdeb78
mathieu J. <macteuts@gmail.com>
2024-10-01 15:32:50 +0300

    ✨ (AccountKit): Update RootAccountManager + VM for SavedActivity

--- Modules/AccountKit/Sources/Managers/RootAccounts/RootAccountManager.swift
+++ Modules/AccountKit/Sources/Managers/RootAccounts/RootAccountManager.swift
@@ -8,26 +8,97 @@ public class RootAccountManager {
     // MARK: Lifecycle
 
     private init() {
-        // Nothing to do
+        self.initializeRootAccountListener()
     }
 
     // MARK: Public
 
     public static let shared = RootAccountManager()
 
-    public func createRootAccount(rootAccount: RootAccount) {
-        self.dbOps.create(data: rootAccount, in: .rootAccounts)
-            .sink(receiveCompletion: { completion in
+    public func initializeRootAccountListener() {
+        self.dbOps.getCurrentRootAccount()
+            .handleLoadingState(using: self.loadingStatePublisher)
+            .sink(receiveCompletion: { [weak self] completion in
                 if case let .failure(error) = completion {
-                    self.fetchErrorSubject.send(error)
+                    self?.fetchErrorSubject.send(error)
                 }
-            }, receiveValue: { _ in
-                // Deal with newly created RootAccount in the Back-Office
+            }, receiveValue: { [weak self] rootAccount in
+                guard let self else { return }
+                self.currentRootAccount.send(rootAccount)
             })
             .store(in: &self.cancellables)
     }
 
+    public func createRootAccount(rootAccount: RootAccount) {
+        self.dbOps.create(data: rootAccount, in: .rootAccounts)
+            .sink(receiveCompletion: { [weak self] completion in
+                if case let .failure(error) = completion {
+                    self?.fetchErrorSubject.send(error)
+                }
+            }, receiveValue: { _ in
+                // Handle successful creation from within the dashboard if needed
+            })
+            .store(in: &self.cancellables)
+    }
+
+    public func updateRootAccount(rootAccount: inout RootAccount) {
+        rootAccount.lastEditedAt = nil
+        self.dbOps.update(data: rootAccount, in: .rootAccounts)
+            .sink(
+                receiveCompletion: { [weak self] completion in
+                    if case let .failure(error) = completion {
+                        self?.fetchErrorSubject.send(error)
+                    }
+                },
+                receiveValue: { _ in
+                    // Successfully updated
+                }
+            )
+            .store(in: &self.cancellables)
+    }
+
+    public func addSavedActivity(_ savedActivity: SavedActivity) {
+        guard var rootAccount = self.currentRootAccount.value else {
+            self.fetchErrorSubject.send(DatabaseError.customError("RootAccount not found"))
+            return
+        }
+
+        if !rootAccount.savedActivities.contains(where: { $0.id == savedActivity.id }) {
+            rootAccount.savedActivities.append(savedActivity)
+            self.updateRootAccount(rootAccount: &rootAccount)
+        } else {
+            log.info("The activity \(String(describing: savedActivity.id)) has already been saved by \(savedActivity.caregiverID).")
+        }
+    }
+
+    public func removeSavedActivity(activityID: String) {
+        guard var rootAccount = self.currentRootAccount.value else {
+            self.fetchErrorSubject.send(DatabaseError.customError("RootAccount not found"))
+            return
+        }
+
+        if let index = rootAccount.savedActivities.firstIndex(where: { $0.id == activityID }) {
+            rootAccount.savedActivities.remove(at: index)
+            self.updateRootAccount(rootAccount: &rootAccount)
+        }
+    }
+
+    public func resetData() {
+        self.currentRootAccount.send(nil)
+        self.dbOps.clearAllListeners()
+        self.cancellables.forEach { $0.cancel() }
+        self.cancellables.removeAll()
+    }
+
     // MARK: Internal
+
+    var currentRootAccountPublisher: AnyPublisher<RootAccount?, Never> {
+        self.currentRootAccount.eraseToAnyPublisher()
+    }
+
+    var isLoadingPublisher: AnyPublisher<Bool, Never> {
+        self.loadingStatePublisher.eraseToAnyPublisher()
+    }
 
     var fetchErrorPublisher: AnyPublisher<Error, Never> {
         self.fetchErrorSubject.eraseToAnyPublisher()
@@ -35,8 +106,9 @@ public class RootAccountManager {
 
     // MARK: Private
 
+    private var currentRootAccount = CurrentValueSubject<RootAccount?, Never>(nil)
+    private let loadingStatePublisher = PassthroughSubject<Bool, Never>()
+    private let fetchErrorSubject = PassthroughSubject<Error, Never>()
     private let dbOps = DatabaseOperations.shared
     private var cancellables = Set<AnyCancellable>()
-
-    private var fetchErrorSubject = PassthroughSubject<Error, Never>()
 }
--- Modules/AccountKit/Sources/Managers/RootAccounts/RootAccountManagerViewModel.swift
+++ Modules/AccountKit/Sources/Managers/RootAccounts/RootAccountManagerViewModel.swift
@@ -14,7 +14,22 @@ public class RootAccountManagerViewModel: ObservableObject {
 
     // MARK: Public
 
+    @Published public var savedActivities: [SavedActivity] = []
     @Published public var errorMessage: String = ""
+    @Published public var showErrorAlert: Bool = false
+    @Published public var isLoading: Bool = false
+
+    public func addSavedActivity(_ savedActivity: SavedActivity) {
+        self.rootAccountManager.addSavedActivity(savedActivity)
+    }
+
+    public func removeSavedActivity(activityID: String, caregiverID _: String) {
+        self.rootAccountManager.removeSavedActivity(activityID: activityID)
+    }
+
+    public func resetData() {
+        self.rootAccountManager.resetData()
+    }
 
     // MARK: Private
 
@@ -22,6 +37,20 @@ public class RootAccountManagerViewModel: ObservableObject {
     private let rootAccountManager = RootAccountManager.shared
 
     private func subscribeToManager() {
+        self.rootAccountManager.currentRootAccountPublisher
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { [weak self] rootAccount in
+                self?.savedActivities = rootAccount?.savedActivities ?? []
+            })
+            .store(in: &self.cancellables)
+
+        self.rootAccountManager.isLoadingPublisher
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { [weak self] isLoading in
+                self?.isLoading = isLoading
+            })
+            .store(in: &self.cancellables)
+
         self.rootAccountManager.fetchErrorPublisher
             .receive(on: DispatchQueue.main)
             .sink(receiveValue: { [weak self] error in
@@ -31,6 +60,24 @@ public class RootAccountManagerViewModel: ObservableObject {
     }
 
     private func handleError(_ error: Error) {
-        self.errorMessage = "An unknown error occurred: \(error.localizedDescription)"
+        if let databaseError = error as? DatabaseError {
+            switch databaseError {
+                case let .customError(message):
+                    if message == "User not authenticated" {
+                        self.errorMessage = "Please log in to continue."
+                    } else {
+                        self.errorMessage = message
+                    }
+                case .documentNotFound:
+                    self.errorMessage = "The requested data could not be found."
+                case .decodeError:
+                    self.errorMessage = "There was an error decoding the data."
+                case .encodeError:
+                    self.errorMessage = "There was an error encoding the data."
+            }
+        } else {
+            self.errorMessage = "An unknown error occurred: \(error.localizedDescription)"
+        }
+        self.showErrorAlert = true
     }
 }
