f81ff758905417e5f7c90784c4ed9d2ee7783b66
Hugo Pezziardi <hugo@pezziardi.net>
2024-10-09 17:09:44 +0200

    ✨ (GEKExample): Add new DNDChoiceEvent enum

---
+++ Modules/GameEngineKit/Examples/GameEngineKitExample/Sources/Views/DND/Shared/DNDTouchEvent.swift
@@ -0,0 +1,10 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+// MARK: - DNDTouchEvent
+
+public enum DNDTouchEvent {
+    case began
+    case ended
+}
