54b0c23c7dcca2bc71414252f6707f5995ce05cd
Hugo Pezziardi <hugo@pezziardi.net>
2024-10-09 11:30:56 +0200

    ✨ (GEKExample): Add DND views

---
+++ Modules/GameEngineKit/Examples/GameEngineKitExample/Sources/Views/DND/DNDView.swift
@@ -0,0 +1,50 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import SpriteKit
+import SwiftUI
+
+// MARK: - DNDView
+
+public struct DNDView: View {
+    // MARK: Lifecycle
+
+    init(viewModel: DNDViewViewModel) {
+        self._viewModel = StateObject(wrappedValue: viewModel)
+    }
+
+    // MARK: Public
+
+    public var body: some View {
+        GeometryReader { proxy in
+            SpriteView(scene: self.makeScene(size: proxy.size), options: [.allowsTransparency])
+                .frame(width: proxy.size.width, height: proxy.size.height)
+                .onAppear {
+                    self.scene = self.getScene(for: self.viewModel.choices.count, size: proxy.size)
+                }
+        }
+    }
+
+    // MARK: Private
+
+    @StateObject private var viewModel: DNDViewViewModel
+    @State private var scene: SKScene = .init()
+
+    private func makeScene(size: CGSize) -> SKScene {
+        guard let finalScene = scene as? DNDBaseScene else {
+            return SKScene()
+        }
+        finalScene.size = size
+        return finalScene
+    }
+
+    private func getScene(for choiceCount: Int, size _: CGSize) -> SKScene {
+        switch choiceCount {
+            case 6:
+                SixChoicesScene(viewModel: self.viewModel)
+            default:
+                DNDBaseScene(viewModel: self.viewModel)
+        }
+    }
+}
---
+++ Modules/GameEngineKit/Examples/GameEngineKitExample/Sources/Views/DND/SKComponents/DNDView_SixChoicesScene.swift
@@ -0,0 +1,22 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import SwiftUI
+
+extension DNDView {
+    class SixChoicesScene: DNDBaseScene {
+        override func setFirstAnswerPosition() {
+            spacer = 2 * size.width / CGFloat(self.viewModel.choices.count)
+            initialNodeX = (size.width / 2) - spacer
+            verticalSpacing = 2 * size.height / CGFloat(self.viewModel.choices.count)
+            defaultPosition = CGPoint(x: initialNodeX, y: verticalSpacing - 30)
+        }
+
+        override func setInitialPosition(_ index: Int) -> CGPoint {
+            let positionX = initialNodeX + (spacer * CGFloat(index % 3))
+            let positionY = verticalSpacing - 30 + (verticalSpacing + 60) * CGFloat(2 * index / self.viewModel.choices.count)
+            return CGPoint(x: positionX, y: positionY)
+        }
+    }
+}
