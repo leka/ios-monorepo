159995df02c3d015a55134b4240cc7c86ee57227
Ladislas de Toldi <ladislas@detoldi.me>
2024-10-17 20:53:07 +0200

    ♻️ (GEKExample): Refactor DnDDropZoneNode

--- Modules/GameEngineKit/Examples/GameEngineKitExample/Sources/Views/DnD/SKComponents/DnDDropZoneNode.swift
+++ Modules/GameEngineKit/Examples/GameEngineKitExample/Sources/Views/DnD/SKComponents/DnDDropZoneNode.swift
@@ -20,36 +20,41 @@ class DnDDropZoneNode: SKSpriteNode {
                 }
 
                 super.init(texture: SKTexture(image: image), color: .clear, size: size)
+
             case .sfsymbol:
                 guard let image = UIImage(systemName: value, withConfiguration: UIImage.SymbolConfiguration(pointSize: size.height)) else {
                     fatalError("SFSymbol not found")
                 }
 
                 super.init(texture: SKTexture(image: image), color: .clear, size: size)
-            case .text:
-                let tempNode = SKNode()
 
-                let rectangle = SKShapeNode(rect: CGRect(x: -size.width / 2, y: -size.height / 2, width: size.width, height: size.height),
-                                            cornerRadius: 10)
+            case .text:
+                super.init(texture: nil, color: .clear, size: size)
+
+                let rectangle = SKShapeNode(
+                    rect: CGRect(x: -size.width / 2, y: -size.height / 2, width: size.width, height: size.height),
+                    cornerRadius: 10
+                )
+
                 rectangle.fillColor = .white
                 rectangle.strokeColor = .black
                 rectangle.lineWidth = 0.5
                 rectangle.zPosition = -1
                 rectangle.position = CGPoint(x: 0, y: 0)
-                tempNode.addChild(rectangle)
+
+                self.addChild(rectangle)
 
                 let label = SKLabelNode(text: value)
+
                 label.fontSize = 20
                 label.fontName = "AvenirNext-Bold"
                 label.fontColor = .black
                 label.position = CGPoint(x: 0, y: -10)
                 label.zPosition = 0
-                tempNode.addChild(label)
 
-                let texture = SKView().texture(from: tempNode)
-
-                super.init(texture: texture, color: .clear, size: size)
+                self.addChild(label)
         }
+
         self.name = value
         self.position = position
         self.zPosition = 10
@@ -65,9 +70,4 @@ class DnDDropZoneNode: SKSpriteNode {
     let id: String
     var initialPosition: CGPoint?
     var isDraggable = true
-
-    // MARK: Private
-
-    private var rectangle: SKShapeNode!
-    private var label: SKLabelNode!
 }
