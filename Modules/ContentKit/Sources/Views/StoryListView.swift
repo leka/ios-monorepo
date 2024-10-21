03ba4b30bbd66ecb001c170913c99cbdd87c5920
mathieu J. <macteuts@gmail.com>
2024-10-21 16:56:12 +0300

    ✨ (ContentKit): Add StoryListView to present Stories as list

---
+++ Modules/ContentKit/Sources/Views/StoryListView.swift
@@ -0,0 +1,121 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import AccountKit
+import DesignKit
+import LocalizationKit
+import SwiftUI
+
+// MARK: - StoryListView
+
+public struct StoryListView: View {
+    // MARK: Lifecycle
+
+    public init(stories: [Story]? = nil, onStartStory: ((Story) -> Void)?) {
+        self.stories = stories ?? []
+        self.onStartStory = onStartStory
+    }
+
+    // MARK: Public
+
+    public var body: some View {
+        LazyVStack(alignment: .leading, spacing: 20) {
+            ForEach(self.stories) { story in
+                NavigationLink(destination:
+                    StoryDetailsView(story: story, onStartStory: self.onStartStory)
+                ) {
+                    HStack(alignment: .center, spacing: 30) {
+                        Image(uiImage: story.details.iconImage)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .frame(width: 50)
+                            .overlay(
+                                Circle()
+                                    .stroke(self.styleManager.accentColor!, lineWidth: 1)
+                            )
+
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Text(story.details.title)
+                                    .font(.headline)
+                                    .frame(alignment: .leading)
+                            }
+
+                            if let subtitle = story.details.subtitle {
+                                Text(subtitle)
+                                    .font(.subheadline)
+                                    .frame(alignment: .leading)
+                            }
+                        }
+                        .padding(.vertical)
+
+                        Spacer()
+
+                        Button {
+                            let currentCaregiverID: String = self.caregiverManagerViewModel.currentCaregiver?.id ?? ""
+                            if self.rootAccountViewModel.isStorySaved(storyID: story.uuid) {
+                                self.rootAccountViewModel.removeSavedStory(storyID: story.uuid)
+                            } else {
+                                self.rootAccountViewModel.addSavedStory(
+                                    storyID: story.uuid,
+                                    caregiverID: currentCaregiverID
+                                )
+                            }
+                        } label: {
+                            if self.rootAccountViewModel.isStorySaved(storyID: story.uuid) {
+                                Image(systemName: "trash")
+                            } else {
+                                Image(systemName: "plus")
+                            }
+                        }
+                        .tint(self.rootAccountViewModel.isStorySaved(storyID: story.uuid) ? .red : self.styleManager.accentColor!)
+
+                        Button {
+                            self.onStartStory?(story)
+                        } label: {
+                            Image(systemName: "play.circle")
+                                .font(.system(size: 24))
+                                .contentShape(Rectangle())
+                        }
+                        .tint(.lkGreen)
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: 120)
+                    .contentShape(Rectangle())
+                }
+                .buttonStyle(.plain)
+            }
+        }
+        .padding()
+    }
+
+    // MARK: Internal
+
+    let stories: [Story]
+    let onStartStory: ((Story) -> Void)?
+
+    // MARK: Private
+
+    @ObservedObject private var styleManager: StyleManager = .shared
+
+    @StateObject private var rootAccountViewModel = RootAccountManagerViewModel()
+    @StateObject private var caregiverManagerViewModel = CaregiverManagerViewModel()
+}
+
+#Preview {
+    NavigationSplitView {
+        Text("Sidebar")
+    } detail: {
+        NavigationStack {
+            ScrollView {
+                StoryListView(
+                    stories: ContentKit.allStories,
+                    onStartStory: { _ in
+                        print("Story Started")
+                    }
+                )
+            }
+        }
+    }
+}
