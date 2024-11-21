a064c449b0402a19b6c08a5ea379049e4d147ebf
Ladislas de Toldi <ladislas@detoldi.me>
2024-11-21 15:42:18 +0100

    📈 (Analytics): Add View extension to log screen_view

--- Modules/AnalyticsKit/Sources/Manager/AnalyticsManager+ScreenView.swift
+++ Modules/AnalyticsKit/Sources/Manager/AnalyticsManager+ScreenView.swift
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import FirebaseAnalytics
+import SwiftUI
 
 public extension AnalyticsManager {
     func logEventScreenView(screenName: String, screenClass: String? = nil) {
@@ -16,4 +17,41 @@ public extension AnalyticsManager {
 
         Analytics.logEvent(AnalyticsEventScreenView, parameters: params)
     }
+}
+
+public extension View {
+    func logEventScreenView(screenName: String, screenClass: String? = nil) -> some View {
+        let screenClass = screenClass ?? String(describing: type(of: self))
+        return self.onAppear {
+            log.debug("logEventScreenView: \(screenName) - \(screenClass)")
+            AnalyticsManager.shared.logEventScreenView(screenName: screenName, screenClass: screenClass)
+        }
+    }
+}
+
+// MARK: - MyCustomView
+
+#Preview {
+    struct MyCustomView: View {
+        @State private var isPresented = true
+
+        var body: some View {
+            VStack {
+                Text("My Custom View")
+            }
+            .sheet(isPresented: $isPresented) {
+                MyCustomSheet()
+                    .logEventScreenView(screenName: "my_custom_sheet")
+            }
+        }
+
+        struct MyCustomSheet: View {
+            var body: some View {
+                Text("My Custom Sheet")
+            }
+        }
+    }
+
+    return MyCustomView()
+        .logEventScreenView(screenName: "my_custom_view")
 }
