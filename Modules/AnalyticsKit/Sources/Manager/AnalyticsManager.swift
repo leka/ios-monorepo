73b8bd45c15c5e85c59e482df0cee952a24aea49
Ladislas de Toldi <ladislas@detoldi.me>
2024-11-22 16:39:28 +0100

    📈 (Analytics): Add event enum with all our tracked events

--- Modules/AnalyticsKit/Sources/Manager/AnalyticsManager.swift
+++ Modules/AnalyticsKit/Sources/Manager/AnalyticsManager.swift
@@ -18,6 +18,76 @@ public class AnalyticsManager {
 
     // MARK: Internal
 
+    enum Event {
+        case activityStart
+        case activityEnd
+
+        case appUpdateSkip
+        case appUpdateOpenAppStore
+
+        case login
+        case logout
+        case signup
+        case skipAuthentication
+        case accountDelete
+
+        case screenView
+
+        case robotConnect
+        case robotDisconnect
+        case robotRename
+
+        case selectContent
+
+        case caregiverSelect
+        case carereceiverSkipSelect
+
+        // MARK: Internal
+
+        var name: String {
+            switch self {
+                case .activityStart:
+                    "activity_start"
+                case .activityEnd:
+                    "activity_end"
+
+                case .appUpdateSkip:
+                    "app_update_skip"
+                case .appUpdateOpenAppStore:
+                    "app_update_open_app_store"
+
+                case .login:
+                    AnalyticsEventLogin // ? "login"
+                case .logout:
+                    "logout"
+                case .signup:
+                    AnalyticsEventSignUp // ? "sign_up"
+                case .skipAuthentication:
+                    "skip_authentication"
+                case .accountDelete:
+                    "account_delete"
+
+                case .screenView:
+                    AnalyticsEventScreenView // ? "screen_view"
+
+                case .robotConnect:
+                    "robot_connect"
+                case .robotDisconnect:
+                    "robot_disconnect"
+                case .robotRename:
+                    "robot_rename"
+
+                case .selectContent:
+                    AnalyticsEventSelectContent // ? "select_content"
+
+                case .caregiverSelect:
+                    "caregiver_select"
+                case .carereceiverSkipSelect:
+                    "carereceiver_skip_select"
+            }
+        }
+    }
+
     static func logEvent(name: String, parameters: [String: Any] = [:]) {
         Analytics.logEvent(name, parameters: parameters)
     }
