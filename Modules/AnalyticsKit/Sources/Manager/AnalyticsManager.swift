7ebd01a286affd53466524a6427a5189097d456d
Ladislas de Toldi <ladislas@detoldi.me>
2024-11-25 17:39:11 +0100

    🐛 (Analytics): Fix default parameters name

--- Modules/AnalyticsKit/Sources/Manager/AnalyticsManager.swift
+++ Modules/AnalyticsKit/Sources/Manager/AnalyticsManager.swift
@@ -100,9 +100,9 @@ public class AnalyticsManager {
     static func setDefaultEventParameter(for parameter: DefaultEventParamters) {
         switch parameter {
             case let .rootOwnerUid(uid):
-                Analytics.setDefaultEventParameters(["root_owner_uid": uid ?? "(lk_not_set)"])
+                Analytics.setDefaultEventParameters(["lk_default_root_owner_uid": uid ?? "(lk_not_set)"])
             case let .caregiverUid(uid):
-                Analytics.setDefaultEventParameters(["caregiver_uid": uid ?? "(lk_not_set)"])
+                Analytics.setDefaultEventParameters(["lk_default_caregiver_id": uid ?? "(lk_not_set)"])
         }
     }
 }
