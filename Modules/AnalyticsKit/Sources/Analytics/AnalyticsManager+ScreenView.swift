b135c35d726e5f6c2197c8b1a8106a4281a59855
Ladislas de Toldi <ladislas@detoldi.me>
2024-11-27 12:04:12 +0100

    📈 (Analytics): Improve log event screen_view with context

--- Modules/AnalyticsKit/Sources/Analytics/AnalyticsManager+ScreenView.swift
+++ Modules/AnalyticsKit/Sources/Analytics/AnalyticsManager+ScreenView.swift
@@ -17,14 +17,92 @@ public extension AnalyticsManager {
 
         logEvent(.screenView, parameters: params)
     }
+
+    enum ScreenViewContext {
+        case splitView
+        case sheet
+        case fullScreenCover
+        case context(String)
+        case none
+        case additionalInfo(String)
+
+        // MARK: Internal
+
+        var description: String {
+            switch self {
+                case .splitView:
+                    "main_splitview"
+                case .sheet:
+                    "main_sheet"
+                case .fullScreenCover:
+                    "main_fullscreen"
+                case let .context(value):
+                    "\(value)"
+                case .none:
+                    "none"
+                case let .additionalInfo(value):
+                    "\(value)"
+            }
+        }
+
+        static func + (lhs: Self, rhs: Self) -> ScreenViewContext {
+            .additionalInfo("\(lhs.description)-\(rhs.description)")
+        }
+    }
+}
+
+// MARK: - AnalyticsLogScreenViewViewModifier
+
+struct AnalyticsLogScreenViewViewModifier: ViewModifier {
+    // MARK: Lifecycle
+
+    init(
+        screenName: String,
+        screenClass: String,
+        context: AnalyticsManager.ScreenViewContext? = nil,
+        parameters: [String: Any] = [:]
+    ) {
+        self.screenName = screenName
+        self.screenClass = screenClass
+        self.context = context
+        self.parameters = parameters
+    }
+
+    // MARK: Internal
+
+    let screenName: String
+    let screenClass: String
+    let context: AnalyticsManager.ScreenViewContext?
+    let parameters: [String: Any]
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear {
+                let screenName = "\(screenName)"
+                let params: [String: Any] = [
+                    "lk_context": context?.description ?? NSNull(),
+                ].merging(self.parameters) { _, new in new }
+
+                AnalyticsManager.logEventScreenView(screenName: screenName, screenClass: self.screenClass, parameters: self.parameters)
+            }
+    }
 }
 
 public extension View {
-    func logEventScreenView(screenName: String, screenClass: String? = nil, parameters: [String: Any] = [:]) -> some View {
-        let screenClass = screenClass ?? String(describing: type(of: self))
-        return self.onAppear {
-            AnalyticsManager.logEventScreenView(screenName: screenName, screenClass: screenClass, parameters: parameters)
-        }
+    func logEventScreenView(
+        screenName: String,
+        context: AnalyticsManager.ScreenViewContext?,
+        screenClass: String? = nil,
+        parameters: [String: Any] = [:]
+    ) -> some View {
+        self.modifier(
+            AnalyticsLogScreenViewViewModifier(
+                screenName: screenName,
+                screenClass: screenClass ?? String(describing: type(of: self)),
+                context: context,
+                parameters: parameters
+            )
+        )
     }
 }
 
