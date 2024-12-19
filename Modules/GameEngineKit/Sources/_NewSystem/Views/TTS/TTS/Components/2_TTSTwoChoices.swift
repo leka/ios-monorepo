25db7bb4eaa0bfd81030dd031b5cc0c61745dd76
Hugo Pezziardi <hugo@pezziardi.net>
2024-12-19 19:02:24 +0100

    ✨ (NewGEK): Add 1 to 6 Views components for TTSView

---
+++ Modules/GameEngineKit/Sources/_NewSystem/Views/TTS/TTS/Components/2_TTSTwoChoices.swift
@@ -0,0 +1,52 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Combine
+import ContentKit
+import SwiftUI
+
+extension TTSView {
+    struct TwoChoicesView: View {
+        // MARK: Internal
+
+        @ObservedObject var viewModel: TTSViewViewModel
+
+        var body: some View {
+            HStack(spacing: self.kHorizontalSpacing) {
+                ForEach(self.viewModel.choices) { choice in
+                    Button {
+                        self.viewModel.onTapped(choice: choice)
+                    } label: {
+                        choice.view
+                    }
+                }
+            }
+        }
+
+        // MARK: Private
+
+        private let kHorizontalSpacing: CGFloat = 150
+    }
+}
+
+#Preview {
+    // MARK: - TTSEmptyCoordinator
+
+    class TTSEmptyCoordinator: TTSGameplayCoordinatorProtocol {
+        var uiModel = CurrentValueSubject<TTSUIModel, Never>(TTSUIModel(action: nil, choices: [
+            TTSUIChoiceModel(view: TTSCoordinatorFindTheRightAnswers.ChoiceView(value: "Choice 1", type: .text, size: 240, state: .idle)),
+            TTSUIChoiceModel(view: TTSCoordinatorFindTheRightAnswers.ChoiceView(value: "Choice 2", type: .text,
+                                                                                size: 240, state: .idle)),
+        ]))
+
+        func processUserSelection(choice: TTSUIChoiceModel) {
+            log.debug("\(choice.id)")
+        }
+    }
+
+    let coordinator = TTSEmptyCoordinator()
+    let viewModel = TTSViewViewModel(coordinator: coordinator)
+
+    return TTSView(viewModel: viewModel)
+}
