b3b101c43165443f08bae4ee93719ab32e7bcdfb
Hugo Pezziardi <hugo@pezziardi.net>
2025-01-10 11:36:20 +0100

    ✨ (NewGEK): Add MagicCard ViewModel

---
+++ Modules/GameEngineKit/Sources/_NewSystem/Views/MagicCard/MagicCardViewViewModel.swift
@@ -0,0 +1,28 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import ContentKit
+import RobotKit
+import SwiftUI
+
+// MARK: - MagicCardViewViewModel
+
+public class MagicCardViewViewModel: ObservableObject {
+    // MARK: Lifecycle
+
+    public init(coordinator: MagicCardGameplayCoordinatorProtocol) {
+        self.action = coordinator.action
+        self.coordinator = coordinator
+    }
+
+    // MARK: Internal
+
+    @Published var didTriggerAction = true
+
+    let action: Exercise.Action
+
+    // MARK: Private
+
+    private let coordinator: MagicCardGameplayCoordinatorProtocol
+}
