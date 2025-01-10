87db5000afab9cbb8b74644ef56a6d877a4a5652
Hugo Pezziardi <hugo@pezziardi.net>
2025-01-10 11:35:27 +0100

    ✨ (NewGEK): Add MagicCard coordinator

---
+++ Modules/GameEngineKit/Sources/_NewSystem/Views/MagicCard/Coordinators/MagicCardCoordinator+FindtheRightAnswers.swift
@@ -0,0 +1,68 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Combine
+import ContentKit
+import RobotKit
+import SwiftUI
+
+// MARK: - MagicCardCoordinatorFindTheRightAnswers
+
+public class MagicCardCoordinatorFindTheRightAnswers: MagicCardGameplayCoordinatorProtocol {
+    // MARK: Lifecycle
+
+    public init(choices: [MagicCardCoordinatorFindTheRightAnswersChoiceModel], action: Exercise.Action) {
+        self.rawChoices = choices
+
+        self.gameplay = NewGameplayFindTheRightAnswers(
+            choices: choices
+                .map { .init(id: $0.id, isRightAnswer: $0.isRightAnswer)
+                })
+        self.action = action
+
+        self.robot.magicCard
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] card in
+                if self!.gameplay.isCompleted.value { return }
+                self!.processUserSelection(magicCard: card)
+            }
+            .store(in: &self.cancellables)
+    }
+
+    // MARK: Public
+
+    public var action: Exercise.Action
+
+    // MARK: Private
+
+    private var cancellables = Set<AnyCancellable>()
+
+    private var robot: Robot = .shared
+    private let gameplay: NewGameplayFindTheRightAnswers
+    private let rawChoices: [MagicCardCoordinatorFindTheRightAnswersChoiceModel]
+
+    private func processUserSelection(magicCard: MagicCard) {
+        guard let choiceID = self.rawChoices.first(where: { $0.value == magicCard }) else { return }
+
+        _ = self.gameplay.process(choiceIDs: [choiceID.id])
+    }
+}
+
+#Preview {
+    let kDefaultChoices: [MagicCardCoordinatorFindTheRightAnswersChoiceModel] = [
+        .init(value: MagicCard.emotion_disgust_leka, isRightAnswer: true),
+        .init(value: MagicCard.emotion_fear_leka),
+        .init(value: MagicCard.emotion_joy_leka),
+        .init(value: MagicCard.emotion_sadness_leka),
+        .init(value: MagicCard.emotion_anger_leka),
+    ]
+
+    let coordinator = MagicCardCoordinatorFindTheRightAnswers(
+        choices: kDefaultChoices,
+        action: Exercise.Action.robot(type: .image("robotFaceDisgusted"))
+    )
+    let viewModel = MagicCardViewViewModel(coordinator: coordinator)
+
+    return MagicCardView(viewModel: viewModel)
+}
