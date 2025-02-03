6a22d0d057251703536e245cee6ca266f5586660
Ladislas de Toldi <ladislas@detoldi.me>
2025-02-03 13:14:07 +0100

    ✨ (ContentKit): Add new Exercise model

---
+++ Modules/ContentKit/Sources/_NewSystem/Exercise/NewExercise.swift
@@ -0,0 +1,16 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Foundation
+
+// MARK: - NewExercise
+
+public struct NewExercise {
+    public let instructions: String?
+    public let interface: NewExerciseInterface
+    public let gameplay: NewExerciseGameplay?
+    public let action: NewExerciseAction?
+    public let options: NewExerciseOptions
+    public let payload: Data?
+}
