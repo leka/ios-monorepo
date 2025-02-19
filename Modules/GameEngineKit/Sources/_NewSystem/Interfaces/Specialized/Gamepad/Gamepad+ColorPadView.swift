5ad7ae36d2e53c0ac52abe4ee45cd3f496dba748
Hugo Pezziardi <hugo@pezziardi.net>
2025-02-19 14:53:40 +0100

    🧑‍💻 (NewGEK): Give LekaColor & ReinforcerBar buttons their own file

--- Modules/GameEngineKit/Sources/_NewSystem/Interfaces/Specialized/Gamepad/Gamepad+ColorPadView.swift
+++ Modules/GameEngineKit/Sources/_NewSystem/Interfaces/Specialized/Gamepad/Gamepad+ColorPadView.swift
@@ -2,7 +2,6 @@
 // Copyright APF France handicap
 // SPDX-License-Identifier: Apache-2.0
 
-import DesignKit
 import RobotKit
 import SwiftUI
 
@@ -14,7 +13,7 @@ extension Gamepad {
             VStack(spacing: 50) {
                 LazyVGrid(columns: self.columns, spacing: self.kHorizontalSpacing) {
                     ForEach(self.colors, id: \.screen) { color in
-                        ColorButtonLabel(color: color, isPressed: self.selectedColor?.screen == color.screen)
+                        ColorLekaButtonLabel(color: color, isPressed: self.selectedColor?.screen == color.screen)
                             .onTapGesture {
                                 if self.selectedColor?.screen == color.screen {
                                     self.selectedColor = nil
@@ -34,17 +33,7 @@ extension Gamepad {
                     }
                 }
 
-                HStack {
-                    ForEach(Robot.Reinforcer.allCases, id: \.self) { reinforcer in
-                        reinforcer.icon()
-                            .resizable()
-                            .frame(maxWidth: 100, maxHeight: 100)
-                            .onTapGesture {
-                                self.reinforcerTriggered = true
-                                Robot.shared.run(reinforcer)
-                            }
-                    }
-                }
+                ReinforcerBarButton(onReinforcerTriggerCallback: { self.reinforcerTriggered = true })
             }
             .padding(.horizontal)
         }
@@ -56,40 +45,6 @@ extension Gamepad {
         private let kHorizontalSpacing: CGFloat = 20
         @State private var selectedColor: Robot.Color?
         @State private var reinforcerTriggered: Bool = false
-    }
-
-    struct ColorButtonLabel: View {
-        // MARK: Internal
-
-        let color: Robot.Color
-        let isPressed: Bool
-
-        var body: some View {
-            ZStack {
-                Circle()
-                    .fill(.white)
-                    .frame(width: self.kButtonSize, height: self.kButtonSize)
-                    .shadow(color: self.color.screen != .white ? self.color.screen : .gray.opacity(0.6), radius: self.isPressed ? 20 : 0)
-                    .animation(.easeIn(duration: 0.2), value: self.isPressed)
-
-                Circle()
-                    .stroke(self.color.screen != .white ? self.color.screen : .gray.opacity(0.3), lineWidth: 8)
-                    .frame(width: self.kButtonSize, height: self.kButtonSize)
-
-                HStack(spacing: 30) {
-                    Circle()
-                        .fill(.black)
-                        .frame(width: 20, height: 20)
-                    Circle()
-                        .fill(.black)
-                        .frame(width: 20, height: 20)
-                }
-            }
-        }
-
-        // MARK: Private
-
-        private let kButtonSize: CGFloat = 190
     }
 }
 
