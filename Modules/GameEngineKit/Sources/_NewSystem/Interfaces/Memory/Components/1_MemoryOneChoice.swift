bc8ccb0f22446ac1413cac8293b5882e8da0e868
Hugo Pezziardi <hugo@pezziardi.net>
2025-02-26 14:00:29 +0100

    ✨ (NewGEK): Add components Memory views

---
+++ Modules/GameEngineKit/Sources/_NewSystem/Interfaces/Memory/Components/1_MemoryOneChoice.swift
@@ -0,0 +1,24 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import Combine
+import ContentKit
+import SwiftUI
+
+// MARK: - NewMemoryView.OneChoiceView
+
+extension NewMemoryView {
+    struct OneChoiceView: View {
+        @ObservedObject var viewModel: NewMemoryViewViewModel
+
+        var body: some View {
+            let choice = self.viewModel.choices[0]
+            choice.view
+                .onTapGesture {
+                    self.viewModel.onTapped(choice: choice)
+                }
+                .disabled(choice.disabled)
+        }
+    }
+}
