0658d840cc026ef3a0da0b5b73b7e1bda1d024ce
mathieu J. <macteuts@gmail.com>
2025-02-27 20:17:49 +0200

    ✨ (AccountKit): Handle add/remove favs in DbOps & LibManager

--- Modules/AccountKit/Sources/Managers/Library/LibraryManager.swift
+++ Modules/AccountKit/Sources/Managers/Library/LibraryManager.swift
@@ -5,6 +5,8 @@
 import Combine
 import Foundation
 
+// swiftlint:disable type_body_length file_length
+
 public class LibraryManager {
     // MARK: Lifecycle
 
@@ -83,12 +85,16 @@ public class LibraryManager {
             return
         }
 
-        let newCurriculum = SavedCurriculum(id: curriculumID, caregiverID: caregiverID, addedAt: Date())
+        let newCurriculum = SavedCurriculum(
+            id: curriculumID,
+            caregiverID: caregiverID,
+            addedAt: Date(),
+            favoritedBy: [:]
+        )
 
         self.dbOps.addItemToLibrarySubCollection(
             libraryID: libraryID,
-            subCollection: .curriculums,
-            item: newCurriculum
+            item: .curriculum(newCurriculum)
         )
         .sink(receiveCompletion: { [weak self] completion in
             if case let .failure(error) = completion {
@@ -121,6 +127,61 @@ public class LibraryManager {
         .store(in: &self.cancellables)
     }
 
+    public func addCurriculumToLibraryAsFavorite(curriculumID: String, caregiverID: String) {
+        guard let libraryID = currentLibrary.value?.id else {
+            self.fetchError.send(DatabaseError.customError("Library not found"))
+            return
+        }
+
+        let alreadyInTheLibrary: Bool = self.savedCurriculums.value.contains { $0.id == curriculumID }
+
+        guard alreadyInTheLibrary else {
+            let newCurriculum = SavedCurriculum(
+                id: curriculumID,
+                caregiverID: caregiverID,
+                addedAt: Date(),
+                favoritedBy: [caregiverID: Date()]
+            )
+
+            self.dbOps.addItemToLibrarySubCollection(
+                libraryID: libraryID,
+                item: .curriculum(newCurriculum)
+            )
+            .sink(receiveCompletion: { [weak self] completion in
+                if case let .failure(error) = completion {
+                    self?.fetchError.send(error)
+                }
+            }, receiveValue: {
+                log.info("Curriculum \(curriculumID) added and favorited successfully.")
+            })
+            .store(in: &self.cancellables)
+            return
+        }
+        self.addSavedCurriculumToFavorites(curriculumID: curriculumID, caregiverID: caregiverID)
+    }
+
+    public func removeCurriculumFromFavorites(curriculumID: String, caregiverID: String) {
+        guard let libraryID = currentLibrary.value?.id else {
+            self.fetchError.send(DatabaseError.customError("Library not found"))
+            return
+        }
+
+        self.dbOps.removeLibraryItemFromFavorites(
+            libraryID: libraryID,
+            subCollection: .curriculums,
+            itemID: curriculumID,
+            caregiverID: caregiverID
+        )
+        .sink(receiveCompletion: { [weak self] completion in
+            if case let .failure(error) = completion {
+                self?.fetchError.send(error)
+            }
+        }, receiveValue: {
+            // Nothing to do
+        })
+        .store(in: &self.cancellables)
+    }
+
     // MARK: - Activities + Gamepads
 
     public func addActivity(activityID: String, caregiverID: String) {
@@ -129,12 +190,15 @@ public class LibraryManager {
             return
         }
 
-        let newActivity = SavedActivity(id: activityID, caregiverID: caregiverID, addedAt: Date())
-
+        let newActivity = SavedActivity(
+            id: activityID,
+            caregiverID: caregiverID,
+            addedAt: Date(),
+            favoritedBy: [:]
+        )
         self.dbOps.addItemToLibrarySubCollection(
             libraryID: libraryID,
-            subCollection: .activities,
-            item: newActivity
+            item: .activity(newActivity)
         )
         .sink(receiveCompletion: { [weak self] completion in
             if case let .failure(error) = completion {
@@ -167,6 +231,61 @@ public class LibraryManager {
         .store(in: &self.cancellables)
     }
 
+    public func addActivityToLibraryAsFavorite(activityID: String, caregiverID: String) {
+        guard let libraryID = currentLibrary.value?.id else {
+            self.fetchError.send(DatabaseError.customError("Library not found"))
+            return
+        }
+
+        let alreadyInTheLibrary: Bool = self.savedActivities.value.contains { $0.id == activityID }
+
+        guard alreadyInTheLibrary else {
+            let newActivity = SavedActivity(
+                id: activityID,
+                caregiverID: caregiverID,
+                addedAt: Date(),
+                favoritedBy: [caregiverID: Date()]
+            )
+
+            self.dbOps.addItemToLibrarySubCollection(
+                libraryID: libraryID,
+                item: .activity(newActivity)
+            )
+            .sink(receiveCompletion: { [weak self] completion in
+                if case let .failure(error) = completion {
+                    self?.fetchError.send(error)
+                }
+            }, receiveValue: {
+                log.info("Activity \(activityID) added and favorited successfully.")
+            })
+            .store(in: &self.cancellables)
+            return
+        }
+        self.addSavedActivityToFavorites(activityID: activityID, caregiverID: caregiverID)
+    }
+
+    public func removeActivityFromFavorites(activityID: String, caregiverID: String) {
+        guard let libraryID = currentLibrary.value?.id else {
+            self.fetchError.send(DatabaseError.customError("Library not found"))
+            return
+        }
+
+        self.dbOps.removeLibraryItemFromFavorites(
+            libraryID: libraryID,
+            subCollection: .activities,
+            itemID: activityID,
+            caregiverID: caregiverID
+        )
+        .sink(receiveCompletion: { [weak self] completion in
+            if case let .failure(error) = completion {
+                self?.fetchError.send(error)
+            }
+        }, receiveValue: {
+            // Nothing to do
+        })
+        .store(in: &self.cancellables)
+    }
+
     // MARK: - Stories
 
     public func addStory(storyID: String, caregiverID: String) {
@@ -175,12 +294,16 @@ public class LibraryManager {
             return
         }
 
-        let newStory = SavedStory(id: storyID, caregiverID: caregiverID, addedAt: Date())
+        let newStory = SavedStory(
+            id: storyID,
+            caregiverID: caregiverID,
+            addedAt: Date(),
+            favoritedBy: [:]
+        )
 
         self.dbOps.addItemToLibrarySubCollection(
             libraryID: libraryID,
-            subCollection: .stories,
-            item: newStory
+            item: .story(newStory)
         )
         .sink(receiveCompletion: { [weak self] completion in
             if case let .failure(error) = completion {
@@ -213,6 +336,61 @@ public class LibraryManager {
         .store(in: &self.cancellables)
     }
 
+    public func addStoryToLibraryAsFavorite(storyID: String, caregiverID: String) {
+        guard let libraryID = currentLibrary.value?.id else {
+            self.fetchError.send(DatabaseError.customError("Library not found"))
+            return
+        }
+
+        let alreadyInTheLibrary: Bool = self.savedStories.value.contains { $0.id == storyID }
+
+        guard alreadyInTheLibrary else {
+            let newStory = SavedStory(
+                id: storyID,
+                caregiverID: caregiverID,
+                addedAt: Date(),
+                favoritedBy: [caregiverID: Date()]
+            )
+
+            self.dbOps.addItemToLibrarySubCollection(
+                libraryID: libraryID,
+                item: .story(newStory)
+            )
+            .sink(receiveCompletion: { [weak self] completion in
+                if case let .failure(error) = completion {
+                    self?.fetchError.send(error)
+                }
+            }, receiveValue: {
+                log.info("Story \(storyID) added and favorited successfully.")
+            })
+            .store(in: &self.cancellables)
+            return
+        }
+        self.addSavedStoryToFavorites(storyID: storyID, caregiverID: caregiverID)
+    }
+
+    public func removeStoryFromFavorites(storyID: String, caregiverID: String) {
+        guard let libraryID = currentLibrary.value?.id else {
+            self.fetchError.send(DatabaseError.customError("Library not found"))
+            return
+        }
+
+        self.dbOps.removeLibraryItemFromFavorites(
+            libraryID: libraryID,
+            subCollection: .stories,
+            itemID: storyID,
+            caregiverID: caregiverID
+        )
+        .sink(receiveCompletion: { [weak self] completion in
+            if case let .failure(error) = completion {
+                self?.fetchError.send(error)
+            }
+        }, receiveValue: {
+            // Nothing to do
+        })
+        .store(in: &self.cancellables)
+    }
+
     // MARK: - Reset Data
 
     public func resetData() {
@@ -229,4 +407,72 @@ public class LibraryManager {
 
     private let dbOps = DatabaseOperations.shared
     private var cancellables = Set<AnyCancellable>()
+
+    private func addSavedCurriculumToFavorites(curriculumID: String, caregiverID: String) {
+        guard let libraryID = currentLibrary.value?.id else {
+            self.fetchError.send(DatabaseError.customError("Library not found"))
+            return
+        }
+
+        self.dbOps.addLibraryItemToFavorites(
+            libraryID: libraryID,
+            subCollection: .curriculums,
+            itemID: curriculumID,
+            caregiverID: caregiverID
+        )
+        .sink(receiveCompletion: { [weak self] completion in
+            if case let .failure(error) = completion {
+                self?.fetchError.send(error)
+            }
+        }, receiveValue: {
+            // Nothing to do
+        })
+        .store(in: &self.cancellables)
+    }
+
+    private func addSavedActivityToFavorites(activityID: String, caregiverID: String) {
+        guard let libraryID = currentLibrary.value?.id else {
+            self.fetchError.send(DatabaseError.customError("Library not found"))
+            return
+        }
+
+        self.dbOps.addLibraryItemToFavorites(
+            libraryID: libraryID,
+            subCollection: .activities,
+            itemID: activityID,
+            caregiverID: caregiverID
+        )
+        .sink(receiveCompletion: { [weak self] completion in
+            if case let .failure(error) = completion {
+                self?.fetchError.send(error)
+            }
+        }, receiveValue: {
+            // Nothing to do
+        })
+        .store(in: &self.cancellables)
+    }
+
+    private func addSavedStoryToFavorites(storyID: String, caregiverID: String) {
+        guard let libraryID = currentLibrary.value?.id else {
+            self.fetchError.send(DatabaseError.customError("Library not found"))
+            return
+        }
+
+        self.dbOps.addLibraryItemToFavorites(
+            libraryID: libraryID,
+            subCollection: .stories,
+            itemID: storyID,
+            caregiverID: caregiverID
+        )
+        .sink(receiveCompletion: { [weak self] completion in
+            if case let .failure(error) = completion {
+                self?.fetchError.send(error)
+            }
+        }, receiveValue: {
+            // Nothing to do
+        })
+        .store(in: &self.cancellables)
+    }
 }
+
+// swiftlint:enable type_body_length file_length
