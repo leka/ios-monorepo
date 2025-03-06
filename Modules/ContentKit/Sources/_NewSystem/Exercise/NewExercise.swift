2b5fd30f379457bcab3d42ff9395eb5145c382e7
Hugo Pezziardi <hugo@pezziardi.net>
2025-03-06 18:52:37 +0100

    ♻️ (ContentKit): Turn exercise options optional

--- Modules/ContentKit/Sources/_NewSystem/Exercise/NewExercise.swift
+++ Modules/ContentKit/Sources/_NewSystem/Exercise/NewExercise.swift
@@ -11,6 +11,6 @@ public struct NewExercise {
     public let interface: NewExerciseInterface
     public let gameplay: NewExerciseGameplay?
     public let action: NewExerciseAction?
-    public let options: NewExerciseOptions
+    public let options: NewExerciseOptions?
     public let payload: Data?
 }
