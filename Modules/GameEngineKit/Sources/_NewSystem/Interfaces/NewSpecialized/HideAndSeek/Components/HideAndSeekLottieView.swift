628511c7a9434f76042c326d394e39692d0eca14
Hugo Pezziardi <hugo@pezziardi.net>
2025-03-12 11:35:51 +0100

    ✨ (NewGEK): Add newHideAndSeek components

---
+++ Modules/GameEngineKit/Sources/_NewSystem/Interfaces/NewSpecialized/HideAndSeek/Components/HideAndSeekLottieView.swift
@@ -0,0 +1,34 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import LocalizationKit
+import Lottie
+import SwiftUI
+
+struct HideAndSeekLottieView: View {
+    // MARK: Internal
+
+    var body: some View {
+        VStack {
+            Text(l10n.HideAndSeekView.Player.instructions)
+                .font(.headline)
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .padding(.top, 30)
+
+            LottieView(animation: self.animation, speed: 0.5)
+        }
+        .background(.black)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+        .padding(10)
+    }
+
+    // MARK: Private
+
+    private let animation = LottieAnimation.named("hide_and_seek_hidden.animation.lottie", bundle: .module)!
+}
+
+#Preview {
+    HideAndSeekLottieView()
+}
