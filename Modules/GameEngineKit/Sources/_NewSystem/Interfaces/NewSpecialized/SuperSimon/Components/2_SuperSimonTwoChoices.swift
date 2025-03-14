750346b0f31f843f424e95c2b133eec5e239761c
Hugo Pezziardi <hugo@pezziardi.net>
2025-03-14 16:23:49 +0100

    ✨ (NewGEK): Add NewSuperSimon components view

---
+++ Modules/GameEngineKit/Sources/_NewSystem/Interfaces/NewSpecialized/SuperSimon/Components/2_SuperSimonTwoChoices.swift
@@ -0,0 +1,38 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import ContentKit
+import SwiftUI
+
+extension NewSuperSimonView {
+    struct TwoChoicesView: View {
+        // MARK: Internal
+
+        var viewModel: NewSuperSimonViewViewModel
+
+        var body: some View {
+            HStack(spacing: self.kHorizontalSpacing) {
+                ForEach(self.viewModel.choices) { choice in
+                    Button {
+                        self.viewModel.onTapped(choiceID: choice.id)
+                    } label: {
+                        choice.view
+                    }
+                    .disabled(choice.disabled)
+                }
+            }
+        }
+
+        // MARK: Private
+
+        private let kHorizontalSpacing: CGFloat = 150
+    }
+}
+
+#Preview {
+    let coordinator = NewSuperSimonCoordinator(level: .easy)
+    let viewModel = NewSuperSimonViewViewModel(coordinator: coordinator)
+
+    return NewSuperSimonView(viewModel: viewModel)
+}
