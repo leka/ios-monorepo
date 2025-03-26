da5e7b5ee6838f2ba622bed9e4d33da02a02eb5b
Hugo Pezziardi <hugo@pezziardi.net>
2025-03-26 16:50:48 +0100

    💄 (ContentKit): Improve icon image size to fit activity items

--- Modules/ContentKit/Sources/Content/Views/ActivityListView.swift
+++ Modules/ContentKit/Sources/Content/Views/ActivityListView.swift
@@ -101,7 +101,7 @@ public struct ActivityListView: View {
                                     .bold()
                             }
                             .buttonStyle(TranslucentButtonStyle(color: self.styleManager.accentColor!))
-                            .frame(width: 50)
+                            .frame(width: 40)
                         }
 
                         Button {
@@ -145,11 +145,11 @@ public struct ActivityListView: View {
                     .renderingMode(.template)
                     .foregroundStyle(.secondary)
                     .scaledToFit()
-                    .frame(width: 50)
+                    .frame(width: 40)
                     .padding(.horizontal, 5)
             } else {
                 Color.clear
-                    .frame(width: 60, height: 50)
+                    .frame(width: 40, height: 40)
             }
         }
     }
