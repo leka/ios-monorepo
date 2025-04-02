6e2c35ef0d224eddf6c9a2eda7b49d80254cd74a
mathieu J. <macteuts@gmail.com>
2025-04-02 11:09:21 +0300

    ✨ (AccountKit): Update CaregiverMngr to use new isAdmin property

--- Modules/AccountKit/Sources/Authentication/AuthManager.swift
+++ Modules/AccountKit/Sources/Authentication/AuthManager.swift
@@ -30,6 +30,7 @@ public class AuthManager {
         case userIsSigningOut
         case userIsReAuthenticating
         case userIsResettingPassword
+        case userIsChangingEmail
         case userIsDeletingAccount
     }
 
