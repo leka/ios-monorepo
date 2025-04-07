28a7cb01753b2fad6b173becc12b492d83942221
Hugo Pezziardi <hugo@pezziardi.net>
2025-04-07 15:07:29 +0200

    🚚 (LekaApp): Move Search views in LekaApp

---
+++ Apps/LekaApp/Sources/Views/Search/SearchGridView.swift
@@ -0,0 +1,139 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import ContentKit
+import DesignKit
+import LocalizationKit
+import SwiftUI
+
+// MARK: - SearchGridView
+
+public struct SearchGridView: View {
+    // MARK: Lifecycle
+
+    public init(activities: [Activity]? = nil,
+                skills: [Skill]? = nil,
+                curriculums: [Curriculum]? = nil,
+                onStartActivity: ((Activity) -> Void)?)
+    {
+        self.activities = activities ?? []
+        self.skills = skills ?? []
+        self.curriculums = curriculums ?? []
+        self.onStartActivity = onStartActivity
+    }
+
+    // MARK: Public
+
+    public var body: some View {
+        VStack {
+            if self.activities.isEmpty, self.curriculums.isEmpty, self.skills.isEmpty {
+                Text(l10n.SearchGridView.noResults)
+                    .font(.title)
+                    .foregroundStyle(.secondary)
+                    .multilineTextAlignment(.center)
+                    .padding()
+            }
+
+            if !self.activities.isEmpty {
+                HStack {
+                    Text(l10n.SearchGridView.activitiesTitle)
+                        .font(.title.bold())
+                    Spacer()
+                    NavigationLink(destination:
+                        ScrollView(showsIndicators: true) {
+                            ActivityGridView(activities: self.activities, onStartActivity: self.onStartActivity)
+                                .navigationTitle(String(l10n.SearchGridView.activitiesTitle.characters))
+                        }
+                    ) {
+                        Text(l10n.SearchGridView.seeAllLabel)
+                    }
+                }
+                .padding(.horizontal)
+
+                ActivityHorizontalListView(activities: self.activities, onStartActivity: self.onStartActivity)
+            }
+
+            if !self.curriculums.isEmpty {
+                HStack {
+                    Text(l10n.SearchGridView.curriculumsTitle)
+                        .font(.title.bold())
+                    Spacer()
+                    NavigationLink(destination:
+                        ScrollView(showsIndicators: true) {
+                            CurriculumGridView(curriculums: self.curriculums, onStartActivity: self.onStartActivity)
+                                .navigationTitle(String(l10n.SearchGridView.curriculumsTitle.characters))
+                        }
+                    ) {
+                        Text(l10n.SearchGridView.seeAllLabel)
+                    }
+                }
+                .padding(.horizontal)
+
+                CurriculumHorizontalListView(curriculums: self.curriculums, onStartActivity: self.onStartActivity)
+            }
+
+            if !self.skills.isEmpty {
+                HStack {
+                    Text(l10n.SearchGridView.skillsTitle)
+                        .font(.title.bold())
+                    Spacer()
+                }
+                .padding(.horizontal)
+
+                SkillsGridView(skills: self.skills, onActivitySelected: self.onStartActivity)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+
+    // MARK: Internal
+
+    let activities: [Activity]
+    let skills: [Skill]
+    let curriculums: [Curriculum]
+    let onStartActivity: ((Activity) -> Void)?
+
+    // MARK: Private
+
+    @ObservedObject private var styleManager: StyleManager = .shared
+}
+
+// MARK: - l10n.SearchGridView
+
+extension l10n {
+    enum SearchGridView {
+        static let activitiesTitle = LocalizedString("lekaapp.search_grid_view.activities_title",
+                                                     value: "Activities",
+                                                     comment: "SearchGridView 'activities' section title")
+
+        static let curriculumsTitle = LocalizedString("lekaapp.search_grid_view.curriculums_title",
+                                                      value: "Curriculums",
+                                                      comment: "SearchGridView 'curriculums' section title")
+
+        static let skillsTitle = LocalizedString("lekaapp.search_grid_view.skills_title",
+                                                 value: "Skills",
+                                                 comment: "SearchGridView 'skills' section title")
+
+        static let seeAllLabel = LocalizedString("lekaapp.search_grid_view.see_all_label",
+                                                 value: "See all",
+                                                 comment: "SearchGridView 'see all' button label")
+
+        static let noResults = LocalizedString("lekaapp.search_grid_view.no_results",
+                                               value: "Oops! No results found.",
+                                               comment: "SearchGridView 'see all' button label")
+    }
+}
+
+#Preview {
+    NavigationStack {
+        SearchGridView(
+            activities: ContentKit.allActivities,
+            skills: Skills.primarySkillsList,
+            curriculums: ContentKit.allCurriculums,
+            onStartActivity: { _ in
+                print("Activity Started")
+            }
+        )
+    }
+}
---
+++ Apps/LekaApp/Sources/Views/Search/SubskillsGridView.swift
@@ -0,0 +1,126 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+import ContentKit
+import DesignKit
+import LocalizationKit
+import SwiftUI
+import UtilsKit
+
+// MARK: - SubskillsGridView
+
+public struct SubskillsGridView: View {
+    // MARK: Lifecycle
+
+    public init(subskills: [Skill]? = nil, onActivitySelected: ((Activity) -> Void)?) {
+        self.subskills = subskills ?? []
+        self.onActivitySelected = onActivitySelected
+    }
+
+    // MARK: Public
+
+    public var body: some View {
+        VStack {
+            ScrollView(showsIndicators: true) {
+                HStack(alignment: .center, spacing: 30) {
+                    let mainSkill = self.subskills[0]
+                    if let icon = mainSkill.iconImage {
+                        Image(uiImage: icon)
+                            .resizable()
+                            .renderingMode(.template)
+                            .scaledToFit()
+                            .frame(width: 100)
+                            .foregroundStyle(self.styleManager.accentColor!)
+                    }
+
+                    Text(mainSkill.description)
+                        .font(.title2)
+                }
+                .padding(.horizontal)
+                .padding(.bottom)
+
+                Spacer()
+
+                if self.subskills.count > 1 {
+                    VStack(alignment: .leading, spacing: 30) {
+                        ForEach(self.subskills[1...]) { subskill in
+                            Section {
+                                let subskillActivities = self.activities.filter {
+                                    $0.skills.contains(subskill)
+                                }
+                                if !subskillActivities.isEmpty {
+                                    VStack(alignment: .leading) {
+                                        VStack(alignment: .leading) {
+                                            Text(subskill.name)
+                                                .font(.title2)
+                                                .foregroundStyle(self.styleManager.accentColor!)
+
+                                            Text(subskill.description)
+                                                .font(.subheadline)
+                                                .foregroundStyle(.secondary)
+                                        }
+                                        .padding(.horizontal)
+                                        .padding(.horizontal)
+
+                                        ActivityHorizontalListView(activities: subskillActivities, onStartActivity: self.onActivitySelected)
+
+                                        Divider()
+                                            .padding(.horizontal)
+                                            .padding(.horizontal)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    ScrollView(showsIndicators: true) {
+                        VStack(alignment: .leading, spacing: 30) {
+                            LazyVGrid(columns: Array(repeating: GridItem(), count: 3)) {
+                                ForEach(self.activities.filter {
+                                    $0.skills.contains(self.subskills[0])
+                                }) { activity in
+                                    NavigationLink(destination:
+                                        ActivityDetailsView(activity: activity, onStartActivity: self.onActivitySelected))
+                                    {
+                                        VerticalItem(CurationItemModel(id: activity.uuid, contentType: .activity))
+                                    }
+                                }
+                            }
+                            .padding(.horizontal)
+                        }
+                    }
+                }
+            }
+        }
+        .navigationTitle(self.subskills[0].name)
+    }
+
+    // MARK: Internal
+
+    let backgroundColors: [Color] = [.purple, .blue, .green, .orange, .teal, .pink, .mint, .red, .yellow, .cyan]
+
+    let activities: [Activity] = ContentKit.allPublishedActivities.sorted {
+        $0.details.title.compare($1.details.title, locale: NSLocale.current) == .orderedAscending
+    }
+
+    let subskills: [Skill]
+    let onActivitySelected: ((Activity) -> Void)?
+
+    // MARK: Private
+
+    @ObservedObject private var styleManager: StyleManager = .shared
+
+    private let columns = Array(repeating: GridItem(), count: 2)
+}
+
+#Preview {
+    NavigationStack {
+        SubskillsGridView(
+            subskills: Skills.primarySkillsList,
+            onActivitySelected: { _ in
+                print("Activity Selected")
+            }
+        )
+    }
+}
