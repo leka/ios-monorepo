abd828df41761694953a07a71018394f2ed81150
mathieu J. <macteuts@gmail.com>
2025-05-05 17:53:54 +0300

    ♻️ (LekaApp): Update views w/ observable CarereceiverManagerVM

--- Apps/LekaApp/Sources/Views/Users/CareReceiver/CarereceiverList.swift
+++ Apps/LekaApp/Sources/Views/Users/CareReceiver/CarereceiverList.swift
@@ -93,7 +93,7 @@ struct CarereceiverList: View {
 
     private let columns = Array(repeating: GridItem(), count: 4)
 
-    @StateObject private var carereceiverManagerViewModel = CarereceiverManagerViewModel()
+    @State private var carereceiverManagerViewModel = CarereceiverManagerViewModel()
     @ObservedObject private var styleManager: StyleManager = .shared
     @State private var isCarereceiverCreationPresented: Bool = false
 }
