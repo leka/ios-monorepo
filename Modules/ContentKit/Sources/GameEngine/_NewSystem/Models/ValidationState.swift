1840084d1b074155098aa1db8a8ebdc83c91d3a8
Hugo Pezziardi <hugo@pezziardi.net>
2025-06-30 12:16:09 +0200

    ✨ (NewGEK): Add ValidationState struct

---
+++ Modules/ContentKit/Sources/GameEngine/_NewSystem/Models/ValidationState.swift
@@ -0,0 +1,11 @@
+// Leka - iOS Monorepo
+// Copyright APF France handicap
+// SPDX-License-Identifier: Apache-2.0
+
+// MARK: - ValidationState
+
+public enum ValidationState {
+    case hidden
+    case disabled
+    case enabled
+}
