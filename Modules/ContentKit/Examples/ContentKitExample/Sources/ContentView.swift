37fec25d9927c54f1242e69a26befbd8b2f55774
Ladislas de Toldi <ladislas@detoldi.me>
2025-06-30 13:07:34 +0200

    ⚗️ (GEKExample): Refactor in 3 activities for different interfaces

--- Modules/ContentKit/Examples/ContentKitExample/Sources/ContentView.swift
+++ Modules/ContentKit/Examples/ContentKitExample/Sources/ContentView.swift
@@ -14,14 +14,36 @@ struct ContentView: View {
 
     var body: some View {
         NavigationStack {
-            Button {
-                self.isActivityPresented = true
-                let newgekyaml = Bundle.main.url(forResource: "new_gek_activity", withExtension: "yml")
-                let content = try? String(contentsOf: newgekyaml!, encoding: .utf8)
-                log.debug("\(content ?? "No YAML file found")")
-                self.navigation.setCurrentActivity(NewActivity(yaml: content!)!)
-            } label: {
-                Text("Activity")
+            VStack(spacing: 30) {
+                Button {
+                    self.isActivityPresented = true
+                    let newgekyaml = Bundle.main.url(forResource: "new_gek_activity_tts", withExtension: "yml")
+                    let content = try? String(contentsOf: newgekyaml!, encoding: .utf8)
+                    log.debug("\(content ?? "No YAML file found")")
+                    self.navigation.setCurrentActivity(NewActivity(yaml: content!)!)
+                } label: {
+                    Text("Touch To Select")
+                }
+
+                Button {
+                    self.isActivityPresented = true
+                    let newgekyaml = Bundle.main.url(forResource: "new_gek_activity_dnd_grid", withExtension: "yml")
+                    let content = try? String(contentsOf: newgekyaml!, encoding: .utf8)
+                    log.debug("\(content ?? "No YAML file found")")
+                    self.navigation.setCurrentActivity(NewActivity(yaml: content!)!)
+                } label: {
+                    Text("Drag and Drop w/ Grid")
+                }
+
+                Button {
+                    self.isActivityPresented = true
+                    let newgekyaml = Bundle.main.url(forResource: "new_gek_activity_dnd_with_zones", withExtension: "yml")
+                    let content = try? String(contentsOf: newgekyaml!, encoding: .utf8)
+                    log.debug("\(content ?? "No YAML file found")")
+                    self.navigation.setCurrentActivity(NewActivity(yaml: content!)!)
+                } label: {
+                    Text("Drag and Drop w/ Zones")
+                }
             }
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
