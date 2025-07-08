c2ed95fb8e8378a9020233a7935a5228ad7ea093
Hugo Pezziardi <hugo@pezziardi.net>
2025-07-08 15:04:23 +0200

    ✨ (NewGEK ): Add completionData to MagicCard coordinator

--- Modules/ContentKit/Sources/GameEngine/_NewSystem/Coordinators/CurrentExerciseCoordinator.swift
+++ Modules/ContentKit/Sources/GameEngine/_NewSystem/Coordinators/CurrentExerciseCoordinator.swift
@@ -276,9 +276,9 @@ public class CurrentExerciseCoordinator {
                                         .onAppear {
                                             coordinator.didComplete
                                                 .receive(on: DispatchQueue.main)
-                                                .sink { [weak self] _ in
-                                                    // TODO: (@ladislas) implement calculation
-                                                    self?.didComplete.send((.excellent, nil))
+                                                .sink { [weak self] completionData in
+                                                    let evaluationLevel = coordinator.evaluate(in: .practice)
+                                                    self?.didComplete.send((evaluationLevel, completionData))
                                                 }
                                                 .store(in: &self.cancellables)
                                         }
