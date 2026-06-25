0b1ae1b00012de35149582ad606179b110ced60d
Hugo Pezziardi <hugo@pezziardi.net>
2026-06-25 11:59:37 +0200

    🚚 refactor(content): Move NewGEK shared survivors
    Move shared exercise/gameplay state and action button utilities out of GameEngine/OldSystem so NewGEK no longer relies on declarations living under the old source tree.
    
    Verification: rg survivor declarations under GameEngine/OldSystem.
    
    closes #2050

--- Modules/ContentKit/Sources/GameEngine/OldSystem/Exercises/ActionButton/ActionButton+Observe.swift
+++ Modules/ContentKit/Sources/GameEngine/OldSystem/Exercises/ActionButton/ActionButton+Observe.swift
@@ -7,38 +7,6 @@ import LocalizationKit
 import SVGView
 import SwiftUI
 
-// MARK: - AnimatableBlur
-
-struct AnimatableBlur: AnimatableModifier {
-    var blurRadius: CGFloat
-
-    var animatableData: CGFloat {
-        get { self.blurRadius }
-        set { self.blurRadius = newValue }
-    }
-
-    func body(content: Content) -> some View {
-        content
-            .blur(radius: self.blurRadius)
-    }
-}
-
-// MARK: - AnimatableSaturation
-
-struct AnimatableSaturation: AnimatableModifier {
-    var saturation: Double
-
-    var animatableData: Double {
-        get { self.saturation }
-        set { self.saturation = newValue }
-    }
-
-    func body(content: Content) -> some View {
-        content
-            .saturation(self.saturation)
-    }
-}
-
 // MARK: - ActionButtonObserve
 
 struct ActionButtonObserve: View {
